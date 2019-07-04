2163c66a52bba2d499a2c5b24540feea0b908da6
Takumi Oda <xiaotiantakumi@gmail.com>
2019-07-04 12:44:40 +0900

    Add ABC132

---
+++ Atcoder/ABC132/A.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC132
+{
+    public class A : IAtcoder
+    {
+        public void Run()
+        {
+            var S = Console.ReadLine();
+
+            foreach (var c in S)
+            {
+                if (S.Where(x => x == c).Count() != 2)
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+            }
+            Console.WriteLine("Yes");
+        }
+    }
+}
---
+++ Atcoder/ABC132/B.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC132
+{
+    public class B : IAtcoder
+    {
+        public void Run()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var ps = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int ret = 0;
+            for (int i = 0; i < ps.Length - 2; i++)
+            {
+                if (ps[i] < ps[i + 1] && ps[i + 1] < ps[i + 2] ||
+                    ps[i] > ps[i + 1] && ps[i + 1] > ps[i + 2])
+                {
+                    ret++;
+                }
+            }
+            Console.WriteLine(ret);
+        }
+    }
+}
