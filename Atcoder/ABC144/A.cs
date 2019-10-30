459ce4e4e2acfbe008703dc01d74ef9447b4d4ff
Takumi Oda <xiaotiantakumi@gmail.com>
2019-10-30 23:04:36 +0900

    ABC144のA,Bを回答

---
+++ Atcoder/ABC144/A.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC144
+{
+    public class A : IAtcoder
+    {
+        public void Run()
+        {
+                        var tmp = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            foreach (var val in tmp)
+            {
+                if (val >= 10)
+                {
+                    Console.Write("-1");
+                    return;
+                }
+            }
+
+            var result = tmp[0] * tmp[1];
+            Console.Write(result);
+        }
+    }
+}
---
+++ Atcoder/ABC144/C.cs
@@ -0,0 +1,9 @@
+﻿namespace Atcoder.ABC144
+{
+    public class C : IAtcoder
+    {
+        public void Run()
+        {
+        }
+    }
+}
