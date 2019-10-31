b13b1eb05148ec7f968ba4fc7dd045f0a202793f
Takumi Oda <xiaotiantakumi@gmail.com>
2019-10-31 23:19:30 +0900

    ABC143 C回答

--- Atcoder/ABC143/C.cs
+++ Atcoder/ABC143/C.cs
@@ -1,9 +1,23 @@
-﻿namespace Atcoder.ABC143
+﻿using System;
+
+namespace Atcoder.ABC143
 {
     public class C : IAtcoder
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var S = Console.ReadLine();
+            var lenS = S.Length;
+            long ret = 1;
+            for (int i = 1; i <= lenS - 1; i++)
+            {
+                if (S[i - 1] != S[i])
+                {
+                    ret += 1;
+                }
+            }
+            Console.Write(ret);
         }
     }
 }
