2b7bd40d870faed52e19f48e3cbf722de28c4814
Takumi Oda <xiaotiantakumi@gmail.com>
2019-11-01 08:43:43 +0900

    ABC142のA,Bを回答

---
+++ Atcoder/ABC142/A.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC142
+{
+    public class A : IAtcoder
+    {
+        public void Run()
+        {
+            double N = double.Parse(Console.ReadLine());
+            int oddCount = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    oddCount++;
+                }
+            }
+
+            double ret = oddCount / N;
+            Console.Write(ret);
+        }
+    }
+}
---
+++ Atcoder/ABC142/B.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC142
+{
+    public class B : IAtcoder
+    {
+        public void Run()
+        {
+            var firstLine = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var hArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int ret = 0;
+            for (int i = 0; i < firstLine[0]; i++)
+            {
+                if (hArray[i] >= firstLine[1])
+                {
+                    ret++;
+                }
+            }
+            Console.WriteLine(ret);
+        }
+    }
+}
