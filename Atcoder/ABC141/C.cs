82505798f5b0f2dedaff73de1c6ee56c44899a95
Takumi Oda <xiaotiantakumi@gmail.com>
2019-11-04 00:20:21 +0900

    ABC141 C問題回答

--- Atcoder/ABC141/C.cs
+++ Atcoder/ABC141/C.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Atcoder.ABC141
 {
@@ -6,7 +7,34 @@ namespace Atcoder.ABC141
     {
         public void Run()
         {
-            
+            var firstLine = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+            long N = firstLine[0];
+            var K = firstLine[1];
+            var Q = firstLine[2];
+
+            long[] Points = new long[N];
+            for (int i = 0; i < N; i++)
+            {
+                Points[i] = 0;
+            }
+
+            for (int i = 0; i < Q; i++)
+            {
+                var target = int.Parse(Console.ReadLine());
+                Points[target - 1]++;
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                if (K - Q + Points[i] > 0)
+                {
+                    Console.WriteLine("Yes");
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+            }
         }
     }
 }
