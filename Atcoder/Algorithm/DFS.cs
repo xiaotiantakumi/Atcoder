b19d31ce8a6c5752fd8936e73644db3ff9c10820
Takumi Oda <xiaotiantakumi@gmail.com>
2019-11-09 20:56:49 +0900

    Algorithm用のライブラリを作る。まずはDFSを作る予定だけど、まだ未完成。

---
+++ Atcoder/Algorithm/DFS.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atcoder.Algorithm
+{
+    class DFS
+    {
+        public List<bool> seen = new List<bool>();
+        public void dfs(List<List<int>> graph, int v)
+        {
+            seen[v] = true;
+            foreach (var nextV in graph[v])
+            {
+                if (seen[nextV])
+                {
+                    continue;
+                }
+                dfs(graph, nextV);
+            }
+        }
+
+        public void Execute()
+        {
+            var N = 15;
+            var M = 14;
+            var dfs = new Algorithm.DFS();
+            List<List<int>> graph = new List<List<int>>();
+            int[][] graph2 = new int[][] { };
+            for (int i = 0; i < N; i++)
+            {
+                graph.Add(new List<int>());
+                dfs.seen.Add(false);
+            }
+            for (int i = 0; i < M; i++)
+            {
+                var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                var a = input[0];
+                var b = input[1];
+                graph[a].Add(b);
+                graph[b].Add(a);
+            }
+            dfs.dfs(graph, 0);
+        }
+    }
+//    public class DFS
+//    {
+//        public static void Search(Node root)
+//        {
+//            if (root == null)
+//            {
+//                return;
+//            }
+//            Visit(root);
+//            root.Visited = true;
+//            if (root.Children != null)
+//            {
+//                foreach (var node in root.Children)
+//                {
+//                    if (!node.Visited)
+//                    {
+//                        Search(node);
+//                    }
+//                }
+//            }
+//        }
+//
+//        private static void Visit(Node root)
+//        {
+//            // nop
+//        }
+//    }
+//
+//
+//    public class Node
+//    {
+//        public bool Visited { get; set; }
+//
+//        public Node[] Children { get; set; }
+//    }
+
+}
