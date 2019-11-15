322eb0b48b986b6050f542f4051350c081005ec3
Takumi Oda <xiaotiantakumi@gmail.com>
2019-11-15 12:45:10 +0900

    ABC108A回答

--- Atcoder/ABC108/A.cs
+++ Atcoder/ABC108/A.cs
@@ -6,7 +6,18 @@ namespace Atcoder.ABC108
     {
         public void Run()
         {
-            
+            var K = int.Parse(Console.ReadLine());
+            var A = K / 2;
+            int B;
+            if (K % 2 == 0)
+            {
+                B = A;
+            }
+            else
+            {
+                B = A + 1;
+            }
+            Console.WriteLine(A * B);
         }
     }
 }
