8134de5b082208b55e85db160e2597b3c03dcba4
Takumi Oda <xiaotiantakumi@gmail.com>
2019-11-16 10:28:57 +0900

    ABC107のA回答

--- Atcoder/ABC107/A.cs
+++ Atcoder/ABC107/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC107
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC107
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Console.WriteLine(input[0] - input[1] + 1);
         }
     }
 }
