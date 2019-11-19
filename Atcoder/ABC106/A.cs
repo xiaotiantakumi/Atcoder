0a80509484676db55e34a96a787673a20db8a6d8
Takumi Oda <xiaotiantakumi@gmail.com>
2019-11-20 08:45:53 +0900

    ABC106のAB回答

--- Atcoder/ABC106/A.cs
+++ Atcoder/ABC106/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC106
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC106
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Console.WriteLine((input[0] - 1) * (input[1] - 1));
         }
     }
 }
--- Atcoder/ABC106/B.cs
+++ Atcoder/ABC106/B.cs
@@ -1,9 +1,45 @@
-﻿namespace Atcoder.ABC106
+﻿using System;
+
+namespace Atcoder.ABC106
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            if (N == 105)
+            {
+                Console.WriteLine(1);
+                return;
+            }
+            else if (N < 105)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            int ret = 1;
+            for (int i = 106; i <= N; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    continue;
+                }
+                int tmpRet = 0;
+                for (int j = 1; j <= N; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        tmpRet++;
+                    }
+                }
+
+                if (tmpRet == 8)
+                {
+                    ret++;
+                }
+            }
+            Console.WriteLine(ret);
         }
     }
 }
