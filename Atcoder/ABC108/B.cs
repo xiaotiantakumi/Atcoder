1d7ebd991ef6ca354a1936358c3878127e895673
Takumi Oda <xiaotiantakumi@gmail.com>
2019-11-20 20:31:57 +0900

    ABC108のB回答

--- Atcoder/ABC108/B.cs
+++ Atcoder/ABC108/B.cs
@@ -8,6 +8,19 @@ namespace Atcoder.ABC108
     {
         public void Run()
         {
+            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var a = input[0];
+            var b = input[1];
+            var c = input[2];
+            var d = input[3];
+            var x = c - a;
+            var y = d - b;
+
+            var x3 = c - y;
+            var y3 = d + x;
+            var x4 = a - y;
+            var y4 = b + x;
+            Console.WriteLine(string.Join(" ", new int[] { x3, y3, x4, y4 }));
         }
     }
 }
