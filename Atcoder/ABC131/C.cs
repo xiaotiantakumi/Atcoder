f9f4d9b5339314bbe09bd172735ee96be500a1ad
Takumi Oda <xiaotiantakumi@gmail.com>
2019-11-21 23:36:04 +0900

    ABC131のCの実装途中。時間超過。

--- Atcoder/ABC131/C.cs
+++ Atcoder/ABC131/C.cs
@@ -7,6 +7,23 @@ namespace Atcoder.ABC131
     {
         public void Run()
         {
+            // 最初に思いついたのはこれ。
+            // でもサンプルの３つめですでに結果が返ってこない。。。
+            // 工夫して実行速度を速める必要がある。
+            var input = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+            var A = input[0];
+            var B = input[1];
+            var C = input[2];
+            var D = input[3];
+            int ret = 0;
+            for (long i = A; i <= B; i++)
+            {
+                if (i % C != 0 && i % D != 0)
+                {
+                    ret++;
+                }
+            }
+            Console.Write(ret);
         }
     }
 }
