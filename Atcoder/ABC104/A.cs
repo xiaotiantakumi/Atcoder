3108c4e5341ee0e3335fc4ff6166968955c84546
Takumi Oda <xiaotiantakumi@gmail.com>
2019-11-21 20:33:33 +0900

    ABC104のA回答

--- Atcoder/ABC104/A.cs
+++ Atcoder/ABC104/A.cs
@@ -1,10 +1,24 @@
-﻿namespace Atcoder.ABC104
+﻿using System;
+
+namespace Atcoder.ABC104
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var R = int.Parse(Console.ReadLine());
+            if (R < 1200)
+            {
+                Console.WriteLine("ABC");
+            }
+            else if (R < 2800)
+            {
+                Console.WriteLine("ARC");
+            }
+            else
+            {
+                Console.WriteLine("AGC");
+            }
         }
     }
 }
