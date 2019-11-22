08774b84a11c697df5d327efc62bf080bbea90d0
Takumi Oda <xiaotiantakumi@gmail.com>
2019-11-22 23:19:21 +0900

    ABC131C途中だけど、一度チェックイン。惜しいところまでいけてるはずなんだけど、わからない。境界がうまくいってない。

--- Atcoder/ABC131/C.cs
+++ Atcoder/ABC131/C.cs
@@ -7,23 +7,17 @@ namespace Atcoder.ABC131
     {
         public void Run()
         {
-            // 最初に思いついたのはこれ。
-            // でもサンプルの３つめですでに結果が返ってこない。。。
-            // 工夫して実行速度を速める必要がある。
             var input = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
             var A = input[0];
             var B = input[1];
             var C = input[2];
             var D = input[3];
-            int ret = 0;
-            for (long i = A; i <= B; i++)
-            {
-                if (i % C != 0 && i % D != 0)
-                {
-                    ret++;
-                }
-            }
-            Console.Write(ret);
+            var CdivideCount = (B / C) - ((A - 1) / C);
+            var DdivideCount = (B / D) - ((A - 1) / D);
+            var CDdivideCount = (B / (C * D)) - ((A - 1) / (C * D));
+            var len = B - A + 1;
+            var ret = len - (CdivideCount + DdivideCount - CDdivideCount);
+            Console.WriteLine(ret);
         }
     }
 }
