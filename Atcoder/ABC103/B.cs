723068e7d34e2bf1d5d8d423573d0fbff26d6b08
Takumi Oda <xiaotiantakumi@gmail.com>
2019-11-23 09:29:24 +0900

    ABC103のB回答

--- Atcoder/ABC103/B.cs
+++ Atcoder/ABC103/B.cs
@@ -1,9 +1,28 @@
-﻿namespace Atcoder.ABC103
+﻿using System;
+
+namespace Atcoder.ABC103
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var S = Console.ReadLine();
+            var T = Console.ReadLine();
+
+            var ret = false;
+
+            for (int i = T.Length - 1; i >= 0; i--)
+            {
+                var take = T.Substring(T.Length - 1, 1);
+                T = T.Substring(0, T.Length - 1);
+                T = T.Insert(0, take);
+                if (S == T)
+                {
+                    Console.WriteLine("Yes");
+                    return;
+                }
+            }
+            Console.WriteLine("No");
         }
     }
 }
