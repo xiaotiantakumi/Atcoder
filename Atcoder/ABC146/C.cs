c124563df4c44d11e26d004f003f2ce72df048a1
Takumi Oda <xiaotiantakumi@gmail.com>
2019-11-25 12:17:30 +0900

    ABC146のC途中

--- Atcoder/ABC146/C.cs
+++ Atcoder/ABC146/C.cs
@@ -1,9 +1,62 @@
-﻿namespace Atcoder.ABC146
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC146
 {
     public class C : IAtcoder
     {
         public void Run()
         {
+            var input = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+            var A = input[0];
+            var B = input[1];
+            var X = input[2];
+
+            long N = 1000000000;
+            long ret = long.MinValue;
+
+            int index = 0;
+            long minVal = long.MaxValue;
+            long maxVal = long.MinValue;
+            while (index < 100000)
+            {
+                var tmp = A * N + B * N.ToString().Length;
+                if (X < tmp)
+                {
+                    if (ret != long.MinValue)
+                    {
+                        N = N - ((N - ret) / 2);
+                    }
+                    else
+                    {
+                        N = N / 2;
+                    }
+                }
+                else
+                {
+                    if (N >= 1000000000)
+                    {
+                        N = 1000000000;
+                    }
+                    if (ret <= N)
+                    {
+                        ret = N;
+                    }
+                    N = N + (N / 2);
+                    minVal = N;
+                }
+
+                index++;
+            }
+
+            if (ret == long.MinValue)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                Console.WriteLine(ret);
+            }
         }
     }
 }
