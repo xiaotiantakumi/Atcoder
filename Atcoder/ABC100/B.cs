4f75ad1247b7f3f3bed40984cd12cd594cc431d1
Takumi Oda <xiaotiantakumi@gmail.com>
2019-11-26 21:11:44 +0900

    ABC100のB回答

--- Atcoder/ABC100/B.cs
+++ Atcoder/ABC100/B.cs
@@ -7,12 +7,14 @@ namespace Atcoder.ABC100
     {
         public void Run()
         {
-            // まだ正答できてない。解説見て理解してから。
-            // 100の時だけ違う動きさせないといけないようだけど、なぜかわからない。
             var firstLine = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             var D = firstLine[0];
             var N = firstLine[1];
             var tmp = Math.Pow(100, D);
+            if (N == 100)
+            {
+                N++;
+            }
             Console.WriteLine(tmp * N);
         }
     }
