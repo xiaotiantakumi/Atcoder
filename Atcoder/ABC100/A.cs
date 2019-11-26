6f7a310895bc8ed9f2b2a7fc3860b7a6919db566
Takumi Oda <xiaotiantakumi@gmail.com>
2019-11-26 12:39:00 +0900

    ABC100のA回答，B途中

--- Atcoder/ABC100/A.cs
+++ Atcoder/ABC100/A.cs
@@ -1,10 +1,23 @@
-﻿namespace Atcoder.ABC100
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC100
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var firstLine = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var A = firstLine[0];
+            var B = firstLine[1];
+            if (Math.Abs(A - B) > 1)
+            {
+                Console.WriteLine(":(");
+            }
+            else
+            {
+                Console.WriteLine("Yay!");
+            }
         }
     }
 }
--- Atcoder/ABC100/B.cs
+++ Atcoder/ABC100/B.cs
@@ -1,9 +1,19 @@
-﻿namespace Atcoder.ABC100
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC100
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            // まだ正答できてない。解説見て理解してから。
+            // 100の時だけ違う動きさせないといけないようだけど、なぜかわからない。
+            var firstLine = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var D = firstLine[0];
+            var N = firstLine[1];
+            var tmp = Math.Pow(100, D);
+            Console.WriteLine(tmp * N);
         }
     }
 }
