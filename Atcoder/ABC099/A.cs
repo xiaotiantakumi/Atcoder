ba992dabfbbed9539d7051876e99adcca4ad909f
Takumi Oda <xiaotiantakumi@gmail.com>
2019-11-27 12:35:18 +0900

    ABC099のAB回答

--- Atcoder/ABC099/A.cs
+++ Atcoder/ABC099/A.cs
@@ -1,10 +1,20 @@
-﻿namespace Atcoder.ABC099
+﻿using System;
+
+namespace Atcoder.ABC099
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var N = int.Parse(Console.ReadLine());
+            if (N >= 1000)
+            {
+                Console.WriteLine("ABD");
+            }
+            else
+            {
+                Console.WriteLine("ABC");
+            }
         }
     }
 }
--- Atcoder/ABC099/B.cs
+++ Atcoder/ABC099/B.cs
@@ -1,9 +1,32 @@
-﻿namespace Atcoder.ABC099
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC099
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var firstLine = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var dif = firstLine[1] - firstLine[0];
+            int lastTree = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (lastTree == 0)
+                {
+                    lastTree = i;
+                    continue;
+                }
+
+                var nowTree = lastTree + i;
+                if (nowTree - lastTree == dif)
+                {
+                    Console.WriteLine(lastTree - firstLine[0]);
+                    return;
+                }
+
+                lastTree = nowTree;
+            }
         }
     }
 }
