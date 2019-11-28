72ebe4979d2041137870c9df32ef362f55645830
Takumi Oda <xiaotiantakumi@gmail.com>
2019-11-28 12:40:10 +0900

    ABC098のAB回答

--- Atcoder/ABC098/A.cs
+++ Atcoder/ABC098/A.cs
@@ -1,10 +1,19 @@
-﻿namespace Atcoder.ABC098
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC098
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var firstLine = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var A = firstLine[0];
+            var B = firstLine[1];
+            var sum = A + B;
+            var minus = A - B;
+            var multi = A * B;
+            Console.WriteLine(Math.Max(Math.Max(sum, minus), multi));
         }
     }
 }
--- Atcoder/ABC098/B.cs
+++ Atcoder/ABC098/B.cs
@@ -1,9 +1,43 @@
-﻿namespace Atcoder.ABC098
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC098
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var S = Console.ReadLine();
+            string X = string.Empty;
+            string Y = string.Empty;
+            int ret = 0;
+            for (int i = 0; i < N; i++)
+            {
+                int tmpRet = 0;
+                X += S[i];
+                Y = string.Empty;
+                for (int j = i + 1; j < N; j++)
+                {
+                    Y += S[j];
+                }
+
+                var XList = X.Distinct().ToList();
+                foreach (var x in XList)
+                {
+                    var tmp = Y.IndexOf(x);
+                    if (tmp != -1)
+                    {
+                        tmpRet++;
+                    }
+                }
+
+                if (ret < tmpRet)
+                {
+                    ret = tmpRet;
+                }
+            }
+            Console.WriteLine(ret);
         }
     }
 }
