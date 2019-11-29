0a331e18c28445f339e9f6e3db80290044500caa
Takumi Oda <xiaotiantakumi@gmail.com>
2019-11-29 22:11:20 +0900

    ABC097のAB回答

--- Atcoder/ABC097/A.cs
+++ Atcoder/ABC097/A.cs
@@ -1,10 +1,23 @@
-﻿namespace Atcoder.ABC097
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC097
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var f = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            if ((Math.Abs(f[1] - f[0]) <= f[3] &&
+                 Math.Abs(f[2] - f[1]) <= f[3]) ||
+                Math.Abs(f[2] - f[0]) <= f[3])
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
--- Atcoder/ABC097/B.cs
+++ Atcoder/ABC097/B.cs
@@ -1,9 +1,30 @@
-﻿namespace Atcoder.ABC097
+﻿using System;
+
+namespace Atcoder.ABC097
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var X = int.Parse(Console.ReadLine());
+
+            double ret = 1;
+            for (int i = 1; i < X; i++)
+            {
+                for (int j = 2; j < 1000; j++)
+                {
+                    var cal = Math.Pow(i, j);
+                    if (cal > X)
+                    {
+                        break;
+                    }
+                    if (ret < cal)
+                    {
+                        ret = cal;
+                    }
+                }
+            }
+            Console.WriteLine(ret);
         }
     }
 }
