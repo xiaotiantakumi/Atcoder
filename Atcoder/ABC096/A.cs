99f317e9389611d3c3cae4ddff03d6c5146ceaa0
Takumi Oda <xiaotiantakumi@gmail.com>
2019-11-30 10:14:23 +0900

    ABC096のAB回答

--- Atcoder/ABC096/A.cs
+++ Atcoder/ABC096/A.cs
@@ -1,10 +1,21 @@
-﻿namespace Atcoder.ABC096
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC096
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var r = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            if (r[0] <= r[1])
+            {
+                Console.WriteLine(r[0]);
+            }
+            else
+            {
+                Console.WriteLine(r[0] - 1);
+            }
         }
     }
 }
--- Atcoder/ABC096/B.cs
+++ Atcoder/ABC096/B.cs
@@ -1,9 +1,18 @@
-﻿namespace Atcoder.ABC096
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC096
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var f = Console.ReadLine().Split(' ').Select(int.Parse).OrderBy(x => x).ToList();
+            var k = int.Parse(Console.ReadLine());
+            var max = f[2];
+            var calVal = max * Math.Pow(2, k);
+            var ret = f[0] + f[1] + calVal;
+            Console.WriteLine(ret);
         }
     }
 }
