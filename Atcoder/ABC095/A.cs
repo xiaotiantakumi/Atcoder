bbe837c6bc9b3556faa01a0f5e193ac73146362e
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-01 09:57:49 +0900

    ABC095のAB回答

--- Atcoder/ABC095/A.cs
+++ Atcoder/ABC095/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC095
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC095
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var S = Console.ReadLine().ToList().Count(x => x == 'o');
+            Console.WriteLine(700 + (100 * S));
         }
     }
 }
--- Atcoder/ABC095/B.cs
+++ Atcoder/ABC095/B.cs
@@ -1,9 +1,28 @@
-﻿namespace Atcoder.ABC095
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC095
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var f = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var N = f[0];
+            var X = f[1];
+            int minMaterial = int.MaxValue;
+            for (int i = 0; i < N; i++)
+            {
+                var material = int.Parse(Console.ReadLine());
+                X -= material;
+                if (minMaterial >= material)
+                {
+                    minMaterial = material;
+                }
+            }
+
+            var countMinMaterial = X / minMaterial;
+            Console.WriteLine(N + countMinMaterial);
         }
     }
 }
