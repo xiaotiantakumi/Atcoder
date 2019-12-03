79acca4f1f72e8262ccae59c37d09d7e4eb6659d
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-03 12:41:13 +0900

    ABC094のAB回答

--- Atcoder/ABC094/A.cs
+++ Atcoder/ABC094/A.cs
@@ -1,10 +1,19 @@
-﻿namespace Atcoder.ABC094
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC094
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var f = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            string ret = "NO";
+            if (f[0] + f[1] >= f[2] && f[0] <= f[2])
+            {
+                ret = "YES";
+            }
+            Console.WriteLine(ret);
         }
     }
 }
--- Atcoder/ABC094/B.cs
+++ Atcoder/ABC094/B.cs
@@ -1,9 +1,37 @@
-﻿namespace Atcoder.ABC094
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC094
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var f = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var N = f[0];
+            var M = f[1];
+            var X = f[2];
+            var seconds = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int left = 0;
+            int right = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (!seconds.Contains(i))
+                {
+                    continue;
+                }
+                if (i <= X)
+                {
+                    left++;
+                }
+                else
+                {
+                    right++;
+                }
+            }
+
+            Console.WriteLine(left < right ? left : right);
         }
     }
 }
