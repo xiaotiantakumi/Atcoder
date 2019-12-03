8f0f0d052ec080ac698b02e40c2ea92add4eea8d
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-04 08:36:28 +0900

    ABC093のA回答

--- Atcoder/ABC093/A.cs
+++ Atcoder/ABC093/A.cs
@@ -1,10 +1,22 @@
-﻿namespace Atcoder.ABC093
+﻿using System;
+
+namespace Atcoder.ABC093
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var S = Console.ReadLine();
+            if (S.Contains("a") &&
+                S.Contains("b") &&
+                S.Contains("c"))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
