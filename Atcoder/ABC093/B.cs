141e63a238f79e99200feef626769d8301554f75
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-04 20:10:43 +0900

    ABC093のB回答

--- Atcoder/ABC093/B.cs
+++ Atcoder/ABC093/B.cs
@@ -1,9 +1,40 @@
-﻿namespace Atcoder.ABC093
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC093
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var f = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            List<int> ret = new List<int>();
+            if (f[1] - f[0] <= f[2])
+            {
+                for (int i = f[0]; i <= f[1]; i++)
+                {
+                    Console.WriteLine(i);
+                }
+                return;
+            }
+            for (int i = f[0]; i < f[0] + f[2]; i++)
+            {
+                ret.Add(i);
+            }
+
+            for (int i = f[1] - f[2] + 1; i <= f[1]; i++)
+            {
+                if (!ret.Contains(i))
+                {
+                    ret.Add(i);
+                }
+            }
+
+            foreach (var i in ret)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
