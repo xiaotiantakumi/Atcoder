8db027ef73a06bbbabe1e9f0677a983c20696633
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-05 12:43:12 +0900

    ABC092のB回答

--- Atcoder/ABC092/B.cs
+++ Atcoder/ABC092/B.cs
@@ -1,9 +1,30 @@
-﻿namespace Atcoder.ABC092
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC092
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var f = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var D = f[0];
+            var ret = f[1];
+
+            for (int i = 0; i < N; i++)
+            {
+                var tmp = int.Parse(Console.ReadLine());
+                int val = tmp + 1;
+                int choco = 1;
+                while (val <= D)
+                {
+                    choco++;
+                    val = (choco * tmp) + 1;
+                }
+                ret += choco;
+            }
+            Console.WriteLine(ret);
         }
     }
 }
