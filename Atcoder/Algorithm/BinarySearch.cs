09a45d439b6484d7bcaed819f453be2f9a784b17
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-05 20:36:27 +0900

    ABC146のCで模範解答を記録しておく。まだ自分で組めない。

---
+++ Atcoder/Algorithm/BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atcoder.Algorithm
+{
+    // ABC146のC
+    class BinarySearch : IAtcoder
+    {
+        public void Run()
+        {
+            var f = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            long A = f[0];
+            long B = f[1];
+            long X = f[2];
+
+            long price = 0;
+            long min = 0;
+            long mid = 0;
+            long max = 1000000001;
+
+            while (max - min > 1)
+            {
+                mid = (min + max) / 2;
+                price = A * mid + B * (mid.ToString().Length);
+                if (price <= X)
+                    min = mid;
+                else
+                    max = mid;
+            }
+
+            Console.WriteLine(min);
+        }
+    }
+}
