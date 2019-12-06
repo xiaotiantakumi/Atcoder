b64cbb6c905d3d26694111cb3079397a01576ee0
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-06 12:42:30 +0900

    ABC139のC回答

--- Atcoder/ABC139/C.cs
+++ Atcoder/ABC139/C.cs
@@ -7,7 +7,28 @@ namespace Atcoder.ABC139
     {
         public void Run()
         {
-            
+            var N = int.Parse(Console.ReadLine());
+            var l = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int ret = 0;
+            int tmp = 0;
+
+            for (int i = 0; i < N - 1; i++)
+            {
+                if (l[i] >= l[i + 1])
+                {
+                    tmp++;
+                }
+                else
+                {
+                    tmp = 0;
+                }
+
+                if (tmp > ret)
+                {
+                    ret = tmp;
+                }
+            }
+            Console.WriteLine(ret);
         }
     }
 }
