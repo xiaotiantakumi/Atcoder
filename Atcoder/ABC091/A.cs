f3e04de182c5b878fd7f69e0592f97ec5d65d6e1
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-06 12:31:12 +0900

    ABC091のAB回答

--- Atcoder/ABC091/A.cs
+++ Atcoder/ABC091/A.cs
@@ -1,10 +1,21 @@
-﻿namespace Atcoder.ABC091
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC091
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var l = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            if (l[2] <= l[0] + l[1])
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
--- Atcoder/ABC091/B.cs
+++ Atcoder/ABC091/B.cs
@@ -1,9 +1,50 @@
-﻿namespace Atcoder.ABC091
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC091
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            Dictionary<string, int> valList = new Dictionary<string, int>();
+            for (int i = 0; i < N; i++)
+            {
+                var s = Console.ReadLine();
+                if (valList.ContainsKey(s))
+                {
+                    valList[s]++;
+                }
+                else
+                {
+                    valList.Add(s, 1);
+                }
+            }
+            var M = int.Parse(Console.ReadLine());
+            for (int i = 0; i < M; i++)
+            {
+                var s = Console.ReadLine();
+                if (valList.ContainsKey(s))
+                {
+                    valList[s]--;
+                }
+                else
+                {
+                    valList.Add(s, -1);
+                }
+            }
+
+            if (valList.Values.Max() >= 0)
+            {
+                Console.WriteLine(valList.Values.Max());
+
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
         }
     }
 }
