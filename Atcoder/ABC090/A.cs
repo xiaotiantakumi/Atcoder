18245657e0d7f0a15ca80aa259ea3c118e3e8d8f
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-08 09:26:46 +0900

    ABCのAB回答

--- Atcoder/ABC090/A.cs
+++ Atcoder/ABC090/A.cs
@@ -1,10 +1,16 @@
-﻿namespace Atcoder.ABC090
+﻿using System;
+
+namespace Atcoder.ABC090
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            string ret = string.Empty;
+            ret += Console.ReadLine().ToCharArray()[0];
+            ret += Console.ReadLine().ToCharArray()[1];
+            ret += Console.ReadLine().ToCharArray()[2];
+            Console.WriteLine(ret);
         }
     }
 }
--- Atcoder/ABC090/B.cs
+++ Atcoder/ABC090/B.cs
@@ -1,9 +1,26 @@
-﻿namespace Atcoder.ABC090
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC090
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var l = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var A = l[0];
+            var B = l[1];
+            int count = 0;
+            for (int i = A; i <= B; i++)
+            {
+                var checkTarget = i.ToString().ToCharArray();
+                if (checkTarget[0] == checkTarget[4] &&
+                    checkTarget[1] == checkTarget[3])
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
         }
     }
 }
