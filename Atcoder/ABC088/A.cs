d1632cfed97c80f2ea4fcee463219fa20aa5d271
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-11 07:24:56 +0900

    ABC088のAB回答

--- Atcoder/ABC088/A.cs
+++ Atcoder/ABC088/A.cs
@@ -1,10 +1,22 @@
-﻿namespace Atcoder.ABC088
+﻿using System;
+
+namespace Atcoder.ABC088
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var N = int.Parse(Console.ReadLine());
+            var A = int.Parse(Console.ReadLine());
+            var remain = N % 500;
+            if (remain <= A)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
--- Atcoder/ABC088/B.cs
+++ Atcoder/ABC088/B.cs
@@ -1,9 +1,33 @@
-﻿namespace Atcoder.ABC088
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC088
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            bool isAlice = true;
+            int alicePoint = 0;
+            int bobPoint = 0;
+            var points = Console.ReadLine().Split(' ').Select(int.Parse).OrderByDescending(x => x).ToArray();
+
+            for (int i = 0; i < N; i++)
+            {
+                int tmpPoint = points[i];
+                if (isAlice)
+                {
+                    alicePoint += tmpPoint;
+                    isAlice = false;
+                }
+                else
+                {
+                    bobPoint += tmpPoint;
+                    isAlice = true;
+                }
+            }
+            Console.WriteLine(alicePoint - bobPoint);
         }
     }
 }
