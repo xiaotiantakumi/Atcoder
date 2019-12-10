ac724a58380d4698444f07e6402997c91cfe9233
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-10 12:35:47 +0900

    ABC089のAB回答

--- Atcoder/ABC089/A.cs
+++ Atcoder/ABC089/A.cs
@@ -1,10 +1,13 @@
-﻿namespace Atcoder.ABC089
+﻿using System;
+
+namespace Atcoder.ABC089
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var r = int.Parse(Console.ReadLine());
+            Console.WriteLine(r / 3);
         }
     }
 }
--- Atcoder/ABC089/B.cs
+++ Atcoder/ABC089/B.cs
@@ -1,9 +1,23 @@
-﻿namespace Atcoder.ABC089
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC089
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var r = int.Parse(Console.ReadLine());
+            var l = Console.ReadLine().Split(' ');
+            if (l.Any(x => x == "Y"))
+            {
+                Console.WriteLine("Four");
+            }
+            else
+            {
+                Console.WriteLine("Three");
+
+            }
         }
     }
 }
