bac41335720f6d2724fd349f8ff4882080b6d152
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-13 20:43:54 +0900

    A回答

--- Atcoder/ABC001/A.cs
+++ Atcoder/ABC001/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC001
+﻿using System;
+
+namespace Atcoder.ABC001
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var H1 = int.Parse(Console.ReadLine());
+            var H2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(H1 - H2);
         }
     }
 }
