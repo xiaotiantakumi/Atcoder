1131c24a600888f8f508f461e3c4446f9180dea4
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-16 06:19:42 +0900

    ABC086のAB回答

--- Atcoder/ABC086/A.cs
+++ Atcoder/ABC086/A.cs
@@ -1,10 +1,21 @@
-﻿namespace Atcoder.ABC086
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC086
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var r = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            if ((r[0] * r[1]) % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
         }
     }
 }
--- Atcoder/ABC086/B.cs
+++ Atcoder/ABC086/B.cs
@@ -1,9 +1,21 @@
-﻿namespace Atcoder.ABC086
+﻿using System;
+
+namespace Atcoder.ABC086
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var r = int.Parse(Console.ReadLine().Replace(" ", string.Empty));
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (r == i * i)
+                {
+                    Console.WriteLine("Yes");
+                    return;
+                }
+            }
+            Console.WriteLine("No");
         }
     }
 }
