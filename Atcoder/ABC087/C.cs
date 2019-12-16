e51ccc60581cf94647f1b2f5eefc6265ac5acfc0
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-17 06:49:48 +0900

    ABC087のC回答

--- Atcoder/ABC087/C.cs
+++ Atcoder/ABC087/C.cs
@@ -1,9 +1,25 @@
-﻿namespace Atcoder.ABC087
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC087
 {
     public class C : IAtcoder
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var r1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var r2 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int ret = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                var sum = r1.Take(i).Sum() + r2.Skip(i - 1).Take(N - i + 1).Sum();
+                if (ret < sum)
+                {
+                    ret = sum;
+                }
+            }
+            Console.WriteLine(ret);
         }
     }
 }
