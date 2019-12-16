b642b5115bd13275176c5c9ae6cf7cd9300f3007
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-16 12:42:19 +0900

    ABC086のC回答

--- Atcoder/ABC086/C.cs
+++ Atcoder/ABC086/C.cs
@@ -1,9 +1,56 @@
-﻿namespace Atcoder.ABC086
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC086
 {
     public class C : IAtcoder
     {
         public void Run()
         {
+            var points = int.Parse(Console.ReadLine());
+            bool flg = true;
+            int fromX = 0;
+            int fromY = 0;
+            int startTime = 0;
+            for (int i = 0; i < points; i++)
+            {
+                var r = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                flg = CanArrive(fromX, fromY, r[1], r[2], startTime, r[0]);
+                fromX = r[1];
+                fromY = r[2];
+                startTime = r[0];
+
+
+                if (!flg)
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+            }
+            Console.WriteLine("Yes");
+        }
+        private static bool CanArrive(int fromX, int fromY, int toX, int toY, int startTime, int endTime)
+        {
+            var difTime = endTime - startTime;
+            var difX = Math.Abs(toX - fromX);
+            var difY = Math.Abs(toY - fromY);
+            if (difTime < difX + difY)
+            {
+                return false;
+            }
+
+            if (difTime == difX + difY)
+            {
+                return true;
+            }
+
+            if (difTime % 2 == 1 && (difX + difY) % 2 == 1 ||
+                difTime % 2 == 0 && (difX + difY) % 2 == 0)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
