6380e526b4cdbee5c5deb431e9588b5fdd57374e
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-17 12:35:33 +0900

    ABC085のAB回答

--- Atcoder/ABC085/A.cs
+++ Atcoder/ABC085/A.cs
@@ -1,10 +1,13 @@
-﻿namespace Atcoder.ABC085
+﻿using System;
+
+namespace Atcoder.ABC085
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var S = Console.ReadLine();
+            Console.WriteLine(S.Replace("2017", "2018"));
         }
     }
 }
--- Atcoder/ABC085/B.cs
+++ Atcoder/ABC085/B.cs
@@ -1,9 +1,29 @@
-﻿namespace Atcoder.ABC085
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atcoder.ABC085
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            List<int> mochis = new List<int>();
+            for (int i = 0; i < N; i++)
+            {
+                mochis.Add(int.Parse(Console.ReadLine()));
+            }
+            mochis.Sort();
+            int ret = 1;
+            for (int i = 0; i < mochis.Count - 1; i++)
+            {
+                if (mochis[i] < mochis[i + 1])
+                {
+                    ret++;
+
+                }
+            }
+            Console.WriteLine(ret);
         }
     }
 }
