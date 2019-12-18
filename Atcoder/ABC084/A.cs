ea8d27781e3ecae77e4aafa94805ff642caf7b83
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-18 20:02:13 +0900

    ABC084のAB回答

--- Atcoder/ABC084/A.cs
+++ Atcoder/ABC084/A.cs
@@ -1,10 +1,13 @@
-﻿namespace Atcoder.ABC084
+﻿using System;
+
+namespace Atcoder.ABC084
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var M = int.Parse(Console.ReadLine());
+            Console.WriteLine(48 - M);
         }
     }
 }
--- Atcoder/ABC084/B.cs
+++ Atcoder/ABC084/B.cs
@@ -1,9 +1,35 @@
-﻿namespace Atcoder.ABC084
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC084
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var S = Console.ReadLine();
+            int tmp = 0;
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[r[0]] != '-')
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+
+                if (i == r[0])
+                {
+                    continue;
+                }
+
+                if (!int.TryParse(S[i].ToString(), out tmp))
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+            }
+            Console.WriteLine("Yes");
         }
     }
 }
