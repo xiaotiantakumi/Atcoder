5e9bfa34f404db058fffdb013751a3564e69203f
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-19 10:06:22 +0900

    ABC083のAB回答

--- Atcoder/ABC083/A.cs
+++ Atcoder/ABC083/A.cs
@@ -1,10 +1,25 @@
-﻿namespace Atcoder.ABC083
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC083
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            if (r[0] + r[1] > r[2] + r[3])
+            {
+                Console.WriteLine("Left");
+            }
+            else if (r[0] + r[1] < r[2] + r[3])
+            {
+                Console.WriteLine("Right");
+            }
+            else
+            {
+                Console.WriteLine("Balanced");
+            }
         }
     }
 }
--- Atcoder/ABC083/B.cs
+++ Atcoder/ABC083/B.cs
@@ -1,9 +1,32 @@
-﻿namespace Atcoder.ABC083
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC083
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var N = r[0];
+            var A = r[1];
+            var B = r[2];
+            int ret = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                var tmp = 0;
+                var val = i.ToString();
+                for (int j = 0; j < val.Length; j++)
+                {
+                    tmp += int.Parse(val[j].ToString());
+                }
+
+                if (tmp >= A && tmp <= B)
+                {
+                    ret += i;
+                }
+            }
+            Console.WriteLine(ret);
         }
     }
 }
