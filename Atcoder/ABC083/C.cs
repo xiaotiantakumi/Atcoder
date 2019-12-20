2a54ec3c2933e46a36d28e588751c1588a722855
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-20 12:20:42 +0900

    ABC083のC回答

--- Atcoder/ABC083/C.cs
+++ Atcoder/ABC083/C.cs
@@ -1,9 +1,37 @@
-﻿namespace Atcoder.ABC083
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC083
 {
     public class C : IAtcoder
     {
         public void Run()
         {
+            var r = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            int i = 0;
+            int ret = 0;
+            long tmp = 0;
+
+            while (r[1] >= tmp)
+            {
+                ret++;
+                if (i == 0)
+                {
+                    tmp = r[0];
+                    i++;
+                }
+                else
+                {
+                    tmp *= 2;
+                    i++;
+                }
+            }
+
+            if (ret > 0)
+            {
+                ret--;
+            }
+            Console.WriteLine(ret);
         }
     }
 }
