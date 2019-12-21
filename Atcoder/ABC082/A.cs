c7805d5a7f1cd1413456901722af2b4ec1ac2517
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-21 13:56:54 +0900

    ABC082のAB回答

--- Atcoder/ABC082/A.cs
+++ Atcoder/ABC082/A.cs
@@ -1,10 +1,15 @@
-﻿namespace Atcoder.ABC082
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC082
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var sum = r[0] + r[1];
+            Console.WriteLine((sum + 2 - 1) / 2);
         }
     }
 }
--- Atcoder/ABC082/B.cs
+++ Atcoder/ABC082/B.cs
@@ -1,9 +1,22 @@
-﻿namespace Atcoder.ABC082
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC082
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var s = new string(Console.ReadLine().OrderBy(x => x).ToArray());
+            var t = new string(Console.ReadLine().OrderByDescending(x => x).ToArray());
+            if (s.CompareTo(t) < 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
