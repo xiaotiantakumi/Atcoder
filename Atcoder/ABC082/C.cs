704f97abd2c0a5e841900a1e9470677ac04ca1e4
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-21 14:17:22 +0900

    ABC082のC回答

--- Atcoder/ABC082/C.cs
+++ Atcoder/ABC082/C.cs
@@ -1,9 +1,42 @@
-﻿namespace Atcoder.ABC082
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC082
 {
     public class C : IAtcoder
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            int rmCount = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (dic.ContainsKey(r[i]))
+                {
+                    if (dic[r[i]] >= r[i])
+                    {
+                        rmCount++;
+                        continue;
+                    }
+                    dic[r[i]]++;
+                }
+                else
+                {
+                    dic.Add(r[i], 1);
+                }
+            }
+
+            foreach (var item in dic)
+            {
+                if (item.Key > item.Value)
+                {
+                    rmCount += item.Value;
+                }
+            }
+            Console.WriteLine(rmCount);
         }
     }
 }
