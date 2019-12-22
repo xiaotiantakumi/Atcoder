1fcf07f5aa7d617f56b476c8a3a0dd93fd2975f5
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-22 18:10:20 +0900

    ABC095のC回答

--- Atcoder/ABC095/C.cs
+++ Atcoder/ABC095/C.cs
@@ -13,51 +13,16 @@ namespace Atcoder.ABC095
             var C = r[2];
             var X = r[3];
             var Y = r[4];
-            int p = 0;
+            int p1 = 0;
+            int p2 = 0;
+            int p3 = 0;
 
-            if (A + B <= 2 * C)
-            {
-                Console.WriteLine(X * A + Y * B);
-            }
-            else if (A >= 2 * C &&
-                     B >= 2 * C)
-            {
-                Console.WriteLine(Math.Max(X, Y) * C * 2);
-            }
-            else if (A >= 2 * C &&
-                     B <= 2 * C)
-            {
-                p += X * C * 2;
-                if (B - A > 0)
-                {
-                    p += (B - A) * Y;
-                }
-                Console.WriteLine(p);
-            }
-            else if (A <= 2 * C &&
-                     B >= 2 * C)
-            {
-                p += Y * C * 2;
-                if (A - B > 0)
-                {
-                    p += (A - B) * X;
-                }
-                Console.WriteLine(p);
-            }
-            else
-            {
-                if (X - Y < 0)
-                {
-                    p += (Y - X) * B;
-                    p += X * 2 * C;
-                }
-                else
-                {
-                    p += (X - Y) * A;
-                    p += Y * 2 * C;
-                }
-                Console.WriteLine(p);
-            }
+            p1 = Math.Max(X, Y) * 2 * C;
+            p2 = A * X + B * Y;
+            var tmp = X < Y ? B : A;
+            p3 = 2 * C * Math.Min(X, Y) + Math.Abs(X - Y) * tmp;
+            var ret = Math.Min(Math.Min(p1, p2), p3);
+            Console.WriteLine(ret);
         }
     }
 }
