cace38cebae8cb26de9c44de2044f0bb0414d36c
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-25 18:17:40 +0900

    ABC110C途中

--- Atcoder/ABC110/C.cs
+++ Atcoder/ABC110/C.cs
@@ -1,9 +1,35 @@
-﻿namespace Atcoder.ABC110
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atcoder.ABC110
 {
     public class C : IAtcoder
     {
         public void Run()
         {
+            var S = Console.ReadLine();
+            var T = Console.ReadLine();
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (dic.ContainsKey(S[i].ToString()))
+                {
+                    if (dic[S[i].ToString()] != T[i].ToString())
+                    {
+                        Console.WriteLine("No");
+                        return;
+                    }
+                }
+                else
+                {
+                    dic.Add(S[i].ToString(), T[i].ToString());
+                    if (S[i] != T[i] && !dic.ContainsKey(T[i].ToString()))
+                    {
+                        dic.Add(T[i].ToString(), S[i].ToString());
+                    }
+                }
+            }
+            Console.WriteLine("Yes");
         }
     }
 }
