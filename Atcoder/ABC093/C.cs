b83e09a6625e5f7885af029f84b9e4b59a953d26
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-25 16:17:46 +0900

    ABC093のC回答

--- Atcoder/ABC093/C.cs
+++ Atcoder/ABC093/C.cs
@@ -1,9 +1,29 @@
-﻿namespace Atcoder.ABC093
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC093
 {
     public class C : IAtcoder
     {
         public void Run()
         {
+            var r = Console.ReadLine().Split().Select(int.Parse).OrderBy(x => x).ToArray();
+            int ret = 0;
+            while (r[0] != r[1] || r[0] != r[2])
+            {
+                if (r[0] == r[1])
+                {
+                    ret += r[2] - r[0];
+                    break;
+                }
+                else
+                {
+                    ret++;
+                    r[0] += 2;
+                    r = r.OrderBy(x => x).ToArray();
+                }
+            }
+            Console.WriteLine(ret);
         }
     }
 }
