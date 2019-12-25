08f48eb991b64a2be76504cd50a89d2a2cd2c82b
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-25 16:34:23 +0900

    ABC110のB回答

--- Atcoder/ABC110/B.cs
+++ Atcoder/ABC110/B.cs
@@ -7,41 +7,22 @@ namespace Atcoder.ABC110
     {
         public void Run()
         {
-            var input = Console.ReadLine().Split(' ').Select(int.Parse).OrderBy(x => x).ToArray();
-            var N = input[0];
-            var M = input[1];
-            var X = input[2];
-            var Y = input[3];
-            var Xs = Console.ReadLine().Split(' ').Select(int.Parse).OrderBy(x => x).ToArray();
-            var Ys = Console.ReadLine().Split(' ').Select(int.Parse).OrderBy(x => x).ToArray();
-
-            int Z = int.MinValue;
-
-            if (X + 1 > Xs.Max())
+            var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var X = r[2];
+            var Y = r[3];
+            var xs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var ys = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var maxX = xs.Max();
+            var minY = ys.Min();
+            for (int i = X + 1; i <= Y; i++)
             {
-
-            }
-            int from = Math.Min(X + 1, Xs.Max());
-            int end = Math.Max(Y, Xs.Min());
-            // はじめi = Xとしていがが、これだと条件X<Z≤Yを満たさず、X≤Z≤Yとなってしまう。
-            for (int i = X + 1; i < Y; i++)
-            {
-                // x1,x2,...,xN<Z
-                // y1,y2,...,yM≥Zの条件
-                if (Xs.Max() < i && Ys.Min() <= i)
+                if (maxX < i && minY >= i)
                 {
-                    Z = i;
-                    break;
+                    Console.WriteLine("No War");
+                    return;
                 }
             }
-
-            if (Z == int.MinValue)
-            {
-                Console.WriteLine("War");
-                return;
-            }
-
-            Console.WriteLine("No War");
+            Console.WriteLine("War");
         }
     }
 }
