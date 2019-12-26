d7d0b4a45eeb8317a91db57531cdf07903faed11
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-27 03:34:27 +0900

    フォルダ追加

---
+++ Atcoder/ABC041/B.cs
@@ -0,0 +1,9 @@
+﻿namespace Atcoder.ABC041
+{
+    public class B : IAtcoder
+    {
+        public void Run()
+        {
+        }
+    }
+}
---
+++ Atcoder/ABC041/C.cs
@@ -0,0 +1,9 @@
+﻿namespace Atcoder.ABC041
+{
+    public class C : IAtcoder
+    {
+        public void Run()
+        {
+        }
+    }
+}
