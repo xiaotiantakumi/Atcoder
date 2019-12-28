5e0e23a007c0fca983aeaf0aa8e7c360c5dcbe6e
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-28 19:55:29 +0900

    ABC080のAB回答

--- Atcoder/ABC080/A.cs
+++ Atcoder/ABC080/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC080
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC080
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(Math.Min(r[0] * r[1], r[2]));
         }
     }
 }
--- Atcoder/ABC080/B.cs
+++ Atcoder/ABC080/B.cs
@@ -1,9 +1,26 @@
-﻿namespace Atcoder.ABC080
+﻿using System;
+
+namespace Atcoder.ABC080
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            int val = 0;
+            foreach (var c in N.ToString())
+            {
+                val += int.Parse(c.ToString());
+            }
+
+            if (N % val == 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
