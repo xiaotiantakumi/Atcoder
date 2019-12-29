8dbed9bd2675a3ff0f954a20741b7d35755eb882
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-29 11:31:59 +0900

    ABC079のB_なぜかうまくいかない。。

--- Atcoder/ABC079/B.cs
+++ Atcoder/ABC079/B.cs
@@ -1,9 +1,34 @@
-﻿namespace Atcoder.ABC079
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC079
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            Queue<long> queue = new Queue<long>();
+            int counter = 0;
+            while (counter != N)
+            {
+                if (counter == 0)
+                {
+                    queue.Enqueue(2);
+                }
+                else if (counter == 1)
+                {
+                    queue.Enqueue(1);
+                }
+                else
+                {
+                    queue.Enqueue(queue.Sum());
+                    queue.Dequeue();
+                }
+                counter++;
+            }
+            Console.WriteLine(queue.Sum());
         }
     }
 }
