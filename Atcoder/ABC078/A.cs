ba843ec6d614451b1c5140a5b81b2ee738fb3723
Takumi Oda <xiaotiantakumi@gmail.com>
2019-12-31 22:36:44 +0900

    ABC078のA回答

--- Atcoder/ABC078/A.cs
+++ Atcoder/ABC078/A.cs
@@ -1,10 +1,26 @@
-﻿namespace Atcoder.ABC078
+﻿using System;
+
+namespace Atcoder.ABC078
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var r = Console.ReadLine().Split();
+            if (r[0].CompareTo(r[1]) == 0)
+            {
+                Console.WriteLine("=");
+            }
+            else if (r[0].CompareTo(r[1]) < 0)
+            {
+                Console.WriteLine("<");
+
+            }
+            else
+            {
+                Console.WriteLine(">");
+
+            }
         }
     }
 }
