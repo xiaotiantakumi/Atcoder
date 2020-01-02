4c2f97bcda63abb27e650785768dbd2e27f4fa65
Takumi Oda <xiaotiantakumi@gmail.com>
2020-01-02 14:11:09 +0900

    ABC077のAB回答

--- Atcoder/ABC077/A.cs
+++ Atcoder/ABC077/A.cs
@@ -1,10 +1,21 @@
-﻿namespace Atcoder.ABC077
+﻿using System;
+
+namespace Atcoder.ABC077
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var r1 = Console.ReadLine();
+            var r2 = Console.ReadLine();
+            if (r1[0] == r2[2] && r1[1] == r2[1] && r1[2] == r2[0])
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
         }
     }
 }
--- Atcoder/ABC077/B.cs
+++ Atcoder/ABC077/B.cs
@@ -1,9 +1,15 @@
-﻿namespace Atcoder.ABC077
+﻿using System;
+
+namespace Atcoder.ABC077
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var tmp = Math.Sqrt(N);
+            var val = Math.Truncate(tmp);
+            Console.WriteLine(val * val);
         }
     }
 }
