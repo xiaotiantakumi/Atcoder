18eacf4dfa7c10b8dd8533f83464676bfd4177c0
Takumi Oda <xiaotiantakumi@gmail.com>
2020-01-04 22:39:41 +0900

    ABC132のC回答

--- Atcoder/ABC132/C.cs
+++ Atcoder/ABC132/C.cs
@@ -7,6 +7,11 @@ namespace Atcoder.ABC132
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var r = Console.ReadLine().Split().Select(int.Parse).OrderBy(x => x).ToArray();
+            var startVal = r[N / 2 - 1];
+            var endVal = r[N / 2];
+            Console.WriteLine(endVal - startVal);
         }
     }
 }
