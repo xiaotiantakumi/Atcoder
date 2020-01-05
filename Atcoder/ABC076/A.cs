92fcb17627ec48df1c0c74df3ff6b82a39329c50
Takumi Oda <xiaotiantakumi@gmail.com>
2020-01-05 18:15:11 +0900

    ABC076のA回答

--- Atcoder/ABC076/A.cs
+++ Atcoder/ABC076/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC076
+﻿using System;
+
+namespace Atcoder.ABC076
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var R = int.Parse(Console.ReadLine());
+            var G = int.Parse(Console.ReadLine());
+            Console.WriteLine(2 * G - R);
         }
     }
 }
