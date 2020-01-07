bec3ea9ca2136289cb1d223f59447b3f5dd71e9a
Takumi Oda <xiaotiantakumi@gmail.com>
2020-01-07 12:36:25 +0900

    ABC075のA回答

--- Atcoder/ABC075/A.cs
+++ Atcoder/ABC075/A.cs
@@ -1,10 +1,25 @@
-﻿namespace Atcoder.ABC075
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC075
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            if (r[0] == r[1])
+            {
+                Console.WriteLine(r[2]);
+            }
+            else if (r[0] == r[2])
+            {
+                Console.WriteLine(r[1]);
+            }
+            else
+            {
+                Console.WriteLine(r[0]);
+            }
         }
     }
 }
