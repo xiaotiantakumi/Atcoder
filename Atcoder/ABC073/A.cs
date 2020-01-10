86e3188ca7acf33287a857b292ae612145af7b70
Takumi Oda <xiaotiantakumi@gmail.com>
2020-01-10 12:23:51 +0900

    ABC073のAB回答

--- Atcoder/ABC073/A.cs
+++ Atcoder/ABC073/A.cs
@@ -1,10 +1,21 @@
-﻿namespace Atcoder.ABC073
+﻿using System;
+
+namespace Atcoder.ABC073
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var r = Console.ReadLine();
+            foreach (var c in r)
+            {
+                if (c == '9')
+                {
+                    Console.WriteLine("Yes");
+                    return;
+                }
+            }
+            Console.WriteLine("No");
         }
     }
 }
--- Atcoder/ABC073/B.cs
+++ Atcoder/ABC073/B.cs
@@ -1,9 +1,23 @@
-﻿namespace Atcoder.ABC073
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC073
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            int ret = 0;
+            for (int i = 0; i < N; i++)
+            {
+                var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = r[0]; j <= r[1]; j++)
+                {
+                    ret++;
+                }
+            }
+            Console.WriteLine(ret);
         }
     }
 }
