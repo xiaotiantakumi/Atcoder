ea9d2044ce790faa579e92ccae44aa06b84f1052
Takumi Oda <xiaotiantakumi@gmail.com>
2020-01-10 12:36:33 +0900

    ABC073のC回答

--- Atcoder/ABC073/C.cs
+++ Atcoder/ABC073/C.cs
@@ -1,9 +1,36 @@
-﻿namespace Atcoder.ABC073
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC073
 {
     public class C : IAtcoder
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < N; i++)
+            {
+                var r = int.Parse(Console.ReadLine());
+                if (!dic.ContainsKey(r))
+                {
+                    dic[r] = 1;
+                }
+                else
+                {
+                    if (dic[r] == 1)
+                    {
+                        dic[r] = 2;
+                    }
+                    else
+                    {
+                        dic[r] = 1;
+                    }
+
+                }
+            }
+            Console.WriteLine(dic.Values.Where(x => x % 2 != 0).Sum());
         }
     }
 }
