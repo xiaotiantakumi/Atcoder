0bad7d92ad413846c1e29d59c29e8d2217c2d563
Takumi Oda <xiaotiantakumi@gmail.com>
2020-01-13 23:29:32 +0900

    ABC151のA回答

--- Atcoder/ABC151/A.cs
+++ Atcoder/ABC151/A.cs
@@ -1,10 +1,15 @@
-﻿namespace Atcoder.ABC151
+﻿using System;
+
+namespace Atcoder.ABC151
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var array = "abcdefghijklmnopqrstuvwxyz";
+            var r = Console.ReadLine();
+            var nowPos = array.IndexOf(r);
+            Console.WriteLine(array[nowPos + 1]);
         }
     }
 }
