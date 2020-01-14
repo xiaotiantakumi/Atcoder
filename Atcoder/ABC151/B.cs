a87a90d53f0bb8be42b7aa050066fa87dfcc7e1c
Takumi Oda <xiaotiantakumi@gmail.com>
2020-01-14 19:25:15 +0900

    ABC151のB回答

--- Atcoder/ABC151/B.cs
+++ Atcoder/ABC151/B.cs
@@ -1,9 +1,27 @@
-﻿namespace Atcoder.ABC151
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC151
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var r1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var r2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var last = r1[2] * r1[0] - (r2.Sum());
+            if (last <= 0)
+            {
+                Console.WriteLine("0");
+            }
+            else if (last > r1[1])
+            {
+                Console.WriteLine("-1");
+            }
+            else
+            {
+                Console.WriteLine(last);
+            }
         }
     }
 }
