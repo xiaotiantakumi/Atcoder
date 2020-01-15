506b545e6004d4929ba0b0f3942aeac3282676f5
Takumi Oda <xiaotiantakumi@gmail.com>
2020-01-15 12:35:15 +0900

    第6回 ドワンゴからの挑戦状_A回答

---
+++ Atcoder/OtherRatedCon/Num6Dowango/B.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.OtherRatedCon.Num6Dowango
+{
+    public class B : IAtcoder
+    {
+        public void Run()
+        {
+        }
+    }
+}
---
+++ Atcoder/OtherRatedCon/Num6Dowango/C.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.OtherRatedCon.Num6Dowango
+{
+    public class C : IAtcoder
+    {
+        public void Run()
+        {
+        }
+    }
+}
