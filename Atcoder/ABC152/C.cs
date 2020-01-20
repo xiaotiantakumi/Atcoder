e7e98da929dd10692a647842926cf445af5ead96
Takumi Oda <xiaotiantakumi@gmail.com>
2020-01-20 12:41:57 +0900

    ABC152のC回答

--- Atcoder/ABC152/C.cs
+++ Atcoder/ABC152/C.cs
@@ -1,9 +1,25 @@
-﻿namespace Atcoder.ABC152
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC152
 {
     public class C : IAtcoder
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int ret = 0;
+            int min = N + 1;
+            for (int i = 0; i < N; i++)
+            {
+                if (r[i] < min)
+                {
+                    ret++;
+                    min = r[i];
+                }
+            }
+            Console.WriteLine(ret);
         }
     }
 }
