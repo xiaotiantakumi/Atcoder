6a29db0c326be065739de218cdb6ecba88b049bf
Takumi Oda <xiaotiantakumi@gmail.com>
2020-01-23 07:51:47 +0900

    学習記録

---
+++ Atcoder/MyTest/20200123.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.MyTest
+{
+    public class A20200123 : IAtcoder
+    {
+        public void Run()
+        {
+            var tmp = Combination(3);
+        }
+
+        private static string Combination(int val)
+        {
+            if (val == 0)
+            {
+                return string.Empty;
+            }
+            string ret = string.Empty;
+            return val.ToString() + Combination(val - 1);
+
+        }
+    }
+}
