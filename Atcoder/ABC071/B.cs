d208ad7de1c78bc17b41791de077820f57b889cb
Takumi Oda <xiaotiantakumi@gmail.com>
2020-01-25 08:05:01 +0900

    ABC071のB問題回答

--- Atcoder/ABC071/B.cs
+++ Atcoder/ABC071/B.cs
@@ -1,9 +1,30 @@
-﻿namespace Atcoder.ABC071
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC071
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var allStr = "abcdefghijklmnopqrstuvwxyz";
+            var r = Console.ReadLine();
+            foreach (var c in r)
+            {
+                var index = allStr.IndexOf(c);
+                if (index >= 0)
+                {
+                    allStr = allStr.Remove(index, 1);
+                }
+            }
+            if (string.IsNullOrEmpty(allStr))
+            {
+                Console.WriteLine("None");
+            }
+            else
+            {
+                Console.WriteLine(allStr.FirstOrDefault());
+            }
         }
     }
 }
