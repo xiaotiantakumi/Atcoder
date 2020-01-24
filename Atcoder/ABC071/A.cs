f61dcc8c7a6121b92394ff3d7d0a36d0f351a236
Takumi Oda <xiaotiantakumi@gmail.com>
2020-01-24 22:50:37 +0900

    ABC071のA回答

--- Atcoder/ABC071/A.cs
+++ Atcoder/ABC071/A.cs
@@ -1,10 +1,23 @@
-﻿namespace Atcoder.ABC071
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC071
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var a = Math.Abs(r[0] - r[1]);
+            var b = Math.Abs(r[0] - r[2]);
+            if (a < b)
+            {
+                Console.WriteLine("A");
+            }
+            else
+            {
+                Console.WriteLine("B");
+            }
         }
     }
 }
