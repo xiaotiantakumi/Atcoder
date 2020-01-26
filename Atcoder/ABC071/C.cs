2b57b56a34caa29e1324a4de50e1208dee3a5322
Takumi Oda <xiaotiantakumi@gmail.com>
2020-01-26 15:27:52 +0900

    ABC071のC回答

--- Atcoder/ABC071/C.cs
+++ Atcoder/ABC071/C.cs
@@ -1,9 +1,30 @@
-﻿namespace Atcoder.ABC071
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC071
 {
     public class C : IAtcoder
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var r = Console.ReadLine().Split().Select(long.Parse).OrderByDescending(x => x).ToArray();
+            var ret = new List<long>();
+            for (int i = 0; i < N - 1; i++)
+            {
+                if (r[i] == r[i + 1])
+                {
+                    ret.Add(r[i]);
+                    i++;
+                    if (ret.Count == 2)
+                    {
+                        Console.WriteLine(ret[0] * ret[1]);
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine(0);
         }
     }
 }
