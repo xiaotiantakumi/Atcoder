f58b06c89941b29694af33fdb1e54dcc985d75a2
Takumi Oda <xiaotiantakumi@gmail.com>
2020-01-31 21:19:14 +0900

    ABC067のC回答

--- Atcoder/ABC067/C.cs
+++ Atcoder/ABC067/C.cs
@@ -1,9 +1,29 @@
-﻿namespace Atcoder.ABC067
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC067
 {
     public class C : IAtcoder
     {
         public void Run()
         {
+            var N = long.Parse(Console.ReadLine());
+            var r = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+            var sum = r.Sum();
+
+            long left = 0;
+            long ret = long.MaxValue;
+
+            for (long i = 0; i < N - 1; i++)
+            {
+                left += r[i];
+                var right = sum - left;
+                if (Math.Abs(left - right) < ret)
+                {
+                    ret = Math.Abs(left - right);
+                }
+            }
+            Console.WriteLine(ret);
         }
     }
 }
