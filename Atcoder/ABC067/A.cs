0ff2172ea0476e954418f0f1210886736bb7d252
Takumi Oda <xiaotiantakumi@gmail.com>
2020-01-31 12:28:54 +0900

    ABC067のAB回答

--- Atcoder/ABC067/A.cs
+++ Atcoder/ABC067/A.cs
@@ -1,10 +1,23 @@
-﻿namespace Atcoder.ABC067
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC067
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var r = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            if (r[0] % 3 == 0 ||
+                r[1] % 3 == 0 ||
+                (r[0] + r[1]) % 3 == 0)
+            {
+                Console.WriteLine("Possible");
+            }
+            else
+            {
+                Console.WriteLine("Impossible");
+            }
         }
     }
 }
--- Atcoder/ABC067/B.cs
+++ Atcoder/ABC067/B.cs
@@ -1,9 +1,15 @@
-﻿namespace Atcoder.ABC067
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC067
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var r = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var ret = Console.ReadLine().Split(' ').Select(int.Parse).OrderByDescending(x => x).Take(r[1]).Sum();
+            Console.WriteLine(ret);
         }
     }
 }
