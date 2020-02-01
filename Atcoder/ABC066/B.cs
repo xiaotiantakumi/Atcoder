5250a0c4fd428808b1926c40146a2fff57f126ff
Takumi Oda <xiaotiantakumi@gmail.com>
2020-02-01 21:51:21 +0900

    ABC066のB回答

--- Atcoder/ABC066/B.cs
+++ Atcoder/ABC066/B.cs
@@ -1,9 +1,21 @@
-﻿namespace Atcoder.ABC066
+﻿using System;
+
+namespace Atcoder.ABC066
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var S = Console.ReadLine();
+            for (int i = S.Length - 2; i >= 0; i -= 2)
+            {
+                var halfPos = i / 2;
+                if (S.Substring(0, halfPos) == S.Substring(halfPos, halfPos))
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
         }
     }
 }
