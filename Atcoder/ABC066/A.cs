54e89199c1c11db18107ba48342bb8af64ae63be
Takumi Oda <xiaotiantakumi@gmail.com>
2020-02-01 21:40:17 +0900

    ABC066のA回答

--- Atcoder/ABC066/A.cs
+++ Atcoder/ABC066/A.cs
@@ -1,10 +1,13 @@
-﻿namespace Atcoder.ABC066
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC066
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            Console.WriteLine(Console.ReadLine().Split().Select(int.Parse).OrderBy(x => x).Take(2).Sum());
         }
     }
 }
