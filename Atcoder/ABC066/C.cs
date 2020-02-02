c6a7f3b1a376435df744939423302b35912eb6d1
Takumi Oda <xiaotiantakumi@gmail.com>
2020-02-02 13:25:45 +0900

    ABC066のC回答

--- Atcoder/ABC066/C.cs
+++ Atcoder/ABC066/C.cs
@@ -1,9 +1,45 @@
-﻿namespace Atcoder.ABC066
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC066
 {
     public class C : IAtcoder
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var r = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            if (N == 1)
+            {
+                Console.WriteLine(r[0]);
+                return;
+            }
+
+            List<int> ret = new List<int>();
+
+            bool flg = false;
+            for (int i = 0; i < N; i++)
+            {
+                if (flg)
+                {
+                    ret.Add(r[i]);
+                }
+                else
+                {
+                    ret.Insert(0, r[i]);
+                }
+                flg = !flg;
+            }
+            if (N % 2 != 0)
+            {
+                Console.WriteLine(string.Join(" ", ret));
+            }
+            else
+            {
+                ret.Reverse();
+                Console.WriteLine(string.Join(" ", ret));
+            }
         }
     }
 }
