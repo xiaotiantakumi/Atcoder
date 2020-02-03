0d9e7d060173784758a137d44499bcdea7747c3d
Takumi Oda <xiaotiantakumi@gmail.com>
2020-02-03 12:40:43 +0900

    ABC065のA回答

--- Atcoder/ABC065/A.cs
+++ Atcoder/ABC065/A.cs
@@ -1,10 +1,29 @@
-﻿namespace Atcoder.ABC065
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC065
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var r = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var X = r[0];
+            var A = r[1];
+            var B = r[2];
+            var over = B - A;
+            if (over <= 0)
+            {
+                Console.WriteLine("delicious");
+            }
+            else if (X >= over)
+            {
+                Console.WriteLine("safe");
+            }
+            else
+            {
+                Console.WriteLine("dangerous");
+            }
         }
     }
 }
