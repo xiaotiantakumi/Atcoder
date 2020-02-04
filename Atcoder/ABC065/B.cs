82a2b9b464eb132c04a0fe72966b4ddf6c754dbc
Takumi Oda <xiaotiantakumi@gmail.com>
2020-02-04 21:29:17 +0900

    ABC065のB回答

--- Atcoder/ABC065/B.cs
+++ Atcoder/ABC065/B.cs
@@ -1,9 +1,30 @@
-﻿namespace Atcoder.ABC065
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atcoder.ABC065
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            Dictionary<int, int> input = new Dictionary<int, int>();
+            for (int i = 0; i < N; i++)
+            {
+                input.Add(i + 1, int.Parse(Console.ReadLine()));
+            }
+
+            int buttonPos = 1;
+            for (int i = 0; i < N; i++)
+            {
+                buttonPos = input[buttonPos];
+                if (buttonPos == 2)
+                {
+                    Console.WriteLine(i + 1);
+                    return;
+                }
+            }
+            Console.WriteLine("-1");
         }
     }
 }
