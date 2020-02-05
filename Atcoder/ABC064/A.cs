f22416a87bb2be0210d9a870bd874d8084cfd0d8
Takumi Oda <xiaotiantakumi@gmail.com>
2020-02-05 12:42:36 +0900

    ABC064のAB回答

--- Atcoder/ABC064/A.cs
+++ Atcoder/ABC064/A.cs
@@ -1,10 +1,20 @@
-﻿namespace Atcoder.ABC064
+﻿using System;
+
+namespace Atcoder.ABC064
 {
     public class A : IAtcoder
     {
         public void Run()
         {
-            
+            var r = int.Parse(Console.ReadLine().Replace(" ", string.Empty));
+            if (r % 4 == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
         }
     }
 }
--- Atcoder/ABC064/B.cs
+++ Atcoder/ABC064/B.cs
@@ -1,9 +1,15 @@
-﻿namespace Atcoder.ABC064
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC064
 {
     public class B : IAtcoder
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var points = Console.ReadLine().Split().Select(int.Parse).OrderBy(x => x).ToArray();
+            Console.WriteLine(points.Max() - points.Min());
         }
     }
 }
