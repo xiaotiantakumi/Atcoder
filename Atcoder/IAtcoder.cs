2cb7bbfe2a129a583bda3f3490ada627ab00a4c2
Takumi Oda <xiaotiantakumi@gmail.com>
2020-02-06 12:34:16 +0900

    入出力のメソッドを追加

--- Atcoder/IAtcoder.cs
+++ Atcoder/IAtcoder.cs
@@ -1,7 +1,20 @@
-﻿namespace Atcoder
+﻿using System;
+using System.Linq;
+
+namespace Atcoder
 {
     internal interface IAtcoder
     {
         void Run();
+    }
+    public class AtcoderBase : IAtcoder
+    {
+        public virtual void Run()
+        {
+        }
+        public static readonly Func<string> Read = () => Console.ReadLine();
+        public static readonly Func<string[]> ReadStringArray = () => Console.ReadLine().Split();
+        public static readonly Func<int[]> ReadIntArray = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
+        public static readonly Func<long[]> ReadLongArray = () => Console.ReadLine().Split().Select(long.Parse).ToArray();
     }
 }
