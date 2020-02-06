af0594c67ca03cc9b74ab652513831fe22bbd73f
Takumi Oda <xiaotiantakumi@gmail.com>
2020-02-06 12:18:20 +0900

    ABC064C途中

--- Atcoder/ABC064/C.cs
+++ Atcoder/ABC064/C.cs
@@ -1,9 +1,40 @@
-﻿namespace Atcoder.ABC064
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC064
 {
     public class C : IAtcoder
     {
         public void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int min = 0;
+            int max = 0;
+            min += r.Any(x => x < 400) ? 1 : 0;
+            min += r.Any(x => x >= 400 && x < 800) ? 1 : 0;
+            min += r.Any(x => x >= 800 && x < 1200) ? 1 : 0;
+            min += r.Any(x => x >= 1200 && x < 1600) ? 1 : 0;
+            min += r.Any(x => x >= 1600 && x < 2000) ? 1 : 0;
+            min += r.Any(x => x >= 2000 && x < 2400) ? 1 : 0;
+            min += r.Any(x => x >= 2400 && x < 2800) ? 1 : 0;
+            min += r.Any(x => x >= 2800 && x < 3200) ? 1 : 0;
+            max = min;
+            var p = r.Count(x => x >= 3200);
+            if (min <= 8 && p > 0)
+            {
+                while (max <= 8 && p > 0)
+                {
+                    max += 1;
+                    p--;
+                }
+            }
+
+            if (min > max)
+            {
+                min = max;
+            }
+            Console.WriteLine(min + " " + max);
         }
     }
 }
