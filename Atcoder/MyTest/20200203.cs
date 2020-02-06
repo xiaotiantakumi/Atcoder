3371f185b0ae1dc6b2da2ce224c980054c3e19d5
Takumi Oda <xiaotiantakumi@gmail.com>
2020-02-06 12:33:30 +0900

    DFSの練習用に追加したクラス

---
+++ Atcoder/MyTest/20200203.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atcoder.MyTest
+{
+    class _20200203
+    {
+    }
+
+    struct Graph
+    {
+        //public Graph()
+        //{
+
+        //}
+    }
+}
