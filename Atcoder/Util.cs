91dcdc112d70e2a8350e40afe58ca34e80627a97
Takumi Oda <xiaotiantakumi@gmail.com>
2020-02-08 22:11:07 +0900

    Util整備

--- Atcoder/Util.cs
+++ Atcoder/Util.cs
@@ -6,7 +6,12 @@ namespace ConsoleApp1
 {
     static class Util
     {
-        public static int Gcd(int a, int b)
+        // 最小公倍数
+        public static long Lcm(long a, long b)
+        {
+            return a * (b / Gcd(a, b));
+        }
+        public static long Gcd(long a, long b)
         {
             if (a < b)
                 return Gcd(b, a);
@@ -17,6 +22,47 @@ namespace ConsoleApp1
                 b = remainder;
             }
             return a;
+        }
+
+        public static List<T[]> AllPermutation<T>(params T[] array) where T : IComparable
+        {
+            var a = new List<T>(array).ToArray();
+            var res = new List<T[]>();
+            res.Add(new List<T>(a).ToArray());
+            var n = a.Length;
+            var next = true;
+            while (next)
+            {
+                next = false;
+
+                // 1
+                int i;
+                for (i = n - 2; i >= 0; i--)
+                {
+                    if (a[i].CompareTo(a[i + 1]) < 0) break;
+                }
+                // 2
+                if (i < 0) break;
+
+                // 3
+                var j = n;
+                do
+                {
+                    j--;
+                } while (a[i].CompareTo(a[j]) > 0);
+
+                if (a[i].CompareTo(a[j]) < 0)
+                {
+                    // 4
+                    var tmp = a[i];
+                    a[i] = a[j];
+                    a[j] = tmp;
+                    Array.Reverse(a, i + 1, n - i - 1);
+                    res.Add(new List<T>(a).ToArray());
+                    next = true;
+                }
+            }
+            return res;
         }
     }
 }
