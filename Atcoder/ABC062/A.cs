41093ccf76149094c44f81deba6428826ff896e4
Takumi Oda <xiaotiantakumi@gmail.com>
2020-02-13 07:55:50 +0900

    ABC062A回答、B途中

--- Atcoder/ABC062/A.cs
+++ Atcoder/ABC062/A.cs
@@ -1,10 +1,25 @@
-﻿namespace Atcoder.ABC062
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC062
 {
-    public class A : IAtcoder
+    public class A : AtcoderBase
     {
-        public void Run()
+        public override void Run()
         {
-            
+            var aGroup = new int[7] { 1, 3, 5, 7, 8, 10, 12 };
+            var bGroup = new int[4] { 4, 6, 9, 11 };
+            var r = ReadIntArray();
+            if (aGroup.Contains(r[0]) && aGroup.Contains(r[1]) ||
+                bGroup.Contains(r[0]) && bGroup.Contains(r[1]))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
         }
     }
 }
--- Atcoder/ABC062/B.cs
+++ Atcoder/ABC062/B.cs
@@ -1,9 +1,16 @@
-﻿namespace Atcoder.ABC062
+﻿using System.Collections.Generic;
+
+namespace Atcoder.ABC062
 {
-    public class B : IAtcoder
+    public class B : AtcoderBase
     {
-        public void Run()
+        public override void Run()
         {
+            var r = ReadIntArray();
+            var W = r[0];
+            var H = r[1];
+            List<int[]> rows = new List<int[]>();
+            List<int> row = new List<int>();
         }
     }
 }
