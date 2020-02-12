cb7d2aec049e371450b79f379958cc5513559ac2
Takumi Oda <xiaotiantakumi@gmail.com>
2020-02-12 23:05:03 +0900

    ABC154C途中。なぜTLEするのか、Listが原因か？

--- Atcoder/ABC154/C.cs
+++ Atcoder/ABC154/C.cs
@@ -1,9 +1,27 @@
-﻿namespace Atcoder.ABC154
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atcoder.ABC154
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var N = ReadInt();
+            var r = ReadIntArray();
+            List<int> inputList = new List<int>();
+
+            for (int i = 0; i < N; i++)
+            {
+                var item = r[i];
+                if (inputList.Contains(item))
+                {
+                    Console.WriteLine("NO");
+                    return;
+                }
+                inputList.Add(item);
+            }
+            Console.WriteLine("YES");
         }
     }
 }
