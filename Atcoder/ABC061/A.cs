93d601bff890258a7332113aa0d1a06abcf463df
Takumi Oda <xiaotiantakumi@gmail.com>
2020-02-15 14:28:19 +0900

    ABC061のA回答

--- Atcoder/ABC061/A.cs
+++ Atcoder/ABC061/A.cs
@@ -1,10 +1,20 @@
-﻿namespace Atcoder.ABC061
+﻿using System;
+
+namespace Atcoder.ABC061
 {
-    public class A : IAtcoder
+    public class A : AtcoderBase
     {
-        public void Run()
+        public override void Run()
         {
-            
+            var r = ReadIntArray();
+            if (r[0] <= r[2] && r[1] >= r[2])
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
--- Atcoder/ABC061/B.cs
+++ Atcoder/ABC061/B.cs
@@ -1,9 +1,40 @@
-﻿namespace Atcoder.ABC061
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC061
 {
-    public class B : IAtcoder
+    public class B : AtcoderBase
     {
-        public void Run()
+        public override void Run()
         {
+            var r = ReadIntArray();
+            Dictionary<int, int> roadDic = new Dictionary<int, int>();
+            for (int i = 0; i < r[1]; i++)
+            {
+                var tmp = ReadIntArray();
+                if (roadDic.ContainsKey(tmp[0]))
+                {
+                    roadDic[tmp[0]]++;
+                }
+                else
+                {
+                    roadDic.Add(tmp[0], 1);
+                }
+                if (roadDic.ContainsKey(tmp[1]))
+                {
+                    roadDic[tmp[1]]++;
+                }
+                else
+                {
+                    roadDic.Add(tmp[1], 1);
+                }
+            }
+
+            foreach (var pair in roadDic.OrderBy(x => x.Key))
+            {
+                Console.WriteLine(pair.Value);
+            }
         }
     }
 }
