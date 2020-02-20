6bd9bcbdd625e4f92244e1785fd6af198e1be2b3
Takumi Oda <xiaotiantakumi@gmail.com>
2020-02-21 07:52:41 +0900

    ABC058のAB回答

--- Atcoder/ABC058/A.cs
+++ Atcoder/ABC058/A.cs
@@ -1,10 +1,20 @@
-﻿namespace Atcoder.ABC058
+﻿using System;
+
+namespace Atcoder.ABC058
 {
-    public class A : IAtcoder
+    public class A : AtcoderBase
     {
-        public void Run()
+        public override void Run()
         {
-            
+            var r = ReadIntArray();
+            if (r[1] - r[0] == r[2] - r[1])
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
         }
     }
 }
--- Atcoder/ABC058/B.cs
+++ Atcoder/ABC058/B.cs
@@ -1,9 +1,26 @@
-﻿namespace Atcoder.ABC058
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC058
 {
-    public class B : IAtcoder
+    public class B : AtcoderBase
     {
-        public void Run()
+        public override void Run()
         {
+            var O = Read();
+            var E = Read();
+            string ret = string.Empty;
+            for (int i = 0; i < E.Length; i++)
+            {
+                ret += O[i].ToString() + E[i].ToString();
+            }
+
+            if (O.Length != E.Length)
+            {
+                ret += O.Last().ToString();
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
