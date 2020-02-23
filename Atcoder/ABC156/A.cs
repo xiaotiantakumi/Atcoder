c4beb081750ffe480995462fc7159195223541dc
Takumi Oda <xiaotiantakumi@gmail.com>
2020-02-23 21:05:38 +0900

    ABC156のA回答

--- Atcoder/ABC156/A.cs
+++ Atcoder/ABC156/A.cs
@@ -1,10 +1,23 @@
-﻿namespace Atcoder.ABC156
+﻿using System;
+
+namespace Atcoder.ABC156
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var r = ReadIntArray();
+            var N = r[0];
+            var R = r[1];
+
+            if (N < 10)
+            {
+                Console.WriteLine(R + (100 * (10 - N)));
+            }
+            else
+            {
+                Console.WriteLine(R);
+            }
         }
     }
 }
