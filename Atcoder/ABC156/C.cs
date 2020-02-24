14ffccdc8f30940e071ab5c22391a7cd71102263
Takumi Oda <xiaotiantakumi@gmail.com>
2020-02-24 21:05:58 +0900

    ABC156のC回答

--- Atcoder/ABC156/C.cs
+++ Atcoder/ABC156/C.cs
@@ -1,9 +1,31 @@
-﻿namespace Atcoder.ABC156
+﻿using System;
+
+namespace Atcoder.ABC156
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var N = ReadInt();
+            var r = ReadIntArray();
+
+            double ret = int.MaxValue;
+
+            for (int i = 0; i < 100; i++)
+            {
+                double tmp = 0;
+                foreach (var item in r)
+                {
+                    tmp += Math.Pow(item - i, 2);
+                }
+
+                if (ret > tmp)
+                {
+                    ret = tmp;
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
