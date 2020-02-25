57efd28b969aa9a6d90acb3b942b177db4db0972
Takumi Oda <xiaotiantakumi@gmail.com>
2020-02-25 12:27:32 +0900

    ABC056のA回答

--- Atcoder/ABC056/A.cs
+++ Atcoder/ABC056/A.cs
@@ -1,10 +1,20 @@
-﻿namespace Atcoder.ABC056
+﻿using System;
+
+namespace Atcoder.ABC056
 {
-    public class A : IAtcoder
+    public class A : AtcoderBase
     {
-        public void Run()
+        public override void Run()
         {
-            
+            var r = ReadIntArray();
+            if (r[0] + r[1] >= 24)
+            {
+                Console.WriteLine(r[0] + r[1] - 24);
+            }
+            else
+            {
+                Console.WriteLine(r[0] + r[1]);
+            }
         }
     }
 }
