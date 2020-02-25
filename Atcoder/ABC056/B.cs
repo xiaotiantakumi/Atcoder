3bd4d225d348a7f41e4d7dc9bbc977015836ba7b
Takumi Oda <xiaotiantakumi@gmail.com>
2020-02-25 12:41:28 +0900

    ABC56のB途中

--- Atcoder/ABC056/B.cs
+++ Atcoder/ABC056/B.cs
@@ -1,9 +1,35 @@
-﻿namespace Atcoder.ABC056
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atcoder.ABC056
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var r = ReadIntArray();
+            var N = r[0];
+            var M = r[1];
+            List<int[]> positions = new List<int[]>();
+            for (int i = 0; i < N; i++)
+            {
+                positions.Add(ReadIntArray());
+            }
+            List<int[]> checkPos = new List<int[]>();
+            for (int i = 0; i < M; i++)
+            {
+                checkPos.Add(ReadIntArray());
+            }
+            List<Tuple<int, int>> ret = new List<Tuple<int, int>>();
+            foreach (var position in positions)
+            {
+                int tmpRet = 0;
+                for (int i = 0; i < M; i++)
+                {
+                    var distance = Math.Abs(position[0] - checkPos[i][0]) + Math.Abs(position[1] - checkPos[i][1]);
+                    ret.Add(new Tuple<int, int>(i, distance));
+                }
+            }
         }
     }
 }
