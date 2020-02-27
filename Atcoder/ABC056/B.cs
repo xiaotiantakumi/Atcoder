ac28f479b689fcedb6b0b5e45b267366ed9d8b60
Takumi Oda <xiaotiantakumi@gmail.com>
2020-02-27 12:40:31 +0900

    ABC056のB回答

--- Atcoder/ABC056/B.cs
+++ Atcoder/ABC056/B.cs
@@ -7,7 +7,21 @@ namespace Atcoder.ABC056
     {
         public override void Run()
         {
-            
+            var wab = ReadIntArray();
+            int cal = 0;
+            int ret = 0;
+            if (wab[1] < wab[2])
+            {
+                cal = wab[2] - (wab[0] + wab[1]);
+                ret = cal <= 0 ? 0 : cal;
+            }
+            else
+            {
+                cal = wab[1] - (wab[0] + wab[2]);
+                ret = cal <= 0 ? 0 : cal;
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
