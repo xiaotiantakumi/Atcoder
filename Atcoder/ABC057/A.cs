0cea4c1630ec7df9ac2cda69665f73cd18bb133a
Takumi Oda <xiaotiantakumi@gmail.com>
2020-02-27 12:27:15 +0900

    チェックイン位置のミス

--- Atcoder/ABC057/A.cs
+++ Atcoder/ABC057/A.cs
@@ -1,10 +1,21 @@
-﻿namespace Atcoder.ABC057
+﻿using System;
+
+namespace Atcoder.ABC057
 {
-    public class A : IAtcoder
+    public class A : AtcoderBase
     {
-        public void Run()
+        public override void Run()
         {
-            
+
+            var r = ReadIntArray();
+            if (r[0] + r[1] >= 24)
+            {
+                Console.WriteLine(r[0] + r[1] - 24);
+            }
+            else
+            {
+                Console.WriteLine(r[0] + r[1]);
+            }
         }
     }
 }
--- Atcoder/ABC057/B.cs
+++ Atcoder/ABC057/B.cs
@@ -1,9 +1,46 @@
-﻿namespace Atcoder.ABC057
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atcoder.ABC057
 {
-    public class B : IAtcoder
+    public class B : AtcoderBase
     {
-        public void Run()
+        public override void Run()
         {
+            var r = ReadIntArray();
+            var N = r[0];
+            var M = r[1];
+            List<int[]> positions = new List<int[]>();
+            for (int i = 0; i < N; i++)
+            {
+                positions.Add(ReadIntArray());
+            }
+            List<int[]> checkPos = new List<int[]>();
+            for (int i = 0; i < M; i++)
+            {
+                checkPos.Add(ReadIntArray());
+            }
+
+            int[] retList = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                double distance = double.MaxValue;
+                for (int j = 0; j < M; j++)
+                {
+                    var tmpDistance = Math.Abs(positions[i][0] - checkPos[j][0]) +
+                                      Math.Abs(positions[i][1] - checkPos[j][1]);
+                    if (tmpDistance < distance)
+                    {
+                        distance = Math.Min(distance, tmpDistance);
+                        retList[i] = j + 1;
+                    }
+                }
+            }
+
+            foreach (var item in retList)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
