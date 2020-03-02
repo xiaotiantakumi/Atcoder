34dd16ac7d5af7a3d23c122ff81b1c5d83c5c6e3
Takumi Oda <xiaotiantakumi@gmail.com>
2020-03-02 12:33:09 +0900

    ABC157のA回答

--- Atcoder/ABC157/A.cs
+++ Atcoder/ABC157/A.cs
@@ -1,10 +1,13 @@
-﻿namespace Atcoder.ABC157
+﻿using System;
+
+namespace Atcoder.ABC157
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var N = ReadInt();
+            Console.WriteLine(N % 2 == 0 ? N / 2 : N / 2 + 1);
         }
     }
 }
