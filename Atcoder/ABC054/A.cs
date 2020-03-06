d9563d1352d0eccb5c228250deaad78089fccdfe
Takumi Oda <xiaotiantakumi@gmail.com>
2020-03-06 12:35:08 +0900

    ABC054のA回答

--- Atcoder/ABC054/A.cs
+++ Atcoder/ABC054/A.cs
@@ -1,10 +1,24 @@
-﻿namespace Atcoder.ABC054
+﻿using System;
+
+namespace Atcoder.ABC054
 {
-    public class A : IAtcoder
+    public class A : AtcoderBase
     {
-        public void Run()
+        public override void Run()
         {
-            
+            var AB = ReadIntArray();
+            if (AB[0] == AB[1])
+            {
+                Console.WriteLine("Draw");
+            }
+            else if (AB[0] == 1 || (AB[0] > AB[1] && AB[1] != 1))
+            {
+                Console.WriteLine("Alice");
+            }
+            else
+            {
+                Console.WriteLine("Bob");
+            }
         }
     }
 }
