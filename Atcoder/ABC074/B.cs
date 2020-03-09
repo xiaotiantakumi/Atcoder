25a095311a3934d405cfcf60afc9e51d7b270c73
Takumi Oda <xiaotiantakumi@gmail.com>
2020-03-09 12:28:29 +0900

    ABC074のB回答

--- Atcoder/ABC074/B.cs
+++ Atcoder/ABC074/B.cs
@@ -1,9 +1,21 @@
-﻿namespace Atcoder.ABC074
+﻿using System;
+
+namespace Atcoder.ABC074
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = ReadInt();
+            var K = ReadInt();
+            var xs = ReadIntArray();
+            int ret = 0;
+            for (int i = 0; i < xs.Length; i++)
+            {
+                ret += Math.Min(xs[i] * 2, ((K - xs[i]) * 2));
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
