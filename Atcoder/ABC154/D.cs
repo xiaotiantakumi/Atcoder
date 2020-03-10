7b0e7727e05869292c350d917ac5659afaabac96
Takumi Oda <xiaotiantakumi@gmail.com>
2020-03-10 21:05:31 +0900

    ABC154のD途中

--- Atcoder/ABC154/D.cs
+++ Atcoder/ABC154/D.cs
@@ -1,9 +1,48 @@
-﻿namespace Atcoder.ABC154
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC154
 {
     public class D : AtcoderBase
     {
         public override void Run()
         {
+            // やりたいこと
+            // 区間最大値を求める
+            // その区間内の期待値の合計を求める。
+
+            // 区間最大値がうまくもとまってない？？
+            var NK = ReadIntArray();
+            var ps = ReadIntArray();
+
+            var len = NK[0] - NK[1];
+            var posFrom = 0;
+            int maxValue = 0;
+            for (int i = 0; i <= len; i++)
+            {
+                var tmp = ps.Skip(i).Take(NK[1]).Sum();
+                if (maxValue < tmp)
+                {
+                    posFrom = i;
+                }
+            }
+            double kitai = 0;
+            for (int i = posFrom; i < posFrom + NK[1]; i++)
+            {
+                double p = ps[i];
+                kitai += SumAll(p) / p;
+            }
+
+            Console.WriteLine(kitai);
+        }
+
+        private static double SumAll(double p)
+        {
+            if (p == 0)
+            {
+                return 0;
+            }
+            return p + SumAll(p - 1);
         }
     }
 }
