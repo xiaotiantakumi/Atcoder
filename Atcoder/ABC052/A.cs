9375d84cac2439dd33f4ecf407fbffd0771a11c0
Takumi Oda <xiaotiantakumi@gmail.com>
2020-03-11 12:27:58 +0900

    ABC052のAB回答

--- Atcoder/ABC052/A.cs
+++ Atcoder/ABC052/A.cs
@@ -1,10 +1,13 @@
-﻿namespace Atcoder.ABC052
+﻿using System;
+
+namespace Atcoder.ABC052
 {
     public class A : AtcoderBase
     {
         public override void Run()
         {
-            
+            var abcd = ReadIntArray();
+            Console.WriteLine(Math.Max(abcd[0] * abcd[1], abcd[2] * abcd[3]));
         }
     }
 }
--- Atcoder/ABC052/B.cs
+++ Atcoder/ABC052/B.cs
@@ -1,9 +1,30 @@
-﻿namespace Atcoder.ABC052
+﻿using System;
+
+namespace Atcoder.ABC052
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = ReadInt();
+            var S = Read();
+            int ret = 0;
+            int cal = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (S[i] == 'I')
+                {
+                    ++cal;
+                }
+                else if (S[i] == 'D')
+                {
+                    --cal;
+                }
+
+                ret = Math.Max(cal, ret);
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
