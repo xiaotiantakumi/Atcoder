590719f34e7c6da4393acbfdcbb475b0a5f36fec
Takumi Oda <xiaotiantakumi@gmail.com>
2020-03-12 19:53:00 +0900

    ABC051のA回答

--- Atcoder/ABC051/A.cs
+++ Atcoder/ABC051/A.cs
@@ -1,10 +1,13 @@
-﻿namespace Atcoder.ABC051
+﻿using System;
+
+namespace Atcoder.ABC051
 {
     public class A : AtcoderBase
     {
         public override void Run()
         {
-            
+            var s = Read();
+            Console.WriteLine(s.Replace(",", " "));
         }
     }
 }
