04897c7cf06936966b46d8c64502570b1c6c997d
Takumi Oda <xiaotiantakumi@gmail.com>
2020-03-13 18:45:43 +0900

    ABC050のAB回答

--- Atcoder/ABC050/A.cs
+++ Atcoder/ABC050/A.cs
@@ -1,10 +1,22 @@
-﻿namespace Atcoder.ABC050
+﻿using System;
+
+namespace Atcoder.ABC050
 {
     public class A : AtcoderBase
     {
         public override void Run()
         {
-            
+            var r = ReadStringArray();
+            var A = int.Parse(r[0]);
+            var B = int.Parse(r[2]);
+            if (r[1] == "+")
+            {
+                Console.WriteLine(A + B);
+            }
+            else
+            {
+                Console.WriteLine(A - B);
+            }
         }
     }
 }
--- Atcoder/ABC050/B.cs
+++ Atcoder/ABC050/B.cs
@@ -1,9 +1,29 @@
-﻿namespace Atcoder.ABC050
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC050
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = ReadInt();
+            var Ts = ReadIntArray();
+            var M = ReadInt();
+            var sum = Ts.Sum();
+            List<int> rets = new List<int>();
+            for (int i = 0; i < M; i++)
+            {
+                var PX = ReadIntArray();
+                var ret = sum - Ts[PX[0] - 1] + PX[1];
+                rets.Add(ret);
+            }
+
+            foreach (var ret in rets)
+            {
+                Console.WriteLine(ret);
+            }
         }
     }
 }
