401016bc2e3b5d8b6ce99fa77d9e26dc89a28e0e
Takumi Oda <xiaotiantakumi@gmail.com>
2020-03-14 16:22:19 +0900

    ABC049のA回答

--- Atcoder/ABC049/A.cs
+++ Atcoder/ABC049/A.cs
@@ -1,10 +1,21 @@
-﻿namespace Atcoder.ABC049
+﻿using System;
+
+namespace Atcoder.ABC049
 {
     public class A : AtcoderBase
     {
         public override void Run()
         {
-            
+            var str = "aiueo";
+            var c = Read()[0];
+            if (str.Contains(c))
+            {
+                Console.WriteLine("vowel");
+            }
+            else
+            {
+                Console.WriteLine("consonant");
+            }
         }
     }
 }
