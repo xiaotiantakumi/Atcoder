7f0d86dfb6af902424ed976ef3e4fd9e01b52e10
Takumi Oda <xiaotiantakumi@gmail.com>
2020-03-17 12:23:36 +0900

    ABC049のB回答

--- Atcoder/ABC049/B.cs
+++ Atcoder/ABC049/B.cs
@@ -1,9 +1,26 @@
-﻿namespace Atcoder.ABC049
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atcoder.ABC049
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var HW = ReadIntArray();
+            List<string> ret = new List<string>();
+
+            for (int i = 0; i < HW[0]; i++)
+            {
+                var add = Read();
+                ret.Add(add);
+                ret.Add(add);
+            }
+
+            foreach (var item in ret)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
