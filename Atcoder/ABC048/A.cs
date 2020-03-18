cca827fcb8002956140325c153a786f8273ee017
Takumi Oda <xiaotiantakumi@gmail.com>
2020-03-18 12:34:50 +0900

    ABC048のAB回答

--- Atcoder/ABC048/A.cs
+++ Atcoder/ABC048/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC048
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC048
 {
     public class A : AtcoderBase
     {
         public override void Run()
         {
-            
+            var r = ReadStringArray();
+            Console.WriteLine("A" + r[1].First() + "C");
         }
     }
 }
--- Atcoder/ABC048/B.cs
+++ Atcoder/ABC048/B.cs
@@ -1,9 +1,20 @@
-﻿namespace Atcoder.ABC048
+﻿using System;
+
+namespace Atcoder.ABC048
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var abx = ReadLongArray();
+            var bDiv = abx[1] / abx[2];
+            long aDiv = -1;
+            if (abx[0] != 0)
+            {
+                aDiv = (abx[0] - 1) / abx[2];
+            }
+
+            Console.WriteLine(bDiv - aDiv);
         }
     }
 }
