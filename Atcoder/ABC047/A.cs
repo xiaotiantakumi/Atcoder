4de6c019526a9444b76e8d4fc6c3364492701198
Takumi Oda <xiaotiantakumi@gmail.com>
2020-03-19 22:22:51 +0900

    ABC047のA回答

--- Atcoder/ABC047/A.cs
+++ Atcoder/ABC047/A.cs
@@ -1,10 +1,22 @@
-﻿namespace Atcoder.ABC047
+﻿using System;
+
+namespace Atcoder.ABC047
 {
     public class A : AtcoderBase
     {
         public override void Run()
         {
-            
+            var abc = ReadIntArray();
+            if (abc[0] == abc[1] + abc[2] ||
+                abc[1] == abc[0] + abc[2] ||
+                abc[2] == abc[0] + abc[1])
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
