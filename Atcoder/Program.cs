81f5d522d09d2168da297d72a0e914afbf7b5d4d
Takumi Oda <xiaotiantakumi@gmail.com>
2020-03-24 19:18:12 +0900

    スニペットに対応させたので入力系を排除

--- Atcoder/Program.cs
+++ Atcoder/Program.cs
@@ -11,14 +11,6 @@ namespace Atcoder
     {
         static void Main(string[] args)
         {
-            
         }
-        private static readonly Func<string> Read = () => Console.ReadLine();
-        private static readonly Func<string[]> ReadStringArray = () => Console.ReadLine().Split();
-        private static readonly Func<int> ReadInt = () => int.Parse(Console.ReadLine());
-        private static readonly Func<long> ReadLong = () => long.Parse(Console.ReadLine());
-        private static readonly Func<int[]> ReadIntArray = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
-        private static readonly Func<long[]> ReadLongArray = () => Console.ReadLine().Split().Select(long.Parse).ToArray();
-        private static readonly Action<string> Cw = str => Console.WriteLine(str);
     }
 }
