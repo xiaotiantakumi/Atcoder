08733fdbdcfe65c9f950073693350e8cfe53d6b6
Takumi Oda <xiaotiantakumi@gmail.com>
2020-03-25 12:45:11 +0900

    ABC046のA回答

--- Atcoder/ABC/ABC046/A.cs
+++ Atcoder/ABC/ABC046/A.cs
@@ -1,10 +1,22 @@
-﻿namespace Atcoder.ABC046
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC046
 {
     public class A : AtcoderBase
     {
         public override void Run()
         {
-            
+            var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int ret = 3;
+            if (r[0] == r[1]) ret--;
+            if (r[1] == r[2]) ret--;
+            if (r[0] == r[2]) ret--;
+            if (ret == 0)
+            {
+                ret++;
+            }
+            Console.WriteLine(ret);
         }
     }
 }
--- Atcoder/Program.cs
+++ Atcoder/Program.cs
@@ -11,6 +11,7 @@ namespace Atcoder
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
