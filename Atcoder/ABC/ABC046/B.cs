8dc240c44ce5dc9df9059650cc3a3ebc51cd8998
Takumi Oda <xiaotiantakumi@gmail.com>
2020-03-26 21:52:16 +0900

    ABC046のB回答

--- Atcoder/ABC/ABC046/B.cs
+++ Atcoder/ABC/ABC046/B.cs
@@ -1,9 +1,21 @@
-﻿namespace Atcoder.ABC046
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC046
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var NK = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            if (NK[0] == 1)
+            {
+                Console.WriteLine(NK[1]);
+            }
+            else
+            {
+                Console.WriteLine(NK[1] * (Math.Pow(NK[1] - 1, NK[0] - 1)));
+            }
         }
     }
 }
--- Atcoder/Program.cs
+++ Atcoder/Program.cs
@@ -11,7 +11,7 @@ namespace Atcoder
     {
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
