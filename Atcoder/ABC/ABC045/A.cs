1c95a27b1c2e830a49fe6405397cfbba7ae64ebc
Takumi Oda <xiaotiantakumi@gmail.com>
2020-03-27 12:40:23 +0900

    ABC045のAB回答

--- Atcoder/ABC/ABC045/A.cs
+++ Atcoder/ABC/ABC045/A.cs
@@ -1,10 +1,16 @@
-﻿namespace Atcoder.ABC045
+﻿using System;
+
+namespace Atcoder.ABC045
 {
     public class A : AtcoderBase
     {
         public override void Run()
         {
-            
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var n = int.Parse(Console.ReadLine());
+            var ret = (a + b) * n / 2;
+            Console.WriteLine(ret);
         }
     }
 }
--- Atcoder/ABC/ABC045/B.cs
+++ Atcoder/ABC/ABC045/B.cs
@@ -1,9 +1,51 @@
-﻿namespace Atcoder.ABC045
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC045
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var A = Console.ReadLine();
+            var B = Console.ReadLine();
+            var C = Console.ReadLine();
+            var t = A.First();
+            A = A.Substring(1);
+
+            while (true)
+            {
+                switch (t)
+                {
+                    case 'a':
+                        if (!A.Any())
+                        {
+                            Console.WriteLine("A");
+                            return;
+                        }
+                        t = A.First();
+                        A = A.Substring(1);
+                        break;
+                    case 'b':
+                        if (!B.Any())
+                        {
+                            Console.WriteLine("B");
+                            return;
+                        }
+                        t = B.First();
+                        B = B.Substring(1);
+                        break;
+                    case 'c':
+                        if (!C.Any())
+                        {
+                            Console.WriteLine("C");
+                            return;
+                        }
+                        t = C.First();
+                        C = C.Substring(1);
+                        break;
+                }
+            }
         }
     }
 }
