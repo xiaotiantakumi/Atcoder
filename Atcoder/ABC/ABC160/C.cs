59d4b5ddfbd4f896c3fd780e76ae2e17ff7c2cb2
Takumi Oda <xiaotiantakumi@gmail.com>
2020-03-30 12:48:25 +0900

    ABC160のC回答

--- Atcoder/ABC/ABC160/C.cs
+++ Atcoder/ABC/ABC160/C.cs
@@ -1,9 +1,35 @@
-﻿namespace Atcoder.ABC160
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC160
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var KN = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var As = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var distance = int.MaxValue;
+            for (int i = 0; i < KN[1]; i++)
+            {
+                if (i == 0)
+                {
+                    distance = Math.Min(distance, KN[0] - (As[i] - 0));
+                    distance = Math.Min(distance, As[KN[1] - 1] - As[0]);
+                }
+                else if (i == KN[1] - 1)
+                {
+                    distance = Math.Min(distance, KN[0] - (KN[1] - As[i]));
+                    distance = Math.Min(distance, As[KN[1] - 1] - As[0]);
+                }
+                else
+                {
+                    distance = Math.Min(distance, KN[0] - (As[i] - As[i - 1]));
+                    distance = Math.Min(distance, KN[0] - (As[i + 1] - As[i]));
+                }
+            }
+
+            Console.WriteLine(distance);
         }
     }
 }
