f58b1f4e4c398bd41c007984de656a2cd605392b
Takumi Oda <xiaotiantakumi@gmail.com>
2020-03-31 20:30:27 +0900

    ABC044のAB回答

--- Atcoder/ABC/ABC044/A.cs
+++ Atcoder/ABC/ABC044/A.cs
@@ -1,10 +1,16 @@
-﻿namespace Atcoder.ABC044
+﻿using System;
+
+namespace Atcoder.ABC044
 {
     public class A : AtcoderBase
     {
         public override void Run()
         {
-            
+            var N = int.Parse(Console.ReadLine());
+            var K = int.Parse(Console.ReadLine());
+            var X = int.Parse(Console.ReadLine());
+            var Y = int.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Min(K, N) * X + Math.Max(0, (N - K) * Y));
         }
     }
 }
--- Atcoder/ABC/ABC044/B.cs
+++ Atcoder/ABC/ABC044/B.cs
@@ -1,9 +1,36 @@
-﻿namespace Atcoder.ABC044
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atcoder.ABC044
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var r = Console.ReadLine();
+            Dictionary<char, int> word = new Dictionary<char, int>();
+            foreach (var c in r)
+            {
+                if (word.ContainsKey(c))
+                {
+                    word[c]++;
+                }
+                else
+                {
+                    word.Add(c, 1);
+                }
+            }
+
+            foreach (var pair in word)
+            {
+                if (pair.Value % 2 != 0)
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Yes");
         }
     }
 }
