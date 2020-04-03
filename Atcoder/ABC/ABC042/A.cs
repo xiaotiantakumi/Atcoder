520018a5d223551f7e028903530ff6230e7a58d2
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-03 12:34:29 +0900

    ABC042のAB回答

--- Atcoder/ABC/ABC042/A.cs
+++ Atcoder/ABC/ABC042/A.cs
@@ -1,10 +1,15 @@
-﻿namespace Atcoder.ABC042
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC042
 {
     public class A : AtcoderBase
     {
         public override void Run()
         {
-            
+            var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var ret = r.Count(x => x == 5) == 2 && r.Any(x => x == 7);
+            Console.WriteLine(ret ? "YES" : "NO");
         }
     }
 }
--- Atcoder/ABC/ABC042/B.cs
+++ Atcoder/ABC/ABC042/B.cs
@@ -1,9 +1,28 @@
-﻿namespace Atcoder.ABC042
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC042
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var NL = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            List<string> strs = new List<string>();
+            for (int i = 0; i < NL[0]; i++)
+            {
+                strs.Add(Console.ReadLine());
+            }
+
+            strs = strs.OrderBy(x => x, StringComparer.OrdinalIgnoreCase).ToList();
+            string ret = string.Empty;
+            foreach (var str in strs)
+            {
+                ret += str;
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
