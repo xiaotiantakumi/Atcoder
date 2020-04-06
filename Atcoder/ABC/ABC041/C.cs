b81ef1f00230ab2984b1f74db1c90151913a311d
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-06 12:41:52 +0900

    ABC041のC回答

--- Atcoder/ABC/ABC041/C.cs
+++ Atcoder/ABC/ABC041/C.cs
@@ -1,9 +1,25 @@
-﻿namespace Atcoder.ABC041
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC041
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var As = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < N; i++)
+            {
+                dic.Add(i + 1, As[i]);
+            }
+
+            foreach (var item in dic.OrderByDescending(x => x.Value))
+            {
+                Console.WriteLine(item.Key);
+            }
         }
     }
 }
