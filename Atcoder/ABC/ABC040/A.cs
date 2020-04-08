c8bba9c24b6db167dad1cde261e90a2530641891
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-08 22:46:57 +0900

    ABC040のA回答

--- Atcoder/ABC/ABC040/A.cs
+++ Atcoder/ABC/ABC040/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC040
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC040
 {
     public class A : AtcoderBase
     {
         public override void Run()
         {
-            
+            var nx = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(Math.Min(nx[0] - nx[1], nx[1] - 1));
         }
     }
 }
