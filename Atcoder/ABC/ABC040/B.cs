1bc6a35b8689abf2ae1f9d4d626bed8de6c82217
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-09 13:32:57 +0900

    ABC040のB回答

--- Atcoder/ABC/ABC040/B.cs
+++ Atcoder/ABC/ABC040/B.cs
@@ -1,9 +1,28 @@
-﻿namespace Atcoder.ABC040
+﻿using System;
+
+namespace Atcoder.ABC040
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var ret = int.MaxValue;
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = 1; j <= N; j++)
+                {
+                    var mple = i * j;
+                    if (mple > N)
+                    {
+                        break;
+                    }
+
+                    var tmp = Math.Abs(i - j) + (N - mple);
+                    ret = Math.Min(ret, tmp);
+                }
+            }
+            Console.WriteLine(ret);
         }
     }
 }
