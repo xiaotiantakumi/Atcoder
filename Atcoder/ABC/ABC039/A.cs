bf663eb98b8dcdd4dc6ec61fd4b75785924da53b
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-10 20:07:04 +0900

    ABC039のAB回答

--- Atcoder/ABC/ABC039/A.cs
+++ Atcoder/ABC/ABC039/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC039
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC039
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var ABC = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(ABC[0] * ABC[1] * 2 + ABC[1] * ABC[2] * 2 + ABC[0] * ABC[2] * 2);
         }
     }
 }
--- Atcoder/ABC/ABC039/B.cs
+++ Atcoder/ABC/ABC039/B.cs
@@ -1,9 +1,14 @@
-﻿namespace Atcoder.ABC039
+﻿using System;
+
+namespace Atcoder.ABC039
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var r = long.Parse(Console.ReadLine());
+            var ret = Math.Sqrt(Math.Sqrt(r));
+            Console.WriteLine(ret);
         }
     }
 }
