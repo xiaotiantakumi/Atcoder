76bccdf6e3515cb76a872de2dc9f7f836faad708
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-11 19:01:51 +0900

    ABC039のC途中

--- Atcoder/ABC/ABC039/C.cs
+++ Atcoder/ABC/ABC039/C.cs
@@ -1,9 +1,35 @@
-﻿namespace Atcoder.ABC039
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atcoder.ABC039
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            List<string> doremi = new List<string>() { "Do", "Do", "Re", "Re", "Mi", "Fa", "Fa", "So", "So", "La", "La", "Si" };
+            var mark = "WW";
+            var r = Console.ReadLine();
+            List<int> postions = new List<int>();
+            for (int i = 0; i < r.Length - 1; i++)
+            {
+                if (r[i] == 'W' && r[i] == r[i + 1])
+                {
+                    postions.Add(i);
+                }
+            }
+
+            if (postions[1] - postions[0] >= 7)
+            {
+                if (postions[0] <= 4)
+                {
+
+                }
+            }
+            else
+            {
+
+            }
         }
     }
 }
