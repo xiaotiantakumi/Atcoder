180a168ff74972f0733476297c8c92fbdc0ee240
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-13 21:52:45 +0900

    ABC038のA回答

--- Atcoder/ABC/ABC038/A.cs
+++ Atcoder/ABC/ABC038/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC038
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC038
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var r = Console.ReadLine();
+            Console.WriteLine(r.Last() == 'T' ? "YES" : "NO");
         }
     }
 }
