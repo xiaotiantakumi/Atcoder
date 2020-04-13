efdab76f02ce5463bd45be46939415a8ac56e189
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-13 22:00:02 +0900

    ABC038のB回答

--- Atcoder/ABC/ABC038/B.cs
+++ Atcoder/ABC/ABC038/B.cs
@@ -1,9 +1,23 @@
-﻿namespace Atcoder.ABC038
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC038
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var WH1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var WH2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            if (WH1[0] == WH2[0] || WH1[0] == WH2[1] ||
+                WH1[1] == WH2[0] || WH1[1] == WH2[1])
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
         }
     }
 }
