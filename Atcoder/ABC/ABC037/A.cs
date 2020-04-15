254883669a393f736ef3099a3d491f6ebf383b6f
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-15 20:07:35 +0900

    ABC037のAB回答

--- Atcoder/ABC/ABC037/A.cs
+++ Atcoder/ABC/ABC037/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC037
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC037
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var ABC = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(ABC[2] / Math.Min(ABC[0], ABC[1]));
         }
     }
 }
--- Atcoder/ABC/ABC037/B.cs
+++ Atcoder/ABC/ABC037/B.cs
@@ -1,9 +1,29 @@
-﻿namespace Atcoder.ABC037
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC037
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var NQ = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var N = NQ[0];
+            var Q = NQ[1];
+            int[] ret = new int[N];
+            for (int i = 0; i < Q; i++)
+            {
+                var LRT = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = LRT[0] - 1; j < LRT[1]; j++)
+                {
+                    ret[j] = LRT[2];
+                }
+            }
+
+            for (int i = 0; i < ret.Length; i++)
+            {
+                Console.WriteLine(ret[i]);
+            }
         }
     }
 }
