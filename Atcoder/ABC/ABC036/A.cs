498150075d1d17d4e19afb5a27e4421e5ed8b584
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-17 23:13:51 +0900

    ABC036のAB回答

--- Atcoder/ABC/ABC036/A.cs
+++ Atcoder/ABC/ABC036/A.cs
@@ -1,10 +1,19 @@
-﻿namespace Atcoder.ABC036
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC036
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var AB = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int ret = AB[1] / AB[0];
+            if (AB[1] % AB[0] != 0)
+            {
+                ret++;
+            }
+            Console.WriteLine(ret);
         }
     }
 }
--- Atcoder/ABC/ABC036/B.cs
+++ Atcoder/ABC/ABC036/B.cs
@@ -1,9 +1,33 @@
-﻿namespace Atcoder.ABC036
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atcoder.ABC036
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            List<string> graph = new List<string>();
+            for (int i = 0; i < N; i++)
+            {
+                graph.Add(Console.ReadLine());
+            }
+            List<string> ret = new List<string>();
+            for (int i = 0; i < N; i++)
+            {
+                string row = string.Empty;
+                for (int j = N - 1; j >= 0; j--)
+                {
+                    row += graph[j][i].ToString();
+                }
+                ret.Add(row);
+            }
+
+            foreach (var item in ret)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
