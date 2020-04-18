c280fd72769fd4534aa9f2200e4aaf7ef6d282ee
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-18 14:19:03 +0900

    ABC035のA回答

--- Atcoder/ABC/ABC035/A.cs
+++ Atcoder/ABC/ABC035/A.cs
@@ -1,10 +1,24 @@
-﻿namespace Atcoder.ABC035
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC035
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var WH = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var W = WH[0];
+            var H = WH[1];
+
+            if (W % 16 == 0 && H % 9 == 0)
+            {
+                Console.WriteLine("16:9");
+            }
+            else
+            {
+                Console.WriteLine("4:3");
+            }
         }
     }
 }
