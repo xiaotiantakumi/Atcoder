d75fd71cb5695e94c91415d9254d4678cb02630d
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-20 06:36:25 +0900

    ABC163のAB回答

--- Atcoder/ABC/ABC163/A.cs
+++ Atcoder/ABC/ABC163/A.cs
@@ -1,10 +1,13 @@
-﻿namespace Atcoder.ABC.ABC163
+﻿using System;
+
+namespace Atcoder.ABC.ABC163
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var r = int.Parse(Console.ReadLine());
+            Console.WriteLine(r * 2 * Math.PI);
         }
     }
 }
--- Atcoder/ABC/ABC163/B.cs
+++ Atcoder/ABC/ABC163/B.cs
@@ -1,9 +1,22 @@
-﻿namespace Atcoder.ABC.ABC163
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC163
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var NM = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var sum = Console.ReadLine().Split().Select(int.Parse).Sum();
+            if (NM[0] - sum < 0)
+            {
+                Console.WriteLine("-1");
+            }
+            else
+            {
+                Console.WriteLine(NM[0] - sum);
+            }
         }
     }
 }
