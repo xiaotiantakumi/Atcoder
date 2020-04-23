2d050b2b97f36da75066209a017295da25458c2f
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-23 23:27:16 +0900

    ABC034のAB回答

--- Atcoder/ABC/ABC034/A.cs
+++ Atcoder/ABC/ABC034/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC.ABC034
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC034
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var xy = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(xy[0] < xy[1] ? "Better" : "Worse");
         }
     }
 }
--- Atcoder/ABC/ABC034/B.cs
+++ Atcoder/ABC/ABC034/B.cs
@@ -1,9 +1,20 @@
-﻿namespace Atcoder.ABC.ABC034
+﻿using System;
+
+namespace Atcoder.ABC.ABC034
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                Console.WriteLine(n - 1);
+            }
+            else
+            {
+                Console.WriteLine(n + 1);
+            }
         }
     }
 }
