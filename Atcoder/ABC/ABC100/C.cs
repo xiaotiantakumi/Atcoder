2bdc7b7e841d1e37947aab62344de6990e547546
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-24 17:52:19 +0900

    ABC100のC回答

--- Atcoder/ABC/ABC100/C.cs
+++ Atcoder/ABC/ABC100/C.cs
@@ -1,9 +1,34 @@
-﻿namespace Atcoder.ABC100
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC100
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var As = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int ret = 0;
+            foreach (var item in As)
+            {
+
+                var tmp = item;
+                while (true)
+                {
+                    if (tmp % 2 == 0)
+                    {
+                        ret++;
+                        tmp = tmp / 2;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
