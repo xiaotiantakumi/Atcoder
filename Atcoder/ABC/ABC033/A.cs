912f22df4af42cc4f8c514004ffe5872f614dc21
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-25 17:33:34 +0900

    ABC033のA回答

--- Atcoder/ABC/ABC033/A.cs
+++ Atcoder/ABC/ABC033/A.cs
@@ -1,10 +1,15 @@
-﻿namespace Atcoder.ABC.ABC033
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC033
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var r = Console.ReadLine();
+            var first = r.First();
+            Console.WriteLine(r.All(x => x == first) ? "SAME" : "DIFFERENT");
         }
     }
 }
