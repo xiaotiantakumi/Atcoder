57a0844e356c262cf04f50a010ff767d166fd761
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-26 14:25:02 +0900

    ABC061のB回答

--- Atcoder/ABC/ABC061/B.cs
+++ Atcoder/ABC/ABC061/B.cs
@@ -8,33 +8,16 @@ namespace Atcoder.ABC061
     {
         public override void Run()
         {
-            var r = ReadIntArray();
-            Dictionary<int, int> roadDic = new Dictionary<int, int>();
-            for (int i = 0; i < r[1]; i++)
+            var NM = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] ret = new int[NM[0]];
+            for (int i = 0; i < NM[1]; i++)
             {
-                var tmp = ReadIntArray();
-                if (roadDic.ContainsKey(tmp[0]))
-                {
-                    roadDic[tmp[0]]++;
-                }
-                else
-                {
-                    roadDic.Add(tmp[0], 1);
-                }
-                if (roadDic.ContainsKey(tmp[1]))
-                {
-                    roadDic[tmp[1]]++;
-                }
-                else
-                {
-                    roadDic.Add(tmp[1], 1);
-                }
+                var ab = Console.ReadLine().Split().Select(x => int.Parse(x) - 1).ToArray();
+                ret[ab[0]]++;
+                ret[ab[1]]++;
             }
 
-            foreach (var pair in roadDic.OrderBy(x => x.Key))
-            {
-                Console.WriteLine(pair.Value);
-            }
+            Console.WriteLine(string.Join("\n", ret));
         }
     }
 }
