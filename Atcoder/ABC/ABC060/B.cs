2ed2a8fc79e76e15c673dd0bb5adadcb4b24f80f
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-26 14:42:24 +0900

    ABC060のB回答

--- Atcoder/ABC/ABC060/B.cs
+++ Atcoder/ABC/ABC060/B.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Atcoder.ABC060
 {
@@ -6,25 +7,30 @@ namespace Atcoder.ABC060
     {
         public override void Run()
         {
-            var r = ReadIntArray();
-            if (r[0] < r[1])
+            var ABC = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var A = ABC[0];
+            var B = ABC[1];
+            var C = ABC[2];
+            Calc(A,B,C);
+        }
+        private static int count = 0;
+        private static int sum = 0;
+        private static void Calc(int A,int B,int C)
+        {
+            if (count > 1000)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+
+            count++;
+            sum += A;
+            if (sum % B == C)
             {
                 Console.WriteLine("YES");
                 return;
             }
-            if (r[0] - r[1] < r[2])
-            {
-                Console.WriteLine("NO");
-                return;
-            }
-
-            if (r[0] == r[1])
-            {
-                Console.WriteLine("NO");
-                return;
-            }
-
-            Console.WriteLine("YES");
+            Calc(A,B,C);
         }
     }
 }
