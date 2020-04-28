91884c95c61b1d18498fd8232b8ff1e844693de2
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-28 21:32:14 +0900

    ABC032のA回答

--- Atcoder/ABC/ABC032/A.cs
+++ Atcoder/ABC/ABC032/A.cs
@@ -1,10 +1,23 @@
-﻿namespace Atcoder.ABC.ABC032
+﻿using System;
+
+namespace Atcoder.ABC.ABC032
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var n = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                if (n % a == 0 && n % b == 0)
+                {
+                    Console.WriteLine(n);
+                    return;
+                }
+                n++;
+            }
         }
     }
 }
