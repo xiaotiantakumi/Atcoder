2bedc11fbc63fb9a151382f69e672a9be09af4b1
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-29 08:15:51 +0900

    ABC033のC回答

--- Atcoder/ABC/ABC033/C.cs
+++ Atcoder/ABC/ABC033/C.cs
@@ -1,9 +1,29 @@
-﻿namespace Atcoder.ABC.ABC033
+﻿using System;
+using System.Text;
+
+namespace Atcoder.ABC.ABC033
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var S = Console.ReadLine();
+            StringBuilder part = new StringBuilder();
+            int ret = 0;
+            bool hasZero = false;
+            for (int i = 0; i < S.Length; i++)
+            {
+                part.Append(S[i]);
+                if (S[i] == '0') hasZero = true;
+                if (S[i] == '+' || i == S.Length - 1)
+                {
+                    ret += hasZero ? 0 : 1;
+                    part.Clear();
+                    hasZero = false;
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
