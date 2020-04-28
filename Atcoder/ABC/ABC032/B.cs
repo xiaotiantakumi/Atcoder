5dafc1ceaeea30555771f799a51d6645422a21e8
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-28 21:43:19 +0900

    ABC032のB回答

--- Atcoder/ABC/ABC032/B.cs
+++ Atcoder/ABC/ABC032/B.cs
@@ -1,9 +1,23 @@
-﻿namespace Atcoder.ABC.ABC032
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC032
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var s = Console.ReadLine();
+            var k = int.Parse(Console.ReadLine());
+            HashSet<string> set = new HashSet<string>();
+            for (int i = 0; i <= s.Length - k; i++)
+            {
+                var tmp = s.Substring(i, k);
+                set.Add(tmp);
+            }
+
+            Console.WriteLine(set.Count());
         }
     }
 }
