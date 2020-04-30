f6a44aa1e426d20fc2a92e82ba84df51ec28b77f
Takumi Oda <xiaotiantakumi@gmail.com>
2020-04-30 22:53:56 +0900

    ABC031のA回答

--- Atcoder/ABC/ABC031/A.cs
+++ Atcoder/ABC/ABC031/A.cs
@@ -1,10 +1,17 @@
-﻿namespace Atcoder.ABC.ABC031
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC031
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var AD = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var small = Math.Min(AD[0], AD[1]) + 1;
+            var big = Math.Max(AD[0], AD[1]);
+
+            Console.WriteLine(small * big);
         }
     }
 }
