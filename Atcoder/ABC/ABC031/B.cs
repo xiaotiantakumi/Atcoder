a9ec9fcf8dac69f0698c00eb042ff4d682c43d44
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-01 22:44:49 +0900

    ABC031のB回答

--- Atcoder/ABC/ABC031/B.cs
+++ Atcoder/ABC/ABC031/B.cs
@@ -1,9 +1,36 @@
-﻿namespace Atcoder.ABC.ABC031
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC031
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var LH = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var L = LH[0];
+            var H = LH[1];
+            var N = int.Parse(Console.ReadLine());
+            List<int> ret = new List<int>();
+            for (int i = 0; i < N; i++)
+            {
+                var r = int.Parse(Console.ReadLine());
+                if (r < L)
+                {
+                    ret.Add(L - r);
+                }
+                else if (r > H)
+                {
+                    ret.Add(-1);
+                }
+                else
+                {
+                    ret.Add(0);
+                }
+            }
+
+            Console.WriteLine(string.Join("\n", ret));
         }
     }
 }
