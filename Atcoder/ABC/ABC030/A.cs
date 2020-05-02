b0e29251a5b4afeca960c8eeb76624ecede240f6
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-02 18:30:56 +0900

    ABC030のA回答

--- Atcoder/ABC/ABC030/A.cs
+++ Atcoder/ABC/ABC030/A.cs
@@ -1,10 +1,25 @@
-﻿namespace Atcoder.ABC.ABC030
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC030
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var ABCD = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            if (ABCD[1] / ABCD[0] < ABCD[3] / ABCD[2])
+            {
+                Console.WriteLine("AOKI");
+            }
+            else if (ABCD[1] / ABCD[0] == ABCD[3] / ABCD[2])
+            {
+                Console.WriteLine("DRAW");
+            }
+            else
+            {
+                Console.WriteLine("TAKAHASHI");
+            }
         }
     }
 }
