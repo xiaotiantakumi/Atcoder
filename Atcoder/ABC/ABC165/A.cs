baff8487f66778d3b384c74b93f7d685e0b0b8b3
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-03 10:07:15 +0900

    ABC165のAB回答

--- Atcoder/ABC/ABC165/A.cs
+++ Atcoder/ABC/ABC165/A.cs
@@ -1,10 +1,24 @@
-﻿namespace Atcoder.ABC.ABC165
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC165
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var K = int.Parse(Console.ReadLine());
+            var AB = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = AB[0]; i <= AB[1]; i++)
+            {
+                if (i % K == 0)
+                {
+                    Console.WriteLine("OK");
+                    return;
+                }
+            }
+
+            Console.WriteLine("NG");
         }
     }
 }
--- Atcoder/ABC/ABC165/B.cs
+++ Atcoder/ABC/ABC165/B.cs
@@ -1,9 +1,21 @@
-﻿namespace Atcoder.ABC.ABC165
+﻿using System;
+
+namespace Atcoder.ABC.ABC165
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var X = long.Parse(Console.ReadLine());
+            double cal = 100;
+            int count = 0;
+            while (X > cal)
+            {
+                cal += Math.Truncate(cal * 0.01);
+                count++;
+            }
+
+            Console.WriteLine(count);
         }
     }
 }
