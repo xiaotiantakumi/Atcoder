331a71af7dc8eaa0f16b2e767ec84d2233c2666b
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-05 01:53:56 +0900

    ABC123のC回答

--- Atcoder/ABC/ABC123/C.cs
+++ Atcoder/ABC/ABC123/C.cs
@@ -1,9 +1,27 @@
-﻿namespace Atcoder.ABC.ABC123
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC123
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var N = long.Parse(Console.ReadLine());
+            var A = long.Parse(Console.ReadLine());
+            var B = long.Parse(Console.ReadLine());
+            var C = long.Parse(Console.ReadLine());
+            var D = long.Parse(Console.ReadLine());
+            var E = long.Parse(Console.ReadLine());
+            decimal ret = 4;
+            var min = GetMin(A, B, C, D, E);
+            ret += Math.Ceiling((decimal)N / (decimal)min);
+            Console.WriteLine(ret);
+        }
+
+        static long GetMin(params long[] args)
+        {
+            return args.Min();
         }
     }
 }
