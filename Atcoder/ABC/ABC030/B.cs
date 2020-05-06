13e90be34ac9aa4ed9607a9c35001e40d6bc5ee0
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-06 17:29:33 +0900

    ABC030のB回答

--- Atcoder/ABC/ABC030/B.cs
+++ Atcoder/ABC/ABC030/B.cs
@@ -1,9 +1,17 @@
-﻿namespace Atcoder.ABC.ABC030
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC030
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var nm = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            var hourRan = (nm[0] % 12) * 360 / 12 + nm[1] * 360 / 12 / 60;
+            var minRan = nm[1] * 360 / 60;
+            var ret = Math.Max(hourRan, minRan) - Math.Min(hourRan, minRan);
+            Console.WriteLine(ret > 180 ? 360 - ret : ret);
         }
     }
 }
