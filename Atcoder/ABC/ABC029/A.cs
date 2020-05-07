29a2acb710e44f3ba875ce231085cbed6c683dcf
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-07 20:18:31 +0900

    ABC029のAB回答

--- Atcoder/ABC/ABC029/A.cs
+++ Atcoder/ABC/ABC029/A.cs
@@ -1,10 +1,13 @@
-﻿namespace Atcoder.ABC.ABC029
+﻿using System;
+
+namespace Atcoder.ABC.ABC029
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var s = Console.ReadLine();
+            Console.WriteLine(s + "s");
         }
     }
 }
--- Atcoder/ABC/ABC029/B.cs
+++ Atcoder/ABC/ABC029/B.cs
@@ -1,9 +1,22 @@
-﻿namespace Atcoder.ABC.ABC029
+﻿using System;
+
+namespace Atcoder.ABC.ABC029
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            int ret = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                var s = Console.ReadLine();
+                if (s.Contains('r'))
+                {
+                    ret++;
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
