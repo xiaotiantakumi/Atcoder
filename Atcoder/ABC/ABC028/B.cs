f4b04cd18def212afce1bed8a50f74c5aa6cba7c
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-09 06:55:23 +0900

    ABC028のBC回答

--- Atcoder/ABC/ABC028/B.cs
+++ Atcoder/ABC/ABC028/B.cs
@@ -1,9 +1,28 @@
-﻿namespace Atcoder.ABC.ABC028
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atcoder.ABC.ABC028
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var S = Console.ReadLine();
+            Dictionary<char, int> ret = new Dictionary<char, int>();
+            ret.Add('A', 0);
+            ret.Add('B', 0);
+            ret.Add('C', 0);
+            ret.Add('D', 0);
+            ret.Add('E', 0);
+            ret.Add('F', 0);
+            foreach (var s in S)
+            {
+                if (ret.ContainsKey(s))
+                {
+                    ret[s]++;
+                }
+            }
+            Console.WriteLine(string.Join(" ", ret.Values));
         }
     }
 }
--- Atcoder/ABC/ABC028/C.cs
+++ Atcoder/ABC/ABC028/C.cs
@@ -1,9 +1,14 @@
-﻿namespace Atcoder.ABC.ABC028
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC028
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(Math.Max(r[0] + r[3] + r[4], r[1] + r[2] + r[4]));
         }
     }
 }
