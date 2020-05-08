64bfa3c598a54dce0026cf0041280f521b8dbf4e
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-08 23:05:22 +0900

    ABC028のA回答

--- Atcoder/ABC/ABC028/A.cs
+++ Atcoder/ABC/ABC028/A.cs
@@ -1,10 +1,29 @@
-﻿namespace Atcoder.ABC.ABC028
+﻿using System;
+
+namespace Atcoder.ABC.ABC028
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var N = int.Parse(Console.ReadLine());
+            if (N <= 59)
+            {
+                Console.WriteLine("Bad");
+            }
+            else if (N <= 89)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (N <= 99)
+            {
+                Console.WriteLine("Great");
+
+            }
+            else
+            {
+                Console.WriteLine("Perfect");
+            }
         }
     }
 }
