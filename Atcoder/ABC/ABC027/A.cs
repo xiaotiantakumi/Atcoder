26c1a0ace978de30ee76dd65378f87381a1c96f6
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-10 16:01:50 +0900

    ABC027のA回答

--- Atcoder/ABC/ABC027/A.cs
+++ Atcoder/ABC/ABC027/A.cs
@@ -1,10 +1,25 @@
-﻿namespace Atcoder.ABC.ABC027
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC027
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            if (r[0] == r[1])
+            {
+                Console.WriteLine(r[2]);
+            }
+            else if (r[0] == r[2])
+            {
+                Console.WriteLine(r[1]);
+            }
+            else
+            {
+                Console.WriteLine(r[0]);
+            }
         }
     }
 }
