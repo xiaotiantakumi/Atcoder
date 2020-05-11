0ccba9ce3a8f041324eec6951abbc84bed4569ee
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-11 22:14:05 +0900

    ABC167のAB回答

--- Atcoder/ABC/ABC167/A.cs
+++ Atcoder/ABC/ABC167/A.cs
@@ -1,10 +1,22 @@
-﻿namespace Atcoder.ABC.ABC167
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC167
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var S = Console.ReadLine();
+            var T = Console.ReadLine();
+            if (S == new string(T.Take(S.Length).ToArray()))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
--- Atcoder/ABC/ABC167/B.cs
+++ Atcoder/ABC/ABC167/B.cs
@@ -1,9 +1,32 @@
-﻿namespace Atcoder.ABC.ABC167
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC167
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var ABCK = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var A = ABCK[0];
+            var B = ABCK[1];
+            var C = ABCK[2];
+            var K = ABCK[3];
+            int ret = 0;
+
+            if (K <= A)
+            {
+                Console.WriteLine(K);
+                return;
+            }
+
+            if (K <= A + B)
+            {
+                Console.WriteLine(A);
+                return;
+            }
+
+            Console.WriteLine(A - (K - A - B));
         }
     }
 }
