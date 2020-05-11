009651fad69dd0fb1fe430c6130b7bef0286998c
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-11 22:34:48 +0900

    ABC026のB回答

--- Atcoder/ABC/ABC026/B.cs
+++ Atcoder/ABC/ABC026/B.cs
@@ -1,9 +1,40 @@
-﻿namespace Atcoder.ABC.ABC026
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC026
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            bool isPlus = false;
+            if (N % 2 != 0)
+            {
+                isPlus = true;
+            }
+            double ret = 0;
+            List<int> rList = new List<int>();
+            for (int i = 0; i < N; i++)
+            {
+                rList.Add(int.Parse(Console.ReadLine()));
+            }
+            foreach (var r in rList.OrderBy(x => x))
+            {
+                if (isPlus)
+                {
+                    ret += Math.Pow(r, 2);
+                }
+                else
+                {
+                    ret -= Math.Pow(r, 2);
+                }
+
+                isPlus = !isPlus;
+            }
+
+            Console.WriteLine(ret * Math.PI);
         }
     }
 }
