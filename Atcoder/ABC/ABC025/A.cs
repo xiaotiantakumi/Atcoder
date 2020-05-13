0bf36aaa4554370f050ce632fd7a9de9ed018341
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-13 12:00:07 +0900

    ABC025のA回答

--- Atcoder/ABC/ABC025/A.cs
+++ Atcoder/ABC/ABC025/A.cs
@@ -1,10 +1,24 @@
-﻿namespace Atcoder.ABC.ABC025
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atcoder.ABC.ABC025
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var S = Console.ReadLine();
+            var N = int.Parse(Console.ReadLine());
+            List<string> list = new List<string>();
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    list.Add(S[i].ToString() + S[j].ToString());
+                }
+            }
+
+            Console.WriteLine(list[N - 1]);
         }
     }
 }
