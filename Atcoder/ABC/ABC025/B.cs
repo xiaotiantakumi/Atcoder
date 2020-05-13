5db6abd91b83db54642ffc23b037b806bebd4055
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-14 01:13:53 +0900

    ABC025のB回答

--- Atcoder/ABC/ABC025/B.cs
+++ Atcoder/ABC/ABC025/B.cs
@@ -1,9 +1,42 @@
-﻿namespace Atcoder.ABC.ABC025
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC025
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var NAB = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int ret = 0;
+            for (int i = 0; i < NAB[0]; i++)
+            {
+                var tmp = Console.ReadLine().Split();
+                var s = tmp[0];
+                var d = int.Parse(tmp[1]);
+                d = Math.Min(Math.Max(NAB[1], d), NAB[2]);
+                if (s == "West")
+                {
+                    ret -= d;
+                }
+                else
+                {
+                    ret += d;
+                }
+            }
+
+            if (ret == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (ret < 0)
+            {
+                Console.WriteLine("West " + Math.Abs(ret));
+            }
+            else
+            {
+                Console.WriteLine("East " + Math.Abs(ret));
+            }
         }
     }
 }
