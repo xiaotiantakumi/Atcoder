d5d91715b58d6fae0a4a32e245b477df125ff50d
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-15 01:50:40 +0900

    ABC024のA回答

--- Atcoder/ABC/ABC024/A.cs
+++ Atcoder/ABC/ABC024/A.cs
@@ -1,10 +1,21 @@
-﻿namespace Atcoder.ABC.ABC024
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC024
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var ABCK = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var ST = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            if (ST.Sum() >= ABCK[3])
+            {
+                ABCK[0] -= ABCK[2];
+                ABCK[1] -= ABCK[2];
+            }
+
+            Console.WriteLine(ABCK[0] * ST[0] + ABCK[1] * ST[1]);
         }
     }
 }
