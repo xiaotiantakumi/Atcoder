785db1ea15e93f1a4292ca832971916772c5ad4b
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-15 02:02:53 +0900

    ABC024のB回答

--- Atcoder/ABC/ABC024/B.cs
+++ Atcoder/ABC/ABC024/B.cs
@@ -1,9 +1,34 @@
-﻿namespace Atcoder.ABC.ABC024
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC024
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var NT = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            List<int> As = new List<int>();
+            int ret = 0;
+            for (int i = 0; i < NT[0]; i++)
+            {
+                var A = int.Parse(Console.ReadLine());
+                As.Add(A);
+            }
+            for (int i = 0; i < NT[0]; i++)
+            {
+                if (i == NT[0] - 1)
+                {
+                    ret += NT[1];
+                }
+                else
+                {
+                    ret += Math.Min(NT[1], As[i + 1] - As[i]);
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
