24e44672e749d58d1f28e3410e4ec472e0fc0333
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-17 14:26:38 +0900

    ABC144のC回答

--- Atcoder/ABC/ABC144/C.cs
+++ Atcoder/ABC/ABC144/C.cs
@@ -1,9 +1,24 @@
-﻿namespace Atcoder.ABC144
+﻿using System;
+
+namespace Atcoder.ABC144
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var N = long.Parse(Console.ReadLine());
+            var len = (long)Math.Ceiling(Math.Sqrt(N));
+            long dis = long.MaxValue;
+            for (int i = 1; i <= len; i++)
+            {
+                if (N % i == 0)
+                {
+                    long j = N / i;
+                    dis = Math.Min(dis, i + j - 2);
+                }
+            }
+
+            Console.WriteLine(dis);
         }
     }
 }
