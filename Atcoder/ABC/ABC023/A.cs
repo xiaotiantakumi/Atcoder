36fd1e64b6a88a06ec31d06bf1e1b4c9c9e4e92b
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-19 12:50:46 +0900

    ABC023のA回答

--- Atcoder/ABC/ABC023/A.cs
+++ Atcoder/ABC/ABC023/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC.ABC023
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC023
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var r = Console.ReadLine();
+            Console.WriteLine(int.Parse(r.First().ToString()) + int.Parse(r.Last().ToString()));
         }
     }
 }
