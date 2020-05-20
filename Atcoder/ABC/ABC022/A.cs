2050a75f3c6e3d13e6634ef22bb0c3d26af62743
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-20 19:42:14 +0900

    ABC022のA回答

--- Atcoder/ABC/ABC022/A.cs
+++ Atcoder/ABC/ABC022/A.cs
@@ -1,10 +1,25 @@
-﻿namespace Atcoder.ABC.ABC022
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC022
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var NST = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int val = 0;
+            int ret = 0;
+            for (int i = 0; i < NST[0]; i++)
+            {
+                val += int.Parse(Console.ReadLine());
+                if (val >= NST[1] && val <= NST[2])
+                {
+                    ret++;
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
