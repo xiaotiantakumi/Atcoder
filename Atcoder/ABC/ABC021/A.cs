08f54ad6561365842c2e1bc044dc3c72221b8b24
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-21 19:33:40 +0900

    ABC021のA回答

--- Atcoder/ABC/ABC021/A.cs
+++ Atcoder/ABC/ABC021/A.cs
@@ -1,10 +1,26 @@
-﻿namespace Atcoder.ABC.ABC021
+﻿using System;
+
+namespace Atcoder.ABC.ABC021
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var N = int.Parse(Console.ReadLine());
+            if (N % 2 == 0)
+            {
+                Console.WriteLine(N / 2);
+
+            }
+            else
+            {
+                Console.WriteLine(N / 2 + 1);
+                Console.WriteLine(1);
+            }
+            for (int i = 0; i < N / 2; i++)
+            {
+                Console.WriteLine(2);
+            }
         }
     }
 }
