219a63e3cb73758784c84acc0417b5abc6c6b362
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-22 20:08:03 +0900

    ABC020のA回答

--- Atcoder/ABC/ABC020/A.cs
+++ Atcoder/ABC/ABC020/A.cs
@@ -1,10 +1,20 @@
-﻿namespace Atcoder.ABC.ABC020
+﻿using System;
+
+namespace Atcoder.ABC.ABC020
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var r = int.Parse(Console.ReadLine());
+            if (r == 1)
+            {
+                Console.WriteLine("ABC");
+            }
+            else
+            {
+                Console.WriteLine("chokudai");
+            }
         }
     }
 }
