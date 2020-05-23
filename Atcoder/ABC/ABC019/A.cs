8691d46d64a668b7f90312fa421f26d0fdbbab14
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-23 22:35:27 +0900

    ABC019のA回答

--- Atcoder/ABC/ABC019/A.cs
+++ Atcoder/ABC/ABC019/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC.ABC019
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC019
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var abc = Console.ReadLine().Split().Select(int.Parse).OrderBy(x => x).ToArray();
+            Console.WriteLine(abc[1]);
         }
     }
 }
