9b05547e3b5a11b97c475cab489dedde6836e0d8
Takumi Oda <xiaotiantakumi@gmail.com>
2020-05-27 21:17:09 +0900

    PAST1のBC回答

--- Atcoder/PAST/1/B.cs
+++ Atcoder/PAST/1/B.cs
@@ -1,9 +1,37 @@
-﻿namespace Atcoder.PAST._1
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atcoder.PAST._1
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            List<int> items = new List<int>();
+            for (int i = 0; i < N; i++)
+            {
+                items.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < N - 1; i++)
+            {
+                var ret = items[i + 1] - items[i];
+                if (ret > 0)
+                {
+                    Console.WriteLine("up " + Math.Abs(ret));
+                }
+                else if (ret < 0)
+                {
+                    Console.WriteLine("down " + Math.Abs(ret));
+
+                }
+                else
+                {
+                    Console.WriteLine("stay");
+
+                }
+            }
         }
     }
 }
--- Atcoder/PAST/1/C.cs
+++ Atcoder/PAST/1/C.cs
@@ -1,9 +1,14 @@
-﻿namespace Atcoder.PAST._1
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.PAST._1
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var array = Console.ReadLine().Split().Select(int.Parse).OrderByDescending(x => x).ToArray();
+            Console.WriteLine(array[2]);
         }
     }
 }
