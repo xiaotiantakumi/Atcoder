8d8cda752bb76f68b939bf0eae9e6dfd0ded73cc
Takumi Oda <xiaotiantakumi@gmail.com>
2020-06-02 21:05:08 +0900

    ABC169のAB回答

--- Atcoder/ABC/ABC169/A.cs
+++ Atcoder/ABC/ABC169/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC.ABC169
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC169
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var AB = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(AB[0] * AB[1]);
         }
     }
 }
--- Atcoder/ABC/ABC169/B.cs
+++ Atcoder/ABC/ABC169/B.cs
@@ -1,9 +1,40 @@
-﻿namespace Atcoder.ABC.ABC169
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC169
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            long limit = 1000000000000000000;
+            var N = int.Parse(Console.ReadLine());
+            var As = Console.ReadLine().Split().Select(long.Parse).OrderBy(x => x).ToArray();
+            long ret = 1;
+            if (As[0] == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            try
+            {
+                checked
+                {
+
+                    for (int i = 0; i < N; i++)
+                    {
+                        ret *= As[i];
+                    }
+
+                    if (ret > limit) throw new Exception();
+                    Console.WriteLine(ret);
+                }
+            }
+            catch
+            {
+                Console.WriteLine(-1);
+            }
         }
     }
 }
