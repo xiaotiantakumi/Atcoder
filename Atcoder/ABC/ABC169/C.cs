031563634acb574da1c8d9a0d3788508d7157a31
Takumi Oda <xiaotiantakumi@gmail.com>
2020-06-03 20:25:54 +0900

    ABC169のC回答

--- Atcoder/ABC/ABC169/C.cs
+++ Atcoder/ABC/ABC169/C.cs
@@ -1,9 +1,14 @@
-﻿namespace Atcoder.ABC.ABC169
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC169
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var r = Console.ReadLine().Split().Select(decimal.Parse).ToArray();
+            Console.WriteLine(Math.Truncate(r[0] * r[1]));
         }
     }
 }
