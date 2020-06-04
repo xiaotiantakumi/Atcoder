200ceee38e2453e14b4de8f2a21c372a1edb10c6
Takumi Oda <xiaotiantakumi@gmail.com>
2020-06-04 17:53:36 +0900

    Util修正

--- Atcoder/Util.cs
+++ Atcoder/Util.cs
@@ -77,5 +77,24 @@ namespace ConsoleApp1
 
             return default(T);
         }
+
+        static IEnumerable<long> GetPrimes(long n)
+        {
+            while ((n & 1) == 0)
+            {
+                n >>= 1;
+                yield return 2;
+            }
+            for (long i = 3; i * i <= n; i += 2)
+            {
+                while (n % i == 0)
+                {
+                    n /= i;
+                    yield return i;
+                }
+            }
+            if (n != 1) yield return n;
+        }
+
     }
 }
