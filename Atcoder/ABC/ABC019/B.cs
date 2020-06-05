41127c1e967188f965eb126343d08890b0542b35
Takumi Oda <xiaotiantakumi@gmail.com>
2020-06-05 14:42:26 +0900

    ABC019のB回答

--- Atcoder/ABC/ABC019/B.cs
+++ Atcoder/ABC/ABC019/B.cs
@@ -1,9 +1,37 @@
-﻿namespace Atcoder.ABC.ABC019
+﻿using System;
+using System.Text;
+
+namespace Atcoder.ABC.ABC019
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var s = Console.ReadLine();
+            char current = s[0];
+            int count = 1;
+            StringBuilder ret = new StringBuilder();
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (current == s[i])
+                {
+                    count++;
+                }
+                else
+                {
+
+                    ret.Append(current + count.ToString());
+                    current = s[i];
+                    count = 1;
+                }
+
+                if (i == s.Length - 1)
+                {
+                    ret.Append(current + count.ToString());
+                }
+            }
+
+            Console.WriteLine(ret.ToString());
         }
     }
 }
