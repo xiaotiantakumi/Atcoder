5683082a8bd520285e0140abbd46ac169e3715da
Takumi Oda <xiaotiantakumi@gmail.com>
2020-06-05 15:04:56 +0900

    ABC098のC回答

--- Atcoder/ABC/ABC098/C.cs
+++ Atcoder/ABC/ABC098/C.cs
@@ -1,9 +1,26 @@
-﻿namespace Atcoder.ABC098
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC098
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var S = Console.ReadLine();
+            var array = new List<int>();
+            int count = S.Skip(1).Count(x => x == 'E');
+            array.Add(count);
+            for (int i = 1; i < N; i++)
+            {
+                if (S[i] == 'E') --count;
+                if (S[i - 1] == 'W') ++count;
+                array.Add(count);
+            }
+
+            Console.WriteLine(array.Min());
         }
     }
 }
