c949eb3275aa6cd3d45332f1674cd165f244f216
Takumi Oda <xiaotiantakumi@gmail.com>
2020-06-05 14:41:55 +0900

    ABC018のA回答

--- Atcoder/ABC/ABC018/A.cs
+++ Atcoder/ABC/ABC018/A.cs
@@ -1,10 +1,19 @@
-﻿namespace Atcoder.ABC.ABC018
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC018
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var A = int.Parse(Console.ReadLine());
+            var B = int.Parse(Console.ReadLine());
+            var C = int.Parse(Console.ReadLine());
+            var list = new int[3] { A, B, C }.OrderByDescending(_ => _).ToList();
+            Console.WriteLine(list.IndexOf(A) + 1);
+            Console.WriteLine(list.IndexOf(B) + 1);
+            Console.WriteLine(list.IndexOf(C) + 1);
         }
     }
 }
