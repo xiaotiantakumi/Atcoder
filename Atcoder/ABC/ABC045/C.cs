f888fac88f152311a75cb05b5c3b31783582e469
Takumi Oda <xiaotiantakumi@gmail.com>
2020-06-07 14:38:40 +0900

    ABC045のC回答

--- Atcoder/ABC/ABC045/C.cs
+++ Atcoder/ABC/ABC045/C.cs
@@ -1,9 +1,35 @@
-﻿namespace Atcoder.ABC045
+﻿using System;
+
+namespace Atcoder.ABC045
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var S = Console.ReadLine();
+            // {0, 1, ..., n-1} の部分集合の全探索
+            int n = S.Length - 1;
+            long sum = 0;
+            for (int bit = 0; bit < (1 << n); ++bit)
+            {
+                string part = S[0].ToString();
+                for (int i = 0; i < n; ++i)
+                {
+                    if ((bit & (1 << i)) != 0)
+                    {
+                        sum += long.Parse(part);
+                        part = S[i + 1].ToString();
+                    }
+                    else
+                    {
+                        part += S[i + 1];
+                    }
+                }
+
+                sum += long.Parse(part);
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
