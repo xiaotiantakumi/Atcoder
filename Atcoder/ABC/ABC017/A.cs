b2a5045cff7e652fe1defd6fac6a2b8d50553385
Takumi Oda <xiaotiantakumi@gmail.com>
2020-06-08 21:50:31 +0900

    ABC017のAB回答

--- Atcoder/ABC/ABC017/A.cs
+++ Atcoder/ABC/ABC017/A.cs
@@ -1,10 +1,17 @@
-﻿namespace Atcoder.ABC.ABC017
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC017
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var r1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var r2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var r3 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var sum = r1[0] * r1[1] / 10 + r2[0] * r2[1] / 10 + r3[0] * r3[1] / 10;
+            Console.WriteLine(sum);
         }
     }
 }
--- Atcoder/ABC/ABC017/B.cs
+++ Atcoder/ABC/ABC017/B.cs
@@ -1,9 +1,20 @@
-﻿namespace Atcoder.ABC.ABC017
+﻿using System;
+
+namespace Atcoder.ABC.ABC017
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var r = Console.ReadLine();
+            if (string.IsNullOrEmpty(r.Replace("ch", "").Replace("o", "").Replace("k", "").Replace("u", "")))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
         }
     }
 }
