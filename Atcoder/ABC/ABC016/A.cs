6f9d74abf8135902d5fefe0fe2ed4968ec099913
Takumi Oda <xiaotiantakumi@gmail.com>
2020-06-10 21:56:49 +0900

    ABC016のA回答

--- Atcoder/ABC/ABC016/A.cs
+++ Atcoder/ABC/ABC016/A.cs
@@ -1,10 +1,21 @@
-﻿namespace Atcoder.ABC.ABC016
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC016
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            if (r[0] % r[1] == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
         }
     }
 }
