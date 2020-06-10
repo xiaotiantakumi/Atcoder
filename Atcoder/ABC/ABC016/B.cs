16695d472ae31d3816e5da87576711e74a8cef1f
Takumi Oda <xiaotiantakumi@gmail.com>
2020-06-10 22:02:19 +0900

    ABC016のB回答

--- Atcoder/ABC/ABC016/B.cs
+++ Atcoder/ABC/ABC016/B.cs
@@ -1,9 +1,35 @@
-﻿namespace Atcoder.ABC.ABC016
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC016
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var A = r[0];
+            var B = r[1];
+            var C = r[2];
+            var ptn1 = A + B == C;
+            var ptn2 = A - B == C;
+
+            if (ptn1 && ptn2)
+            {
+                Console.WriteLine("?");
+            }
+            else if (ptn1)
+            {
+                Console.WriteLine("+");
+            }
+            else if (ptn2)
+            {
+                Console.WriteLine("-");
+            }
+            else
+            {
+                Console.WriteLine("!");
+            }
         }
     }
 }
