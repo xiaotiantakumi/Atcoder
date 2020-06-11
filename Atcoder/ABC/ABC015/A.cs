a069e5af2652f54826a73564316091d596957f35
Takumi Oda <xiaotiantakumi@gmail.com>
2020-06-11 17:06:46 +0900

    ABC015のA回答

--- Atcoder/ABC/ABC015/A.cs
+++ Atcoder/ABC/ABC015/A.cs
@@ -1,10 +1,17 @@
-﻿namespace Atcoder.ABC.ABC015
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC015
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            List<string> lst = new List<string>();
+            lst.Add(Console.ReadLine());
+            lst.Add(Console.ReadLine());
+            Console.WriteLine(lst.OrderByDescending(x => x.Length).First());
         }
     }
 }
