8c98970813894effea0d80141f795255952efe76
Takumi Oda <xiaotiantakumi@gmail.com>
2020-06-13 00:35:21 +0900

    ABC014のA回答

--- Atcoder/ABC/ABC014/A.cs
+++ Atcoder/ABC/ABC014/A.cs
@@ -1,10 +1,22 @@
-﻿namespace Atcoder.ABC.ABC014
+﻿using System;
+
+namespace Atcoder.ABC.ABC014
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var c = a % b;
+            if (c == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(b - c);
+            }
         }
     }
 }
