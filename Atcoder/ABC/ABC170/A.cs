ad514de95b3ce7ef7cb44616ca25cbea18e6d9d8
Takumi Oda <xiaotiantakumi@gmail.com>
2020-06-18 18:40:14 +0900

    ABC170のAB回答

--- Atcoder/ABC/ABC170/A.cs
+++ Atcoder/ABC/ABC170/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC.ABC170
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC170
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var x = Console.ReadLine().Split().Select(int.Parse).ToList();
+            Console.WriteLine(x.IndexOf(0) + 1);
         }
     }
 }
--- Atcoder/ABC/ABC170/B.cs
+++ Atcoder/ABC/ABC170/B.cs
@@ -1,9 +1,25 @@
-﻿namespace Atcoder.ABC.ABC170
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC170
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var XY = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i <= XY[0]; i++)
+            {
+                var twoLeg = i;
+                var fourLeg = XY[0] - i;
+                if (twoLeg * 2 + fourLeg * 4 == XY[1])
+                {
+                    Console.WriteLine("Yes");
+                    return;
+                }
+            }
+
+            Console.WriteLine("No");
         }
     }
 }
