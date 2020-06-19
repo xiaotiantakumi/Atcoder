4cb31d16a6115998634889b531a33eecd08cf8c3
Takumi Oda <xiaotiantakumi@gmail.com>
2020-06-19 23:18:12 +0900

    ABC013のA回答

--- Atcoder/ABC/ABC013/A.cs
+++ Atcoder/ABC/ABC013/A.cs
@@ -1,10 +1,15 @@
-﻿namespace Atcoder.ABC.ABC013
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC013
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var list = "ABCDE".ToList();
+            var X = Console.ReadLine();
+            Console.WriteLine(list.IndexOf(X.ToCharArray()[0]) + 1);
         }
     }
 }
