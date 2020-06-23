82783fb9c3b259e3bcb6d21741cd6d3b863141ba
Takumi Oda <xiaotiantakumi@gmail.com>
2020-06-23 18:25:26 +0900

    ABC071のB回答

--- Atcoder/ABC/ABC171/B.cs
+++ Atcoder/ABC/ABC171/B.cs
@@ -1,9 +1,15 @@
-﻿namespace Atcoder.ABC.ABC171
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC171
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var NK = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var prices = Console.ReadLine().Split().Select(int.Parse).OrderBy(x => x).ToArray();
+            Console.WriteLine(prices.Take(NK[1]).Sum());
         }
     }
 }
