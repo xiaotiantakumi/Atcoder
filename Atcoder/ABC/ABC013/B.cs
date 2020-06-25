2e7e19717e6414113ea63c4428d9e51f2e59f494
Takumi Oda <xiaotiantakumi@gmail.com>
2020-06-25 22:05:55 +0900

    ABC013のB回答

--- Atcoder/ABC/ABC013/B.cs
+++ Atcoder/ABC/ABC013/B.cs
@@ -1,9 +1,17 @@
-﻿namespace Atcoder.ABC.ABC013
+﻿using System;
+
+namespace Atcoder.ABC.ABC013
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var A = int.Parse(Console.ReadLine());
+            var B = int.Parse(Console.ReadLine());
+            var big = Math.Max(A, B);
+            var small = Math.Min(A, B);
+
+            Console.WriteLine(Math.Min(small + (10 - big), Math.Abs(big - small)));
         }
     }
 }
