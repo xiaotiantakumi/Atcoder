6c237d5db96535c2b4e6356627ac62895fa54bd4
Takumi Oda <xiaotiantakumi@gmail.com>
2020-06-26 21:33:10 +0900

    ABC012のAB回答

--- Atcoder/ABC/ABC012/A.cs
+++ Atcoder/ABC/ABC012/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC.ABC012
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC012
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var AB = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(AB[1] + " " + AB[0]);
         }
     }
 }
--- Atcoder/ABC/ABC012/B.cs
+++ Atcoder/ABC/ABC012/B.cs
@@ -1,9 +1,15 @@
-﻿namespace Atcoder.ABC.ABC012
+﻿using System;
+
+namespace Atcoder.ABC.ABC012
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            TimeSpan ts = new TimeSpan(0, 0, N);
+            Console.WriteLine(ts.Hours.ToString("00") + ":" + ts.Minutes.ToString("00") + ":" + ts.Seconds.ToString("00"));
+
         }
     }
 }
