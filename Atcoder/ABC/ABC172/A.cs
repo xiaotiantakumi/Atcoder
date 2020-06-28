3d88fef95ead45bf636df109ba49929a15fbabe3
Takumi Oda <xiaotiantakumi@gmail.com>
2020-06-28 11:27:12 +0900

    ABC172のAB回答

--- Atcoder/ABC/ABC172/A.cs
+++ Atcoder/ABC/ABC172/A.cs
@@ -1,10 +1,13 @@
-﻿namespace Atcoder.ABC.ABC172
+﻿using System;
+
+namespace Atcoder.ABC.ABC172
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var r = int.Parse(Console.ReadLine());
+            Console.WriteLine(r + Math.Pow(r, 2) + Math.Pow(r, 3));
         }
     }
 }
--- Atcoder/ABC/ABC172/B.cs
+++ Atcoder/ABC/ABC172/B.cs
@@ -1,9 +1,23 @@
-﻿namespace Atcoder.ABC.ABC172
+﻿using System;
+
+namespace Atcoder.ABC.ABC172
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var S = Console.ReadLine();
+            var T = Console.ReadLine();
+            int ret = 0;
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] != T[i])
+                {
+                    ret++;
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
