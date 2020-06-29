93cffe813db534202dc8a1e54994650854a3beb2
Takumi Oda <xiaotiantakumi@gmail.com>
2020-06-29 23:12:19 +0900

    ABC010のA回答

--- Atcoder/ABC/ABC010/A.cs
+++ Atcoder/ABC/ABC010/A.cs
@@ -1,10 +1,13 @@
-﻿namespace Atcoder.ABC.ABC010
+﻿using System;
+
+namespace Atcoder.ABC.ABC010
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var r = Console.ReadLine();
+            Console.WriteLine(r + "pp");
         }
     }
 }
