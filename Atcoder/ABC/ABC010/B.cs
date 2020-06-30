b2a482f21950a71280c88f834cd6cbb3dcd8d0f0
Takumi Oda <xiaotiantakumi@gmail.com>
2020-06-30 12:52:52 +0900

    ABC010のB回答

--- Atcoder/ABC/ABC010/B.cs
+++ Atcoder/ABC/ABC010/B.cs
@@ -1,9 +1,30 @@
-﻿namespace Atcoder.ABC.ABC010
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC010
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var As = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int ret = 0;
+            for (int i = 0; i < N; i++)
+            {
+                var tar = As[i];
+                while (true)
+                {
+                    if (tar % 2 != 0 && tar % 3 != 2)
+                    {
+                        break;
+                    }
+                    ret++;
+                    tar--;
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
