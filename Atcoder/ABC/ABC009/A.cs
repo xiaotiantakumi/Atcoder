691cc1bf7bd92ef15fa45f0c1b1f5f88193018d3
Takumi Oda <xiaotiantakumi@gmail.com>
2020-07-01 21:47:42 +0900

    ABC009のAB回答

--- Atcoder/ABC/ABC009/A.cs
+++ Atcoder/ABC/ABC009/A.cs
@@ -1,10 +1,13 @@
-﻿namespace Atcoder.ABC.ABC009
+﻿using System;
+
+namespace Atcoder.ABC.ABC009
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var N = int.Parse(Console.ReadLine());
+            Console.WriteLine((N + (2 - 1)) / 2);
         }
     }
 }
--- Atcoder/ABC/ABC009/B.cs
+++ Atcoder/ABC/ABC009/B.cs
@@ -1,9 +1,21 @@
-﻿namespace Atcoder.ABC.ABC009
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC009
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            List<int> prices = new List<int>();
+            for (int i = 0; i < N; i++)
+            {
+                prices.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine(prices.OrderByDescending(x => x).Distinct().Skip(1).First());
         }
     }
 }
