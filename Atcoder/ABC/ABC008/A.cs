100b345e9077ad58e9818069a481784353f9d42f
Takumi Oda <xiaotiantakumi@gmail.com>
2020-07-03 19:30:05 +0900

    ABC008のAB回答

--- Atcoder/ABC/ABC008/A.cs
+++ Atcoder/ABC/ABC008/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC.ABC008
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC008
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var ST = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(ST[1] - ST[0] + 1);
         }
     }
 }
--- Atcoder/ABC/ABC008/B.cs
+++ Atcoder/ABC/ABC008/B.cs
@@ -1,9 +1,29 @@
-﻿namespace Atcoder.ABC.ABC008
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC008
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            for (int i = 0; i < N; i++)
+            {
+                var name = Console.ReadLine();
+                if (dic.ContainsKey(name))
+                {
+                    dic[name]++;
+                }
+                else
+                {
+                    dic[name] = 0;
+                }
+            }
+            var ret = dic.OrderByDescending(x => x.Value).ToList().First();
+            Console.WriteLine(ret.Key);
         }
     }
 }
