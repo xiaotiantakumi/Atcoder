d91636134165cba4fc6d21de0cd9d24e22cfb4bc
Takumi Oda <xiaotiantakumi@gmail.com>
2020-07-05 12:10:47 +0900

    ABC007のB回答

--- Atcoder/ABC/ABC007/B.cs
+++ Atcoder/ABC/ABC007/B.cs
@@ -1,9 +1,20 @@
-﻿namespace Atcoder.ABC.ABC007
+﻿using System;
+
+namespace Atcoder.ABC.ABC007
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var s = Console.ReadLine();
+            if (s == "a")
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                Console.WriteLine("a");
+            }
         }
     }
 }
