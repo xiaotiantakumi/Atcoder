b8032e52ef2360a09042eaa4cdc932a46f126fc3
Takumi Oda <xiaotiantakumi@gmail.com>
2020-07-08 21:15:18 +0900

    ABC173のAB回答

--- Atcoder/ABC/ABC173/A.cs
+++ Atcoder/ABC/ABC173/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC.ABC173
+﻿using System;
+
+namespace Atcoder.ABC.ABC173
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var N = int.Parse(Console.ReadLine());
+            N = N % 1000;
+            Console.WriteLine(1000 - N == 1000 ? 0 : 1000 - N);
         }
     }
 }
--- Atcoder/ABC/ABC173/B.cs
+++ Atcoder/ABC/ABC173/B.cs
@@ -1,9 +1,28 @@
-﻿namespace Atcoder.ABC.ABC173
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atcoder.ABC.ABC173
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            Dictionary<string, int> ret = new Dictionary<string, int>();
+            ret["AC"] = 0;
+            ret["WA"] = 0;
+            ret["TLE"] = 0;
+            ret["RE"] = 0;
+            var N = int.Parse(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                var r = Console.ReadLine();
+                ret[r]++;
+            }
+
+            foreach (var item in ret)
+            {
+                Console.WriteLine(item.Key + " x " + item.Value);
+            }
         }
     }
 }
