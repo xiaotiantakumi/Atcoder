911b4b160f197f0d2a2ef5f0c396e8c3376326f4
Takumi Oda <xiaotiantakumi@gmail.com>
2020-07-09 09:52:38 +0900

    ABC005のA回答

--- Atcoder/ABC/ABC005/A.cs
+++ Atcoder/ABC/ABC005/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC.ABC005
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC005
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var xy = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(xy[1] / xy[0]);
         }
     }
 }
