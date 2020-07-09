813fe881616580d14ebb620c6323e46d32d23467
Takumi Oda <xiaotiantakumi@gmail.com>
2020-07-09 09:55:07 +0900

    ABC005のB回答

--- Atcoder/ABC/ABC005/B.cs
+++ Atcoder/ABC/ABC005/B.cs
@@ -1,9 +1,19 @@
-﻿namespace Atcoder.ABC.ABC005
+﻿using System;
+
+namespace Atcoder.ABC.ABC005
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            int ret = int.MaxValue;
+            for (int i = 0; i < N; i++)
+            {
+                ret = Math.Min(ret, int.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
