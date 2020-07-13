99b397cc7b2a006b1fbf4af150751a53007a6086
Takumi Oda <xiaotiantakumi@gmail.com>
2020-07-13 22:52:04 +0900

    エイジングプログラミングコンテスト2020ABC回答

---
+++ Atcoder/OtherCon/エイシング プログラミング コンテスト 2020/B.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.OtherCon.エイシング_プログラミング_コンテスト_2020
+{
+    public class B : AtcoderBase
+    {
+        public override void Run()
+        {
+            var N = int.Parse(Console.ReadLine());
+            var As = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int ret = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if ((i + 1) % 2 == 0) continue;
+                if (As[i] % 2 != 0) ret++;
+            }
+
+            Console.WriteLine(ret);
+        }
+    }
+}
---
+++ Atcoder/OtherCon/エイシング プログラミング コンテスト 2020/D.cs
@@ -0,0 +1,9 @@
+﻿namespace Atcoder.OtherCon.エイシング_プログラミング_コンテスト_2020
+{
+    public class D : AtcoderBase
+    {
+        public override void Run()
+        {
+        }
+    }
+}
