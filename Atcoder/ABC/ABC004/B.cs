c967c882e60b20bd40d34f41b2c89751f681d0c7
Takumi Oda <xiaotiantakumi@gmail.com>
2020-07-15 23:00:12 +0900

    ABC004のB回答

--- Atcoder/ABC/ABC004/B.cs
+++ Atcoder/ABC/ABC004/B.cs
@@ -1,9 +1,24 @@
-﻿namespace Atcoder.ABC.ABC004
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC004
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            List<string> input = new List<string>();
+            for (int i = 0; i < 4; i++)
+            {
+                input.Add(Console.ReadLine());
+            }
+
+            for (int i = 3; i >= 0; i--)
+            {
+                var tmp = input[i];
+                Console.WriteLine(new string(tmp.Reverse().ToArray()));
+            }
         }
     }
 }
