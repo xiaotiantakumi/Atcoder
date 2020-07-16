835af6b5f1e67a5193c3a28462b51be0c1fb95a8
Takumi Oda <xiaotiantakumi@gmail.com>
2020-07-16 12:56:44 +0900

    ABC003のA回答

--- Atcoder/ABC/ABC003/A.cs
+++ Atcoder/ABC/ABC003/A.cs
@@ -1,10 +1,19 @@
-﻿namespace Atcoder.ABC.ABC003
+﻿using System;
+
+namespace Atcoder.ABC.ABC003
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var N = int.Parse(Console.ReadLine());
+            decimal ret = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                ret += i;
+            }
+
+            Console.WriteLine(ret * 10000 / N);
         }
     }
 }
