2f09b49d4d0cb7154c11797f5c77a24a4447c1d7
Takumi Oda <xiaotiantakumi@gmail.com>
2020-07-18 21:25:14 +0900

    ABC002のA回答

--- Atcoder/ABC/ABC002/A.cs
+++ Atcoder/ABC/ABC002/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC.ABC002
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC002
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var XY = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(Math.Max(XY[0], XY[1]));
         }
     }
 }
