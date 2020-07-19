b164f59d892d8bdb93a7f060ce0a6da5ea1960f3
Takumi Oda <xiaotiantakumi@gmail.com>
2020-07-19 23:54:41 +0900

    ABC002のB回答

--- Atcoder/ABC/ABC002/B.cs
+++ Atcoder/ABC/ABC002/B.cs
@@ -1,9 +1,22 @@
-﻿namespace Atcoder.ABC.ABC002
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atcoder.ABC.ABC002
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            HashSet<char> except = new HashSet<char>() { 'a', 'i', 'u', 'e', 'o' };
+            var W = Console.ReadLine().ToList();
+            StringBuilder ret = new StringBuilder();
+            foreach (var c in W)
+            {
+                if (!except.Contains(c)) ret.Append(c);
+            }
+            Console.WriteLine(ret.ToString());
         }
     }
 }
