4739c5639e46d0b1e856a8812902d0397235ba1c
Takumi Oda <xiaotiantakumi@gmail.com>
2020-08-07 18:52:29 +0900

    ABC174のA回答

--- Atcoder/ABC/ABC174/A.cs
+++ Atcoder/ABC/ABC174/A.cs
@@ -1,10 +1,20 @@
-﻿namespace Atcoder.ABC.ABC174
+﻿using System;
+
+namespace Atcoder.ABC.ABC174
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var X = int.Parse(Console.ReadLine());
+            if (X >= 30)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
