26e201b047236fc39a985958b846f5dced0a0071
Takumi Oda <xiaotiantakumi@gmail.com>
2020-08-08 23:08:12 +0900

    ABC174のB回答

--- Atcoder/ABC/ABC174/B.cs
+++ Atcoder/ABC/ABC174/B.cs
@@ -1,9 +1,27 @@
-﻿namespace Atcoder.ABC.ABC174
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC174
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var ND = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var N = ND[0];
+            var D = ND[1];
+            int ret = 0;
+            for (int i = 0; i < N; i++)
+            {
+                var XY = Console.ReadLine().Split().Select(double.Parse).ToArray();
+                var dis = Math.Sqrt(Math.Pow(XY[0], 2) + Math.Pow(XY[1], 2));
+                if (dis <= D)
+                {
+                    ret++;
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
