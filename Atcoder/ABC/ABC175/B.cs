934a6f9ca177a3385ead1e52849e8559b96ee24a
Takumi Oda <xiaotiantakumi@gmail.com>
2020-08-16 16:26:29 +0900

    ABC175のB回答

--- Atcoder/ABC/ABC175/B.cs
+++ Atcoder/ABC/ABC175/B.cs
@@ -1,9 +1,41 @@
-﻿namespace Atcoder.ABC.ABC175
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC175
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var Ls = Console.ReadLine().Split().Select(int.Parse).OrderBy(x => x).ToArray();
+            int ret = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = i + 1; j < N; j++)
+                {
+                    if (Ls[i] == Ls[j])
+                    {
+                        continue;
+                    }
+                    for (int k = j + 1; k < N; k++)
+                    {
+                        if (Ls[i] == Ls[k] || Ls[j] == Ls[k])
+                        {
+                            continue;
+                        }
+
+                        if (Ls[i] + Ls[j] > Ls[k] &&
+                            Ls[j] + Ls[k] > Ls[i] &&
+                            Ls[k] + Ls[i] > Ls[j])
+                        {
+                            ret++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
