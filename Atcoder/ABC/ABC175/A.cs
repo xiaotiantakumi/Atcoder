6bde48a8e81a6758b86ad744bdc8d77711b59607
Takumi Oda <xiaotiantakumi@gmail.com>
2020-08-16 16:07:31 +0900

    ABC175のA回答

--- Atcoder/ABC/ABC175/A.cs
+++ Atcoder/ABC/ABC175/A.cs
@@ -1,10 +1,29 @@
-﻿namespace Atcoder.ABC.ABC175
+﻿using System;
+
+namespace Atcoder.ABC.ABC175
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var S = Console.ReadLine();
+            int ret = 0;
+            if (S == "RRR")
+            {
+                Console.WriteLine(3);
+            }
+            else if (S.Contains("RR"))
+            {
+                Console.WriteLine(2);
+            }
+            else if (S.Contains("R"))
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
         }
     }
 }
