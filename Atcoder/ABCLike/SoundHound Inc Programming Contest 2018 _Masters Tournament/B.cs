fad98d942b31963313179dad7be8b0816afb29f6
Takumi Oda <xiaotiantakumi@gmail.com>
2020-08-21 23:20:19 +0900

    フォルダ作成

---
+++ Atcoder/ABCLike/SoundHound Inc Programming Contest 2018 _Masters Tournament/B.cs
@@ -0,0 +1,10 @@
+﻿namespace Atcoder.ABCLike.SoundHound_Inc_Programming_Contest_2018__Masters_Tournament
+{
+    public class B : AtcoderBase
+    {
+        public override void Run()
+        {
+            
+        }
+    }
+}
---
+++ Atcoder/ABCLike/SoundHound Inc Programming Contest 2018 _Masters Tournament/D.cs
@@ -0,0 +1,9 @@
+﻿namespace Atcoder.ABCLike.SoundHound_Inc_Programming_Contest_2018__Masters_Tournament
+{
+    public class D : AtcoderBase
+    {
+        public override void Run()
+        {
+        }
+    }
+}
