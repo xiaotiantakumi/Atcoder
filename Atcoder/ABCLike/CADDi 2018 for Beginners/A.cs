c0bfe9de5bafe3a685427746743e9008b3df2303
Takumi Oda <xiaotiantakumi@gmail.com>
2020-08-22 17:36:12 +0900

    CADDi 2018 for BeginnersのA回答

--- Atcoder/ABCLike/CADDi 2018 for Beginners/A.cs
+++ Atcoder/ABCLike/CADDi 2018 for Beginners/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABCLike.CADDi_2018_for_Beginners
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABCLike.CADDi_2018_for_Beginners
 {
     public class A : AtcoderBase
     {
         public override void Run()
         {
-            
+            var s = Console.ReadLine();
+            Console.WriteLine(s.Count(x => x == '2'));
         }
     }
 }
