edc02dd84fe5b4c5d98274033433928b6301581e
Takumi Oda <xiaotiantakumi@gmail.com>
2020-08-28 20:15:20 +0900

    ABC021のB回答

--- Atcoder/ABC/ABC021/B.cs
+++ Atcoder/ABC/ABC021/B.cs
@@ -1,9 +1,27 @@
-﻿namespace Atcoder.ABC.ABC021
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC021
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var ab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var K = int.Parse(Console.ReadLine());
+            var Ps = Console.ReadLine().Split().Select(int.Parse).ToList();
+            Ps.AddRange(ab);
+            var count = Ps.Count();
+            var count2 = Ps.Distinct().Count();
+            if (count == count2)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
         }
     }
 }
