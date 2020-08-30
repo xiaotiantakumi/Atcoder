f810fbf52658ef3aa9f5e6b107f737a487f90ca9
Takumi Oda <xiaotiantakumi@gmail.com>
2020-08-30 15:29:42 +0900

    ABC177のA回答

--- Atcoder/ABC/ABC177/A.cs
+++ Atcoder/ABC/ABC177/A.cs
@@ -1,10 +1,21 @@
-﻿namespace Atcoder.ABC.ABC177
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC177
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var DTS = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            if (DTS[0] <= DTS[1] * DTS[2])
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
