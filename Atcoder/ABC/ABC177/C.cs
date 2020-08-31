8c23f37fb9d69741d0feac1b78a2a2d187347446
Takumi Oda <xiaotiantakumi@gmail.com>
2020-09-01 00:25:43 +0900

    ABC177のC回答

--- Atcoder/ABC/ABC177/C.cs
+++ Atcoder/ABC/ABC177/C.cs
@@ -1,9 +1,28 @@
-﻿namespace Atcoder.ABC.ABC177
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC177
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var As = Console.ReadLine().Split().Select(decimal.Parse).ToArray();
+            decimal sumAs = As.Sum();
+            decimal ret = 0;
+            for (int i = 0; i < N - 1; i++)
+            {
+                if (i == N - 2)
+                {
+                    ret = (ret + As[i] * As[i + 1] % COMDIV) % COMDIV;
+                    break;
+                }
+                sumAs -= As[i];
+                ret = (ret + As[i] * sumAs % COMDIV) % COMDIV;
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
