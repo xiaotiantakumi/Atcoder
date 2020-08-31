254b5df4d26895e9577bb1598363f19105b6eb83
Takumi Oda <xiaotiantakumi@gmail.com>
2020-08-31 21:30:24 +0900

    ABC177のB回答

--- Atcoder/ABC/ABC177/B.cs
+++ Atcoder/ABC/ABC177/B.cs
@@ -1,9 +1,29 @@
-﻿namespace Atcoder.ABC.ABC177
+﻿using System;
+
+namespace Atcoder.ABC.ABC177
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var S = Console.ReadLine();
+            var T = Console.ReadLine();
+            var count = S.Length - T.Length;
+            int retMatch = -1;
+            for (int i = 0; i <= count; i++)
+            {
+                int cntMatch = 0;
+                for (int j = 0; j < T.Length; j++)
+                {
+                    if (S[i + j] == T[j])
+                    {
+                        cntMatch++;
+                    }
+                }
+                retMatch = Math.Max(retMatch, cntMatch);
+            }
+
+            Console.WriteLine(T.Length - retMatch);
         }
     }
 }
