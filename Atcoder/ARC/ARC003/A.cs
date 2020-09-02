da112a311847791291773da2361e7c9bd20de83a
Takumi Oda <xiaotiantakumi@gmail.com>
2020-09-02 21:43:22 +0900

    ARC003のA回答

--- Atcoder/ARC/ARC003/A.cs
+++ Atcoder/ARC/ARC003/A.cs
@@ -1,10 +1,35 @@
-﻿namespace Atcoder.ARC.ARC003
+﻿using System;
+
+namespace Atcoder.ARC.ARC003
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var N = int.Parse(Console.ReadLine());
+            var r = Console.ReadLine();
+            double ret = 0;
+            for (int i = 0; i < N; i++)
+            {
+                switch (r[i])
+                {
+                    case 'A':
+                        ret += 4;
+                        break;
+                    case 'B':
+                        ret += 3;
+                        break;
+                    case 'C':
+                        ret += 2;
+                        break;
+                    case 'D':
+                        ret += 1;
+                        break;
+                }
+            }
+
+            ret /= r.Length;
+            Console.WriteLine(ret);
         }
     }
 }
