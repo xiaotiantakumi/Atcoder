041ddf0f9957b25b9cf0d7ea7b590e32711627a8
Takumi Oda <xiaotiantakumi@gmail.com>
2020-09-09 22:54:56 +0900

    indeed_A

--- Atcoder/Other/Indeedなう（予選B）/A.cs
+++ Atcoder/Other/Indeedなう（予選B）/A.cs
@@ -1,9 +1,16 @@
-﻿namespace Atcoder.Other
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.Other
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
+            var xy = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var xy2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var ret = Math.Abs(xy[0] - xy2[0]) + Math.Abs(xy[1] - xy2[1]) + 1;
+            Console.WriteLine(ret);
         }
     }
 }
