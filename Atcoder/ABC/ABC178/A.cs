b2c9940c67d55d170d00f3b8db0b27a1958b33bd
Takumi Oda <xiaotiantakumi@gmail.com>
2020-09-14 22:14:14 +0900

    ABC178のAB回答

--- Atcoder/ABC/ABC178/A.cs
+++ Atcoder/ABC/ABC178/A.cs
@@ -1,10 +1,13 @@
-﻿namespace Atcoder.ABC.ABC178
+﻿using System;
+
+namespace Atcoder.ABC.ABC178
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var x = int.Parse(Console.ReadLine());
+            Console.WriteLine(x == 0 ? 1 : 0);
         }
     }
 }
--- Atcoder/ABC/ABC178/B.cs
+++ Atcoder/ABC/ABC178/B.cs
@@ -1,9 +1,18 @@
-﻿namespace Atcoder.ABC.ABC178
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC178
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var abcd = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            var ret1 = abcd[0] * abcd[2];
+            var ret2 = abcd[0] * abcd[3];
+            var ret3 = abcd[1] * abcd[2];
+            var ret4 = abcd[1] * abcd[3];
+            Console.WriteLine(Math.Max(Math.Max(Math.Max(ret1, ret2), ret3), ret4));
         }
     }
 }
