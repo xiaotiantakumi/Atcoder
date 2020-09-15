8b6405295a6d8916f3eba76f2e66754e028ba34f
Takumi Oda <xiaotiantakumi@gmail.com>
2020-09-15 22:50:30 +0900

    天下一プログラマーコンテスト2014予選AのA回答

--- Atcoder/Other/天下一プログラマーコンテスト2014予選A/A.cs
+++ Atcoder/Other/天下一プログラマーコンテスト2014予選A/A.cs
@@ -1,10 +1,17 @@
-﻿namespace Atcoder.Other.天下一プログラマーコンテスト2014予選A
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.Other.天下一プログラマーコンテスト2014予選A
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-
+            int c = 1000;
+            int i = 0;
+            List<string> s = Enumerable.Repeat(1, c).Select(x_ => (++i).ToString()).OrderBy(x => x).ToList();
+            Console.WriteLine(string.Join("\n", s));
         }
     }
 }
