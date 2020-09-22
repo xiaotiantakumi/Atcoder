3d25bda8746e41f65512dce54d4cf78e1b345c53
Takumi Oda <xiaotiantakumi@gmail.com>
2020-09-22 11:17:49 +0900

    diverta 2019 Programming ContestのA回答

--- Atcoder/ARC/diverta 2019 Programming Contest/A.cs
+++ Atcoder/ARC/diverta 2019 Programming Contest/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ARC.diverta_2019_Programming_Contest
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ARC.diverta_2019_Programming_Contest
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var NK = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(NK[0] - NK[1] + 1);
         }
     }
 }
