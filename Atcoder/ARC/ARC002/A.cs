c7e0d80fe333ef6c72e110817d6012b9b81e3a98
Takumi Oda <xiaotiantakumi@gmail.com>
2020-09-24 22:45:34 +0900

    ARCのA回答

--- Atcoder/ARC/ARC002/A.cs
+++ Atcoder/ARC/ARC002/A.cs
@@ -1,10 +1,29 @@
-﻿namespace Atcoder.ARC.ARC002
+﻿using System;
+
+namespace Atcoder.ARC.ARC002
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var S = Console.ReadLine().ToUpper();
+            var ict = "ICT";
+            int i = 0;
+            foreach (var s in S)
+            {
+                if (s == ict[i])
+                {
+                    i++;
+                }
+
+                if (i == 3)
+                {
+                    Console.WriteLine("YES");
+                    return;
+                }
+            }
+
+            Console.WriteLine("NO");
         }
     }
 }
