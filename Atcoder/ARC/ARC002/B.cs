7252960dfc2db5b83b3d167a28c9e2e479b9fb45
Takumi Oda <xiaotiantakumi@gmail.com>
2020-09-27 11:43:47 +0900

    ARC002のB回答

--- Atcoder/ARC/ARC002/B.cs
+++ Atcoder/ARC/ARC002/B.cs
@@ -1,9 +1,22 @@
-﻿namespace Atcoder.ARC.ARC002
+﻿using System;
+
+namespace Atcoder.ARC.ARC002
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var date = DateTime.Parse(Console.ReadLine());
+            while (true)
+            {
+                var val = (double)date.Year / date.Month / date.Day;
+                if (val == (int)val)
+                {
+                    Console.WriteLine(date.ToString("yyyy/MM/dd"));
+                    return;
+                }
+                date = date.AddDays(1);
+            }
         }
     }
 }
