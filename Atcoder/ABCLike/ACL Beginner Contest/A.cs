dab7d2319efb802a1d640ae66a7633d5ad23632d
Takumi Oda <xiaotiantakumi@gmail.com>
2020-09-28 00:46:57 +0900

    ACLのA回答

---
+++ Atcoder/ABCLike/ACL Beginner Contest/A.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABCLike.ACL_Beginner_Contest
+{
+    public class A : AtcoderBase
+    {
+        public override void Run()
+        {
+            var K = int.Parse(Console.ReadLine());
+            var ret = Enumerable.Repeat("ACL", K).ToArray();
+            Console.WriteLine(string.Join("", ret));
+        }
+    }
+}
