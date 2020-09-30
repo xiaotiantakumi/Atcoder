b24722abac8ada1a07b0522a2af8e355e90a3d71
Takumi Oda <xiaotiantakumi@gmail.com>
2020-09-30 14:32:54 +0900

    ARC046のA回答

---
+++ Atcoder/ARC/ARC046/A.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ARC.ARC046
+{
+    public class A : AtcoderBase
+    {
+        public override  void Run()
+        {
+            var N = int.Parse(Console.ReadLine());
+            int cnt = 0;
+            long current = 1;
+            while (cnt < N)
+            {
+                var first = current.ToString().First();
+                bool flg = true;
+                foreach (var c in current.ToString())
+                {
+                    if (c != first)
+                    {
+                        flg = false;
+                        break;
+                    }
+                }
+
+                if (flg)
+                {
+                    cnt++;
+                }
+                current++;
+            }
+
+            Console.WriteLine(current - 1);
+        }
+    }
+}
---
+++ Atcoder/ARC/ARC046/D.cs
@@ -0,0 +1,9 @@
+﻿namespace Atcoder.ARC.ARC046
+{
+    public class D : AtcoderBase
+    {
+        public override void Run()
+        {
+        }
+    }
+}
