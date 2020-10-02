1176e6dc0c6876a0eee437a2e86cca63cf35d369
Takumi Oda <xiaotiantakumi@gmail.com>
2020-10-02 19:16:47 +0900

    フォルダ作成

---
+++ Atcoder/Other/第1回 ドワンゴからの挑戦状 予選/A.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Atcoder.Other.第1回_ドワンゴからの挑戦状_予選
+{
+    public class A : AtcoderBase
+    {
+        public override  void Run()
+        {
+        }
+    }
+}
---
+++ Atcoder/Other/第1回 ドワンゴからの挑戦状 予選/C.cs
@@ -0,0 +1,9 @@
+﻿namespace Atcoder.Other.第1回_ドワンゴからの挑戦状_予選
+{
+    public class C : AtcoderBase
+    {
+        public override void Run()
+        {
+        }
+    }
+}
