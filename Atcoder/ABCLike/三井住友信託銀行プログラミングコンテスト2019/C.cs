cd4cf5b19b330fef2a5e60b469f299b782f62769
Takumi Oda <xiaotiantakumi@gmail.com>
2020-10-04 23:44:38 +0900

    三井住友信託銀行プログラミングコンテスト2019のC回答

--- Atcoder/ABCLike/三井住友信託銀行プログラミングコンテスト2019/C.cs
+++ Atcoder/ABCLike/三井住友信託銀行プログラミングコンテスト2019/C.cs
@@ -1,9 +1,23 @@
-﻿namespace Atcoder.ABCLike.三井住友信託銀行プログラミングコンテスト2019
+﻿using System;
+
+namespace Atcoder.ABCLike.三井住友信託銀行プログラミングコンテスト2019
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var X = int.Parse(Console.ReadLine());
+            int f = X / 100;
+            decimal rem = X % 100;
+            decimal remC = Math.Ceiling(rem / 5);
+            if (f < (int)remC)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(1);
+            }
         }
     }
 }
