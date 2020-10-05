694c800d61ba9ab6421af1669d444814c349e74f
Takumi Oda <xiaotiantakumi@gmail.com>
2020-10-05 22:39:02 +0900

    SoundHound Inc Programming Contest 2018 _Masters TournamentのA回答

--- Atcoder/ABCLike/SoundHound Inc Programming Contest 2018 _Masters Tournament/A.cs
+++ Atcoder/ABCLike/SoundHound Inc Programming Contest 2018 _Masters Tournament/A.cs
@@ -1,10 +1,25 @@
-﻿namespace Atcoder.ABCLike.SoundHound_Inc_Programming_Contest_2018__Masters_Tournament
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABCLike.SoundHound_Inc_Programming_Contest_2018__Masters_Tournament
 {
     public class A : AtcoderBase
     {
         public override void Run()
         {
-            
+            var ab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            if (ab[0] * ab[1] == 15)
+            {
+                Console.WriteLine("*");
+            }
+            else if (ab[0] + ab[1] == 15)
+            {
+                Console.WriteLine("+");
+            }
+            else
+            {
+                Console.WriteLine("x");
+            }
         }
     }
 }
