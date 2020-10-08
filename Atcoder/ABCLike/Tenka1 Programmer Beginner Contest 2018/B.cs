7296464b463905274a2e38d83241f5ba52b546a2
Takumi Oda <xiaotiantakumi@gmail.com>
2020-10-08 12:54:29 +0900

    Tenka1 Programmer Beginner Contest 2018のＢ回答

--- Atcoder/ABCLike/Tenka1 Programmer Beginner Contest 2018/B.cs
+++ Atcoder/ABCLike/Tenka1 Programmer Beginner Contest 2018/B.cs
@@ -8,6 +8,41 @@ namespace Atcoder.ABCLike.Tenka1_Programmer_Beginner_Contest_2018
     {
         public override void Run()
         {
+            var ABK = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var A = ABK[0];
+            var B = ABK[1];
+            var K = ABK[2];
+            bool flg = true;
+            while (K != 0)
+            {
+                if (flg)
+                {
+                    if (A % 2 == 1)
+                    {
+                        A--;
+                    }
+
+                    var give = A / 2;
+                    A -= give;
+                    B += give;
+                }
+                else
+                {
+                    if (B % 2 == 1)
+                    {
+                        B--;
+                    }
+
+                    var give = B / 2;
+                    B -= give;
+                    A += give;
+                }
+
+                flg = !flg;
+                K--;
+            }
+
+            Console.WriteLine(A + " " + B);
         }
     }
 }
