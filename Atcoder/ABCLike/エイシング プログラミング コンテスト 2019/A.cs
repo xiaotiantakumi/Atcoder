6911833b45762ce294aaacd35d6cdd1d13e8d298
Takumi Oda <xiaotiantakumi@gmail.com>
2020-10-10 18:56:40 +0900

    エイジングプログラミングコンテスト2019A回答

--- Atcoder/ABCLike/エイシング プログラミング コンテスト 2019/A.cs
+++ Atcoder/ABCLike/エイシング プログラミング コンテスト 2019/A.cs
@@ -1,10 +1,15 @@
-﻿namespace Atcoder.ABCLike.エイシング_プログラミング_コンテスト_2019
+﻿using System;
+
+namespace Atcoder.ABCLike.エイシング_プログラミング_コンテスト_2019
 {
     public class A : AtcoderBase
     {
         public override void Run()
         {
-            
+            var N = int.Parse(Console.ReadLine());
+            var H = int.Parse(Console.ReadLine());
+            var W = int.Parse(Console.ReadLine());
+            Console.WriteLine((N - W + 1) * (N - H + 1));
         }
     }
 }
