342319ae59d462eefd05996a2c84dbbee51fd42a
Takumi Oda <xiaotiantakumi@gmail.com>
2020-10-13 08:01:06 +0900

    HHKB プログラミングコンテスト 2020のA回答

---
+++ Atcoder/ABCLike/HHKB プログラミングコンテスト 2020/A.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Atcoder.ABCLike.HHKB_プログラミングコンテスト_2020
+{
+    public class A : AtcoderBase
+    {
+        public override void Run()
+        {
+            var Y = Console.ReadLine();
+            var S = Console.ReadLine();
+            if (Y == "Y")
+            {
+                Console.WriteLine(S.ToUpper());
+            }
+            else
+            {
+                Console.WriteLine(S);
+            }
+        }
+    }
+}
