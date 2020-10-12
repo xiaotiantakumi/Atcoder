dd94b962641e32385c03cc8d6586710e41ab4c44
Takumi Oda <xiaotiantakumi@gmail.com>
2020-10-12 22:47:42 +0900

    HHKB プログラミングコンテスト 2020のB回答

--- Atcoder/ABCLike/エイシング プログラミング コンテスト 2019/B.cs
+++ Atcoder/ABCLike/エイシング プログラミング コンテスト 2019/B.cs
@@ -1,10 +1,35 @@
-﻿namespace Atcoder.ABCLike.エイシング_プログラミング_コンテスト_2019
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABCLike.エイシング_プログラミング_コンテスト_2019
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
-            
+            var N = int.Parse(Console.ReadLine());
+            var AB = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var Ps = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            List<int> ret = new List<int>() { 0, 0, 0 };
+
+            for (int i = 0; i < N; i++)
+            {
+                if (Ps[i] <= AB[0])
+                {
+                    ret[0]++;
+                }
+                else if (Ps[i] > AB[0] && Ps[i] <= AB[1])
+                {
+                    ret[1]++;
+                }
+                else if (Ps[i] > AB[1])
+                {
+                    ret[2]++;
+                }
+            }
+
+            Console.WriteLine(ret.Min());
         }
     }
 }
