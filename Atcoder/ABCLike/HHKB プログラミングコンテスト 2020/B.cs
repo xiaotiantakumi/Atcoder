d885450cd3144d412890315a4f9397a9a325e39f
Takumi Oda <xiaotiantakumi@gmail.com>
2020-10-14 23:26:22 +0900

    HHKB プログラミングコンテスト 2020のB回答

---
+++ Atcoder/ABCLike/HHKB プログラミングコンテスト 2020/B.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABCLike.HHKB_プログラミングコンテスト_2020
+{
+    public class B : AtcoderBase
+    {
+        public override void Run()
+        {
+            var HW = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            List<string> area = new List<string>();
+            for (int i = 0; i < HW[0]; i++)
+            {
+                var r = Console.ReadLine();
+                area.Add(r);
+            }
+
+            int ret = 0;
+            for (int i = 0; i < HW[0]; i++)
+            {
+                for (int j = 0; j < HW[1]; j++)
+                {
+                    var currentPos = area[i][j];
+                    if (currentPos == '#')
+                    {
+                        continue;
+                    }
+                    if (i != HW[0] - 1 && area[i + 1][j] == '.')
+                    {
+                        ret++;
+                    }
+
+                    if (j != HW[1] - 1 && area[i][j + 1] == '.')
+                    {
+                        ret++;
+                    }
+                }
+            }
+
+            Console.WriteLine(ret);
+        }
+    }
+}
