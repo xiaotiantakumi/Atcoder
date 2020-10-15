9da02f1f3b0ed09d7827e09366fab65c21e36e38
Takumi Oda <xiaotiantakumi@gmail.com>
2020-10-15 22:48:22 +0900

    HHKB プログラミングコンテスト 2020のC回答

---
+++ Atcoder/ABCLike/HHKB プログラミングコンテスト 2020/C.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABCLike.HHKB_プログラミングコンテスト_2020
+{
+    public class C : AtcoderBase
+    {
+        public override void Run()
+        {
+            var N = int.Parse(Console.ReadLine());
+            var Ps = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int current = 0;
+            HashSet<int> vals = new HashSet<int>();
+            for (int i = 0; i < N; i++)
+            {
+                vals.Add(Ps[i]);
+                while (true)
+                {
+                    if (!vals.Contains(current))
+                    {
+                        Console.WriteLine(current);
+                        break;
+                    }
+                    else
+                    {
+                        current++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Atcoder/ABCLike/HHKB プログラミングコンテスト 2020/D.cs
@@ -0,0 +1,9 @@
+﻿namespace Atcoder.ABCLike.HHKB_プログラミングコンテスト_2020
+{
+    public class D : AtcoderBase
+    {
+        public override void Run()
+        {
+        }
+    }
+}
