7185e19f9567d1302a765ce3cee7f2ed713ae8cf
Takumi Oda <xiaotiantakumi@gmail.com>
2020-10-16 23:06:30 +0900

    MSOLUTIONS_プロコンオープン_2020のC回答

--- Atcoder/ABCLike/MSOLUTIONS_プロコンオープン_2020/C.cs
+++ Atcoder/ABCLike/MSOLUTIONS_プロコンオープン_2020/C.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Atcoder.ABCLike.MSOLUTIONS_プロコンオープン_2020
 {
@@ -7,37 +8,17 @@ namespace Atcoder.ABCLike.MSOLUTIONS_プロコンオープン_2020
     {
         public override void Run()
         {
-            var N = int.Parse(Console.ReadLine());
-            Dictionary<double, int> dic = new Dictionary<double, int>();
-            for (int x = 1; x <= 100; x++)
+            var NK = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var N = NK[0];
+            var K = NK[1];
+            var As = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            for (int i = K; i < N; i++)
             {
-                for (int y = 1; y <= 100; y++)
-                {
-                    for (int z = 1; z <= 100; z++)
-                    {
-                        var cal = Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2) + x * y + x * z + y * z;
-                        if (!dic.ContainsKey(cal))
-                        {
-                            dic[cal] = 1;
-                        }
-                        else
-                        {
-                            dic[cal]++;
-                        }
-                    }
-                }
-            }
 
-            for (int i = 1; i <= N; i++)
-            {
-                if (!dic.ContainsKey(i))
-                {
-                    Console.WriteLine(0);
-                }
-                else
-                {
-                    Console.WriteLine(dic[i]);
-                }
+                //var cur = As[i - K] + As[i - 1];
+                //var next = As[i - K + 1] + As[i];
+                var isLarge = As[i] - As[i - K] > 0;
+                Console.WriteLine(isLarge ? "Yes" : "No");
             }
         }
     }
