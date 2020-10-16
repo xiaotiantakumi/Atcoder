b95cbf1bee4a132ddb4860d4c0d44d1d15a68191
Takumi Oda <xiaotiantakumi@gmail.com>
2020-10-17 08:41:25 +0900

    MSOLUTIONS_プロコンオープン_2020のC回答

--- Atcoder/ABCLike/MSOLUTIONS_プロコンオープン_2020/D.cs
+++ Atcoder/ABCLike/MSOLUTIONS_プロコンオープン_2020/D.cs
@@ -1,9 +1,58 @@
-﻿namespace Atcoder.ABCLike.MSOLUTIONS_プロコンオープン_2020
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABCLike.MSOLUTIONS_プロコンオープン_2020
 {
     public class D : AtcoderBase
     {
         public override void Run()
         {
+            bool isBuy = true;
+            var N = int.Parse(Console.ReadLine());
+            var As = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            long remain = 1000;
+            long book = 0;
+            long current = As[0];
+            for (int i = 1; i < N; i++)
+            {
+                if (isBuy)
+                {
+                    if (As[i] > current)
+                    {
+                        book = remain / current;
+                        remain -= current * book;
+                        isBuy = !isBuy;
+                        current = As[i];
+                    }
+                    else
+                    {
+                        current = Math.Min(current, As[i]);
+                    }
+
+                }
+                else
+                {
+                    if (As[i] < current)
+                    {
+                        remain += book * current;
+                        book = 0;
+                        isBuy = !isBuy;
+                        current = As[i];
+                    }
+                    else
+                    {
+                        current = Math.Max(current, As[i]);
+                    }
+
+                }
+            }
+
+            if (book != 0)
+            {
+                remain += book * current;
+            }
+
+            Console.WriteLine(remain);
         }
     }
 }
