769432e2dd2527a9f6e9e2bfbbd3cbb6eb15fde8
Takumi Oda <xiaotiantakumi@gmail.com>
2020-10-18 08:37:43 +0900

    ABC180のA回答

--- Atcoder/ABC/ABC180/A.cs
+++ Atcoder/ABC/ABC180/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC.ABC180
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC180
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var NAB = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(NAB[0] - NAB[1] + NAB[2]);
         }
     }
 }
