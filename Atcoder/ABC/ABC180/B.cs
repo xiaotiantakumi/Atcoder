f3a0605980ff3969a4e57a5d48be07c143d4d064
Takumi Oda <xiaotiantakumi@gmail.com>
2020-10-19 22:43:07 +0900

    ABC180のB回答

--- Atcoder/ABC/ABC180/B.cs
+++ Atcoder/ABC/ABC180/B.cs
@@ -1,9 +1,27 @@
-﻿namespace Atcoder.ABC.ABC180
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC180
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = decimal.Parse(Console.ReadLine());
+            var xs = Console.ReadLine().Split().Select(decimal.Parse).ToArray();
+            decimal ret = 0;
+            decimal ret2 = 0;
+            decimal ret3 = 0;
+            for (int i = 0; i < N; i++)
+            {
+                ret += Math.Abs(xs[i]);
+                ret2 += xs[i] * xs[i];
+                ret3 = Math.Max(ret3, Math.Abs(xs[i]));
+            }
+
+            Console.WriteLine(ret);
+            Console.WriteLine(Math.Sqrt((double)ret2));
+            Console.WriteLine(ret3);
         }
     }
 }
