515592e9aa1a1f790c5b7e78b440239e7b9e9460
Takumi Oda <xiaotiantakumi@gmail.com>
2020-10-20 21:56:58 +0900

    ABC180のC回答

--- Atcoder/ABC/ABC180/C.cs
+++ Atcoder/ABC/ABC180/C.cs
@@ -1,9 +1,37 @@
-﻿namespace Atcoder.ABC.ABC180
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atcoder.ABC.ABC180
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var N = long.Parse(Console.ReadLine());
+            var sqrtN = Math.Sqrt(N);
+            List<long> rets = new List<long>();
+            for (int i = 1; i <= sqrtN; i++)
+            {
+                if (N % i == 0)
+                {
+                    rets.Add(i);
+                }
+            }
+
+            var cnt = rets.Count;
+            int cur = 0;
+            for (var index = cnt - 1; index >= 0; index--)
+            {
+                var item = rets[index];
+                if (item == sqrtN)
+                {
+                    continue;
+                }
+                rets.Add(N / item);
+                cur++;
+            }
+
+            Console.WriteLine(string.Join("\n", rets));
         }
     }
 }
