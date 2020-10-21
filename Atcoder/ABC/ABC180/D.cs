d4a12d95d66e1ec61f66bf9f8d36a3594f2ecc06
Takumi Oda <xiaotiantakumi@gmail.com>
2020-10-21 22:01:28 +0900

    ABC180のD途中

--- Atcoder/ABC/ABC180/D.cs
+++ Atcoder/ABC/ABC180/D.cs
@@ -1,9 +1,49 @@
-﻿namespace Atcoder.ABC.ABC180
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC180
 {
     public class D : AtcoderBase
     {
         public override void Run()
         {
+            var XYAB = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            var X = XYAB[0];
+            var Y = XYAB[1];
+            var A = XYAB[2];
+            var B = XYAB[3];
+            long cnt = 0;
+            long current = 0;
+            bool flg = false;
+            while (true)
+            {
+                if (!flg)
+                {
+                    long tmp1 = X * A;
+                    long tmp2 = X + B;
+                    if (tmp1 > tmp2)
+                    {
+                        flg = !flg;
+                    }
+                    if (Y <= X * A)
+                    {
+                        break;
+                    }
+                    X *= A;
+                }
+                else
+                {
+                    var tmp = (Y - X) / B;
+                    if (tmp > 0)
+                    {
+                        cnt += tmp - 1;
+                    }
+                    break;
+                }
+
+                cnt++;
+            }
+            Console.WriteLine(cnt);
         }
     }
 }
