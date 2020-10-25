f6d9c6c83b111392d249d4eead611e0fc1494f7f
Takumi Oda <xiaotiantakumi@gmail.com>
2020-10-25 09:43:53 +0900

    ARC008のA回答

--- Atcoder/ARC/ARC008/A.cs
+++ Atcoder/ARC/ARC008/A.cs
@@ -1,10 +1,23 @@
-﻿namespace Atcoder.ARC.ARC008
+﻿using System;
+
+namespace Atcoder.ARC.ARC008
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var n = int.Parse(Console.ReadLine());
+            var ten = n / 10;
+            var one = n % 10;
+            if (one > 6)
+            {
+                ++ten;
+                Console.WriteLine(ten * 100);
+            }
+            else
+            {
+                Console.WriteLine(ten * 100 + one * 15);
+            }
         }
     }
 }
