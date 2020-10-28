ba21eba3ce92532a61bb6e86b2904ce921d6ed40
Takumi Oda <xiaotiantakumi@gmail.com>
2020-10-28 22:33:32 +0900

    ARC003のB回答

--- Atcoder/ARC/ARC003/B.cs
+++ Atcoder/ARC/ARC003/B.cs
@@ -1,9 +1,25 @@
-﻿namespace Atcoder.ARC.ARC003
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ARC.ARC003
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            for (int i = 0; i < N; i++)
+            {
+                var r = Console.ReadLine();
+                dic[new string(r.Reverse().ToArray())] = r;
+            }
+
+            foreach (var retPair in dic.OrderBy(x => x.Key))
+            {
+                Console.WriteLine(retPair.Value);
+            }
         }
     }
 }
