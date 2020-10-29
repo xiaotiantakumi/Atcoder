cacf3ae5310f095dbd362b701f725a2d7fcce746
Takumi Oda <xiaotiantakumi@gmail.com>
2020-10-29 20:59:48 +0900

    ARC007のB回答

--- Atcoder/ARC/ARC007/B.cs
+++ Atcoder/ARC/ARC007/B.cs
@@ -1,9 +1,36 @@
-﻿namespace Atcoder.ARC.ARC007
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ARC.ARC007
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var NM = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] ret = new int[NM[0] + 1];
+            for (var index = 0; index < ret.Length; index++)
+            {
+                ret[index] = index;
+            }
+
+            for (int i = 0; i < NM[1]; i++)
+            {
+                var inp = int.Parse(Console.ReadLine());
+                var tmp = ret[0];
+                int index = 0;
+                for (int j = 0; j < ret.Length; j++)
+                {
+                    if (ret[j] == inp)
+                    {
+                        index = j;
+                    }
+                }
+                ret[0] = ret[index];
+                ret[index] = tmp;
+            }
+
+            Console.WriteLine(string.Join("\n", ret.Skip(1)));
         }
     }
 }
