78ef3702b4fe0968a044f0aa0884a79661cc0e97
Takumi Oda <xiaotiantakumi@gmail.com>
2020-10-31 17:16:10 +0900

    ARC009のA回答

--- Atcoder/ARC/ARC009/A.cs
+++ Atcoder/ARC/ARC009/A.cs
@@ -1,10 +1,22 @@
-﻿namespace Atcoder.ARC.ARC009
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ARC.ARC009
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var N = int.Parse(Console.ReadLine());
+            double ret = 0;
+            for (int i = 0; i < N; i++)
+            {
+                var AB = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                ret += AB[0] * AB[1];
+            }
+
+            ret *= 1.05;
+            Console.WriteLine(Math.Truncate(ret));
         }
     }
 }
