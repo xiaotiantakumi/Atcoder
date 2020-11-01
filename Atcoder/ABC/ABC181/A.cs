bc719e3b701535ff2141b662454b45fd782ef497
Takumi Oda <xiaotiantakumi@gmail.com>
2020-11-01 23:09:07 +0900

    ABC181のA回答

--- Atcoder/ABC/ABC181/A.cs
+++ Atcoder/ABC/ABC181/A.cs
@@ -1,10 +1,13 @@
-﻿namespace Atcoder.ABC.ABC181
+﻿using System;
+
+namespace Atcoder.ABC.ABC181
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var N = int.Parse(Console.ReadLine());
+            Console.WriteLine(N % 2 == 0 ? "White" : "Black");
         }
     }
 }
