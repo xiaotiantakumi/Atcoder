26c886836bb04e06b103da07b5697a6ecc719c2a
Takumi Oda <xiaotiantakumi@gmail.com>
2020-11-03 00:07:21 +0900

    ABC181のC回答

--- Atcoder/ABC/ABC181/C.cs
+++ Atcoder/ABC/ABC181/C.cs
@@ -1,9 +1,43 @@
-﻿namespace Atcoder.ABC.ABC181
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC181
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            List<double[]> items = new List<double[]>();
+            for (int i = 0; i < N; i++)
+            {
+                var xy = Console.ReadLine().Split().Select(double.Parse).ToArray();
+                items.Add(xy);
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = i + 1; j < N; j++)
+                {
+                    for (int k = j + 1; k < N; k++)
+                    {
+                        var y1 = items[i][1];
+                        var y2 = items[j][1];
+                        var y3 = items[k][1];
+                        var x1 = items[i][0];
+                        var x2 = items[j][0];
+                        var x3 = items[k][0];
+                        if ((y3 - y1) * (x2 - x1) == (y2 - y1) * (x3 - x1))
+                        {
+                            Console.WriteLine("Yes");
+                            return;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("No");
         }
     }
 }
