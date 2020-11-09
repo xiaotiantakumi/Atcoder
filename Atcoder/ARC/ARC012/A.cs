091c3e8b63000ab910a5e2c94162e356e6645157
Takumi Oda <xiaotiantakumi@gmail.com>
2020-11-09 22:35:36 +0900

    ARC012のA回答

--- Atcoder/ARC/ARC012/A.cs
+++ Atcoder/ARC/ARC012/A.cs
@@ -1,10 +1,25 @@
-﻿namespace Atcoder.ARC.ARC012
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atcoder.ARC.ARC012
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            List<string> weeks = new List<string>()
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+            var s = Console.ReadLine();
+            var ret = 5 - weeks.IndexOf(s);
+            Console.WriteLine(ret < 0 ? 0 : ret);
         }
     }
 }
