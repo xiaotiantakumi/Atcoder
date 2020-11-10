7b8272215c32e45f1579af241eef220bd6d620f0
Takumi Oda <xiaotiantakumi@gmail.com>
2020-11-11 08:02:48 +0900

    ABC182のA回答

--- Atcoder/ABC/ABC182/A.cs
+++ Atcoder/ABC/ABC182/A.cs
@@ -1,10 +1,15 @@
-﻿namespace Atcoder.ABC.ABC182
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC182
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var AB = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var limit = 2 * AB[0] + 100;
+            Console.WriteLine(limit - AB[1]);
         }
     }
 }
