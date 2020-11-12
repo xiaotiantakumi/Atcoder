85159fa5fd02f5971dcd6f33601b7957019922ef
Takumi Oda <xiaotiantakumi@gmail.com>
2020-11-12 22:39:23 +0900

    ABC182のB回答

--- Atcoder/ABC/ABC182/B.cs
+++ Atcoder/ABC/ABC182/B.cs
@@ -1,9 +1,35 @@
-﻿namespace Atcoder.ABC.ABC182
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC182
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            int max = 0;
+            int ret = 0;
+            var N = int.Parse(Console.ReadLine());
+            var As = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 2; i < 1001; i++)
+            {
+                int cnt = 0;
+                foreach (var item in As)
+                {
+                    if (item % i == 0)
+                    {
+                        cnt++;
+                    }
+                }
+
+                max = Math.Max(max, cnt);
+                if (cnt == max)
+                {
+                    ret = i;
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
