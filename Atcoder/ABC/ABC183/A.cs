ea93a8967c2def506212c18b5172a7a78245bf9d
Takumi Oda <xiaotiantakumi@gmail.com>
2020-11-16 12:17:14 +0900

    ABC183のA回答

--- Atcoder/ABC/ABC183/A.cs
+++ Atcoder/ABC/ABC183/A.cs
@@ -1,10 +1,13 @@
-﻿namespace Atcoder.ABC.ABC183
+﻿using System;
+
+namespace Atcoder.ABC.ABC183
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var x = int.Parse(Console.ReadLine());
+            Console.WriteLine(x < 0 ? 0 : x);
         }
     }
 }
