9a49197963f5dd2dfe91fdc51e9fb8f36fcf29d4
Takumi Oda <xiaotiantakumi@gmail.com>
2020-11-19 23:30:40 +0900

    ARC014のA回答

--- Atcoder/ARC/ARC014/A.cs
+++ Atcoder/ARC/ARC014/A.cs
@@ -1,10 +1,13 @@
-﻿namespace Atcoder.ARC.ARC014
+﻿using System;
+
+namespace Atcoder.ARC.ARC014
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n % 2 == 0 ? "Blue" : "Red");
         }
     }
 }
