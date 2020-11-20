0f69931bccb89c1a9914260edab74288aa6a4891
Takumi Oda <xiaotiantakumi@gmail.com>
2020-11-20 22:35:10 +0900

    ARC014のB回答

--- Atcoder/ARC/ARC014/B.cs
+++ Atcoder/ARC/ARC014/B.cs
@@ -1,9 +1,35 @@
-﻿namespace Atcoder.ARC.ARC014
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ARC.ARC014
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            HashSet<string> records = new HashSet<string>();
+            int cnt = 0;
+            for (int i = 0; i < N; i++)
+            {
+                var w = Console.ReadLine();
+                if (records.Any() && (records.Contains(w) || records.Last().Last() != w.First()))
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine("LOSE");
+                    }
+                    else
+                    {
+                        Console.WriteLine("WIN");
+                    }
+                    return;
+                }
+
+                records.Add(w);
+            }
+            Console.WriteLine("DRAW");
         }
     }
 }
