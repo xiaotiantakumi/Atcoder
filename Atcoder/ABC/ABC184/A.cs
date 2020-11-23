bc8a099f913cc5ab4025fd86f2b66d64b81f7aea
Takumi Oda <xiaotiantakumi@gmail.com>
2020-11-23 23:00:35 +0900

    ABC184のA回答

--- Atcoder/ABC/ABC184/A.cs
+++ Atcoder/ABC/ABC184/A.cs
@@ -1,10 +1,15 @@
-﻿namespace Atcoder.ABC.ABC184
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC184
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var ab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var cd = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(ab[0] * cd[1] - ab[1] * cd[0]);
         }
     }
 }
