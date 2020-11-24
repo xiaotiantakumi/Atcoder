0c26a29190e1972935fc668353c32465f4837453
Takumi Oda <xiaotiantakumi@gmail.com>
2020-11-24 23:06:37 +0900

    ABC184のB回答

--- Atcoder/ABC/ABC184/B.cs
+++ Atcoder/ABC/ABC184/B.cs
@@ -1,9 +1,31 @@
-﻿namespace Atcoder.ABC.ABC184
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC184
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var NX = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var s = Console.ReadLine();
+            int ret = NX[1];
+            foreach (var c in s)
+            {
+                if (c == 'o')
+                {
+                    ret++;
+                }
+                else
+                {
+                    if (ret > 0)
+                    {
+                        ret--;
+                    }
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
