f818e2c876e8770583bc56ae8d3fa904c0f77b77
Takumi Oda <xiaotiantakumi@gmail.com>
2020-11-25 23:06:39 +0900

    ARC015のA回答

--- Atcoder/ARC/ARC015/A.cs
+++ Atcoder/ARC/ARC015/A.cs
@@ -1,10 +1,13 @@
-﻿namespace Atcoder.ARC.ARC015
+﻿using System;
+
+namespace Atcoder.ARC.ARC015
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var n = double.Parse(Console.ReadLine());
+            Console.WriteLine(9.0 / 5.0 * n + 32.0);
         }
     }
 }
