ebab6c9bb499703c58d4ccdfee1c5fea61316c07
Takumi Oda <xiaotiantakumi@gmail.com>
2020-11-26 23:49:52 +0900

    ARC005のA回答

--- Atcoder/ARC/ARC005/A.cs
+++ Atcoder/ARC/ARC005/A.cs
@@ -1,10 +1,32 @@
-﻿namespace Atcoder.ARC.ARC005
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ARC.ARC005
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            HashSet<string> items = new HashSet<string>()
+            {
+                "TAKAHASHIKUN",
+                "Takahashikun",
+                "takahashikun",
+            };
+            var N = int.Parse(Console.ReadLine());
+            var r = Console.ReadLine().Split().ToArray();
+            int ret = 0;
+            foreach (var target in r)
+            {
+                var target2 = target.Replace(".", "");
+                if (items.Contains(target2))
+                {
+                    ret++;
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
