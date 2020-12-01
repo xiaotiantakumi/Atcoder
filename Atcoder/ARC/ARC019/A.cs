16e1aa80b73cc8418a46a976377b266fa3d22387
Takumi Oda <xiaotiantakumi@gmail.com>
2020-12-02 08:31:23 +0900

    ARC019のA回答

--- Atcoder/ARC/ARC019/A.cs
+++ Atcoder/ARC/ARC019/A.cs
@@ -1,10 +1,34 @@
-﻿namespace Atcoder.ARC.ARC019
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atcoder.ARC.ARC019
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            Dictionary<char, char> dic = new Dictionary<char, char>();
+            dic.Add('O', '0');
+            dic.Add('D', '0');
+            dic.Add('I', '1');
+            dic.Add('Z', '2');
+            dic.Add('S', '5');
+            dic.Add('B', '8');
+            var s = Console.ReadLine();
+            string ret = string.Empty;
+            foreach (var c in s)
+            {
+                if (dic.ContainsKey(c))
+                {
+                    ret += dic[c];
+                }
+                else
+                {
+                    ret += c;
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
