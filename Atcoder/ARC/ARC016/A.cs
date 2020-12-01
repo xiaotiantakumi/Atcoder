d677fe9e9b3d4972b690566099c79600b952451b
Takumi Oda <xiaotiantakumi@gmail.com>
2020-12-01 22:01:43 +0900

    ARC016のA回答

--- Atcoder/ARC/ARC016/A.cs
+++ Atcoder/ARC/ARC016/A.cs
@@ -1,10 +1,21 @@
-﻿namespace Atcoder.ARC.ARC016
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ARC.ARC016
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var NM = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 1; i <= NM[0]; i++)
+            {
+                if (i != NM[1])
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
         }
     }
 }
