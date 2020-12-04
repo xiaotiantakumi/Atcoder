6c144ccca3758b7e48d4f12ae94a2027bdc46349
Takumi Oda <xiaotiantakumi@gmail.com>
2020-12-04 20:17:33 +0900

    ARC021フォルダ作成

---
+++ Atcoder/ARC/ARC021/A.cs
@@ -0,0 +1,10 @@
+﻿namespace Atcoder.ARC.ARC021
+{
+    public class A : AtcoderBase
+    {
+        public override  void Run()
+        {
+            
+        }
+    }
+}
---
+++ Atcoder/ARC/ARC021/B.cs
@@ -0,0 +1,9 @@
+﻿namespace Atcoder.ARC.ARC021
+{
+    public class B : AtcoderBase
+    {
+        public override void Run()
+        {
+        }
+    }
+}
