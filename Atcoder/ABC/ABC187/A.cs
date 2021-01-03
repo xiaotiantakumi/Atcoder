b67dc4a3854b69e6021fb168784eabc2a4059632
Takumi Oda <xiaotiantakumi@gmail.com>
2021-01-03 14:37:21 +0900

    ABC187のA回答

--- Atcoder/ABC/ABC187/A.cs
+++ Atcoder/ABC/ABC187/A.cs
@@ -1,10 +1,24 @@
-﻿namespace Atcoder.ABC.ABC187
+﻿using System;
+
+namespace Atcoder.ABC.ABC187
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var AB = Console.ReadLine().Split();
+            int A = 0;
+            int B = 0;
+            foreach (var a in AB[0])
+            {
+                A += int.Parse(a.ToString());
+            }
+            foreach (var b in AB[1])
+            {
+                B += int.Parse(b.ToString());
+            }
+
+            Console.WriteLine(Math.Max(A, B));
         }
     }
 }
