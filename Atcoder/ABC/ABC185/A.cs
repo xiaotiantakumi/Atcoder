a0ae66a56216389e70b6b3c7c8890c2e7cc892b7
Takumi Oda <xiaotiantakumi@gmail.com>
2021-02-20 08:36:42 +0900

    ABC185のA回答

--- Atcoder/ABC/ABC185/A.cs
+++ Atcoder/ABC/ABC185/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC.ABC185
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC185
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var r = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            Console.WriteLine(r.Min());
         }
     }
 }
