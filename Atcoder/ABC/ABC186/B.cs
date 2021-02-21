fbd3a16bef61e12027d864778946ac6d7d69625a
Takumi Oda <xiaotiantakumi@gmail.com>
2021-02-21 16:28:24 +0900

    ABC186のB回答

--- Atcoder/ABC/ABC186/B.cs
+++ Atcoder/ABC/ABC186/B.cs
@@ -1,9 +1,38 @@
-﻿namespace Atcoder.ABC.ABC186
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC186
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var H = r[0];
+            var W = r[1];
+            int min = int.MaxValue;
+            List<int[]> list = new List<int[]>();
+            for (int i = 0; i < H; i++)
+            {
+                var row = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                min = Math.Min(min, Min(row));
+                list.Add(row);
+            }
+
+            int ret = 0;
+            foreach (var row in list)
+            {
+                foreach (var item in row)
+                {
+                    if (item > min)
+                    {
+                        ret += item - min;
+                    }
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
