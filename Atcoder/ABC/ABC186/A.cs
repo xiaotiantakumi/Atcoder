6d7c98a2c2f65d08ab5d831731fbd9e2cf5740e3
Takumi Oda <xiaotiantakumi@gmail.com>
2021-02-21 16:18:17 +0900

    ABC186のA回答

--- Atcoder/ABC/ABC186/A.cs
+++ Atcoder/ABC/ABC186/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC.ABC186
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC186
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(r[0] / r[1]);
         }
     }
 }
