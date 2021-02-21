d907be173fda9420888b52b8839a79f840c02b94
Takumi Oda <xiaotiantakumi@gmail.com>
2021-02-21 17:31:53 +0900

    ABC186のC回答

--- Atcoder/ABC/ABC186/C.cs
+++ Atcoder/ABC/ABC186/C.cs
@@ -1,9 +1,29 @@
-﻿namespace Atcoder.ABC.ABC186
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atcoder.ABC.ABC186
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            HashSet<int> items = new HashSet<int>();
+            for (int i = 1; i <= N; i++)
+            {
+                var tenVal = i.ToString();
+                if (tenVal.Contains("7"))
+                {
+                    items.Add(i);
+                }
+                var eightVal = Convert.ToString(i, 8);
+                if (eightVal.Contains("7"))
+                {
+                    items.Add(i);
+                }
+            }
+
+            Console.WriteLine(N - items.Count);
         }
     }
 }
