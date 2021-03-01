047da94a9f4e5c2c9e855335d2a0f8d991e59d4b
Takumi Oda <xiaotiantakumi@gmail.com>
2021-03-01 12:57:25 +0900

    ABC188のAB回答

--- Atcoder/ABC/ABC188/A.cs
+++ Atcoder/ABC/ABC188/A.cs
@@ -1,10 +1,15 @@
-﻿namespace Atcoder.ABC.ABC188
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC188
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var dif = Math.Abs(r[0] - r[1]);
+            Console.WriteLine(dif < 3 ? "Yes" : "No");
         }
     }
 }
--- Atcoder/ABC/ABC188/B.cs
+++ Atcoder/ABC/ABC188/B.cs
@@ -1,9 +1,22 @@
-﻿namespace Atcoder.ABC.ABC188
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC188
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var X = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var Y = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int ret = 0;
+            for (int i = 0; i < N; i++)
+            {
+                ret += X[i] * Y[i];
+            }
+
+            Console.WriteLine(ret == 0 ? "Yes" : "No");
         }
     }
 }
