151e3d4abfe6deb68994e040295172f8b94925d2
Takumi Oda <xiaotiantakumi@gmail.com>
2021-04-13 08:19:41 +0900

    ABC189のAB回答

--- Atcoder/ABC/ABC189/A.cs
+++ Atcoder/ABC/ABC189/A.cs
@@ -1,10 +1,22 @@
-﻿namespace Atcoder.ABC.ABC189
+﻿using System;
+
+namespace Atcoder.ABC.ABC189
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var input = Console.ReadLine();
+            for (var i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] != input[i + 1])
+                {
+                    Console.WriteLine("Lost");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Won");
         }
     }
 }
--- Atcoder/ABC/ABC189/B.cs
+++ Atcoder/ABC/ABC189/B.cs
@@ -1,9 +1,26 @@
-﻿namespace Atcoder.ABC.ABC189
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC189
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var NX = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+            for (int i = 0; i < NX[0]; i++)
+            {
+                var VP = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                sum += VP[0] * VP[1];
+                if (sum > NX[1] * 100)
+                {
+                    Console.WriteLine(i + 1);
+                    return;
+                }
+            }
+
+            Console.WriteLine(-1);
         }
     }
 }
