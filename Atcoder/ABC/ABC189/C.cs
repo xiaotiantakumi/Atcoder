c79b4f3e0a551ed62bfa0ac84d83981b51cbedaf
Takumi Oda <xiaotiantakumi@gmail.com>
2021-04-19 00:41:15 +0900

    ABC189のC回答

--- Atcoder/ABC/ABC189/C.cs
+++ Atcoder/ABC/ABC189/C.cs
@@ -1,9 +1,34 @@
-﻿namespace Atcoder.ABC.ABC189
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC189
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            int ret = 0;
+            var N = int.Parse(Console.ReadLine());
+            var As = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int l = 0; l < N; l++)
+            {
+                var min = As[l];
+                for (int r = l; r < N; r++)
+                {
+                    if (As[r] < min)
+                    {
+                        min = As[r];
+                    }
+
+                    var sum = (r - l + 1) * min;
+                    if (sum > ret)
+                    {
+                        ret = sum;
+                    }
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
