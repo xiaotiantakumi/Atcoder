80f6d270a0728f067251ec8eee96c15817c40ea1
Takumi Oda <xiaotiantakumi@gmail.com>
2021-04-21 08:26:59 +0900

    ABC190のA回答

--- Atcoder/ABC/ABC190/A.cs
+++ Atcoder/ABC/ABC190/A.cs
@@ -1,10 +1,41 @@
-﻿namespace Atcoder.ABC.ABC190
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC190
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var ABC = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var A = ABC[0];
+            var B = ABC[1];
+            var C = ABC[2];
+            var isA = C == 0 ? true : false;
+            while (true)
+            {
+                if (isA)
+                {
+                    A--;
+                }
+                else
+                {
+                    B--;
+                }
+
+                isA = !isA;
+                if (A < 0)
+                {
+                    Console.WriteLine("Aoki");
+                    return;
+                }
+
+                if (B < 0)
+                {
+                    Console.WriteLine("Takahashi");
+                    return;
+                }
+            }
         }
     }
 }
