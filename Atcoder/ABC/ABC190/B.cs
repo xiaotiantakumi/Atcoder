1e1e3823e2b4753a8f9477f75586320f7b5aa7db
Takumi Oda <xiaotiantakumi@gmail.com>
2021-04-22 21:44:10 +0900

    ABC190のB回答

--- Atcoder/ABC/ABC190/B.cs
+++ Atcoder/ABC/ABC190/B.cs
@@ -1,9 +1,24 @@
-﻿namespace Atcoder.ABC.ABC190
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC190
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var NSD = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var ret = "No";
+            for (int i = 0; i < NSD[0]; i++)
+            {
+                var XY = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                if (XY[0] < NSD[1] && XY[1] > NSD[2])
+                {
+                    ret = "Yes";
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
