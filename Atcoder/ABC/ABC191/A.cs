5b536e5984b6051075e2186d1c4f610a278da4fd
Takumi Oda <xiaotiantakumi@gmail.com>
2021-04-23 21:57:30 +0900

    ABC191のA。なんか解けない。

--- Atcoder/ABC/ABC191/A.cs
+++ Atcoder/ABC/ABC191/A.cs
@@ -1,10 +1,23 @@
-﻿namespace Atcoder.ABC.ABC191
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC191
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var VTSD = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var min = VTSD[0] * VTSD[1];
+            var max = VTSD[0] * VTSD[2];
+            if (min < VTSD[2] && VTSD[2] < max)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine("Yes");
+            }
         }
     }
 }
