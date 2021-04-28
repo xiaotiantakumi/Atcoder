25751fa69bf6ceaf27b3c9dcf9457a2eb43516fd
Takumi Oda <xiaotiantakumi@gmail.com>
2021-04-28 22:37:57 +0900

    ABC192のA回答

--- Atcoder/ABC/ABC192/A.cs
+++ Atcoder/ABC/ABC192/A.cs
@@ -1,10 +1,13 @@
-﻿namespace Atcoder.ABC.ABC192
+﻿using System;
+
+namespace Atcoder.ABC.ABC192
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var X = int.Parse(Console.ReadLine());
+            Console.WriteLine(100 - (X % 100));
         }
     }
 }
