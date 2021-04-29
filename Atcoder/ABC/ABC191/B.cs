2e48363663ca24f0c80a143b7b0934c89352ff32
Takumi Oda <xiaotiantakumi@gmail.com>
2021-04-29 09:28:06 +0900

    ABC191のB回答

--- Atcoder/ABC/ABC191/B.cs
+++ Atcoder/ABC/ABC191/B.cs
@@ -1,9 +1,28 @@
-﻿namespace Atcoder.ABC.ABC191
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC191
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var NX = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var N = NX[0];
+            var X = NX[1];
+            var As = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            List<int> result = new List<int>();
+            foreach (var item in As)
+            {
+                if (item == X)
+                {
+                    continue;
+                }
+                result.Add(item);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
         }
     }
 }
