b0957d9df12c99c4e6bdb3ddf3c5924ca74308ea
Takumi Oda <xiaotiantakumi@gmail.com>
2021-05-07 07:29:24 +0900

    ABC192のB回答

--- Atcoder/ABC/ABC192/B.cs
+++ Atcoder/ABC/ABC192/B.cs
@@ -1,9 +1,23 @@
-﻿namespace Atcoder.ABC.ABC192
+﻿using System;
+
+namespace Atcoder.ABC.ABC192
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var r = Console.ReadLine();
+            for (int i = 0; i < r.Length; i++)
+            {
+                if ((i % 2 == 0 && char.IsUpper(r[i])) ||
+                    (i % 2 == 1 && char.IsLower(r[i])))
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Yes");
         }
     }
 }
