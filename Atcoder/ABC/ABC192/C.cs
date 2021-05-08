478bdb7ddbcf18db4d56fe2d7c24fb3da21200e8
Takumi Oda <xiaotiantakumi@gmail.com>
2021-05-08 11:17:06 +0900

    ABC192のC回答

--- Atcoder/ABC/ABC192/C.cs
+++ Atcoder/ABC/ABC192/C.cs
@@ -1,9 +1,27 @@
-﻿namespace Atcoder.ABC.ABC192
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC192
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var NK = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var N = NK[0];
+            for (int i = 1; i <= NK[1]; i++)
+            {
+                var strN = N.ToString();
+                var left = int.Parse(new string(strN.OrderByDescending(x => x).ToArray()));
+                var right = int.Parse(new string(strN.Replace("0", "").OrderBy(x => x).ToArray()));
+                N = left - right;
+                if (N == 0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(N);
         }
     }
 }
