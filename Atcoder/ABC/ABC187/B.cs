641353d059c2d138caced690895b4a56b08f58be
Takumi Oda <xiaotiantakumi@gmail.com>
2021-05-09 13:01:52 +0900

    ABC187のB回答

--- Atcoder/ABC/ABC187/B.cs
+++ Atcoder/ABC/ABC187/B.cs
@@ -1,9 +1,34 @@
-﻿namespace Atcoder.ABC.ABC187
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC187
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            List<decimal[]> items = new List<decimal[]>();
+            for (int i = 0; i < N; i++)
+            {
+                var item = Console.ReadLine().Split().Select(decimal.Parse).ToArray();
+                items.Add(item);
+            }
+
+            int ret = 0;
+            for (int i = 0; i < N - 1; i++)
+            {
+                for (int j = i + 1; j < N; j++)
+                {
+                    if (Math.Abs(items[i][1] - items[j][1]) <= Math.Abs(items[i][0] - items[j][0]))
+                    {
+                        ret++;
+                    }
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
