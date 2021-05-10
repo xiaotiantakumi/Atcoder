00081b2db210bfbcb5da04cca95bde8d4293cd6b
Takumi Oda <xiaotiantakumi@gmail.com>
2021-05-11 08:44:38 +0900

    ABC193のA回答

--- Atcoder/ABC/ABC193/A.cs
+++ Atcoder/ABC/ABC193/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC.ABC193
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC193
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var AB = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            Console.WriteLine((1 - AB[1] / AB[0]) * 100);
         }
     }
 }
