b9cc2cd27eaa1050dbee06d1d385a64056e5683e
Takumi Oda <xiaotiantakumi@gmail.com>
2021-05-12 19:32:42 +0900

    ABC193のB回答

--- Atcoder/ABC/ABC193/B.cs
+++ Atcoder/ABC/ABC193/B.cs
@@ -1,9 +1,31 @@
-﻿namespace Atcoder.ABC.ABC193
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC193
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            int ret = int.MaxValue;
+            for (int i = 0; i < N; i++)
+            {
+                var APX = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                if (APX[0] < APX[2])
+                {
+                    ret = Math.Min(ret, APX[1]);
+                }
+            }
+
+            if (ret == int.MaxValue)
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                Console.WriteLine(ret);
+            }
         }
     }
 }
