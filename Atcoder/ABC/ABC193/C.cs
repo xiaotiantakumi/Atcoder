835e9f33c02625874a35656c20ddc1b7b908a7f0
Takumi Oda <xiaotiantakumi@gmail.com>
2021-05-13 20:26:24 +0900

    ABC193のC回答

--- Atcoder/ABC/ABC193/C.cs
+++ Atcoder/ABC/ABC193/C.cs
@@ -1,9 +1,31 @@
-﻿namespace Atcoder.ABC.ABC193
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atcoder.ABC.ABC193
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var N = long.Parse(Console.ReadLine());
+            var limit = Math.Sqrt(N);
+            HashSet<long> lst = new HashSet<long>();
+            for (int i = 2; i <= limit; i++)
+            {
+                int cur = 2;
+                while (true)
+                {
+                    var tmp = Math.Pow(i, cur);
+                    if (tmp > N)
+                    {
+                        break;
+                    }
+                    lst.Add((long)tmp);
+                    cur++;
+                }
+            }
+
+            Console.WriteLine(N - lst.Count);
         }
     }
 }
