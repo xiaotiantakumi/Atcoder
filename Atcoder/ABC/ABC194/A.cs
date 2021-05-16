ee94c04adaf12a331915955dbd90df2a0500eacb
Takumi Oda <xiaotiantakumi@gmail.com>
2021-05-16 22:23:26 +0900

    ABC194のA回答

--- Atcoder/ABC/ABC194/A.cs
+++ Atcoder/ABC/ABC194/A.cs
@@ -1,10 +1,29 @@
-﻿namespace Atcoder.ABC.ABC194
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC194
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var AB = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            if (AB[0] + AB[1] >= 15 && AB[1] >= 8)
+            {
+                Console.WriteLine(1);
+            }
+            else if (AB[0] + AB[1] >= 10 && AB[1] >= 3)
+            {
+                Console.WriteLine(2);
+            }
+            else if (AB[0] + AB[1] >= 3)
+            {
+                Console.WriteLine(3);
+            }
+            else
+            {
+                Console.WriteLine(4);
+            }
         }
     }
 }
