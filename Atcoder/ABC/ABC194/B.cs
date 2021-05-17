e9ed63ecaad39da101445a0bcc95dfe40c0890b4
Takumi Oda <xiaotiantakumi@gmail.com>
2021-05-17 21:15:52 +0900

    ABC194のB回答

--- Atcoder/ABC/ABC194/B.cs
+++ Atcoder/ABC/ABC194/B.cs
@@ -1,9 +1,37 @@
-﻿namespace Atcoder.ABC.ABC194
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC194
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            List<int[]> ABs = new List<int[]>();
+            for (int i = 0; i < N; i++)
+            {
+                var AB = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                ABs.Add(AB);
+            }
+            var minPerOne = ABs.Min(x => x[0] + x[1]);
+            var As = ABs.OrderBy(x => x[0]).Take(2).ToList();
+            var Bs = ABs.OrderBy(x => x[1]).Take(2).ToList();
+            int minAB = int.MaxValue;
+            foreach (var A in As)
+            {
+                foreach (var B in Bs)
+                {
+                    if (A == B)
+                    {
+                        continue;
+                    }
+
+                    minAB = Math.Min(minAB, Math.Max(A[0], B[1]));
+                }
+            }
+            Console.WriteLine(Math.Min(minPerOne, minAB));
         }
     }
 }
