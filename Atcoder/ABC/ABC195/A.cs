685157d4eabafa37ec8e6bbdde850ebb670cc9e2
Takumi Oda <xiaotiantakumi@gmail.com>
2021-05-18 20:45:03 +0900

    ABC195のA回答

--- Atcoder/ABC/ABC195/A.cs
+++ Atcoder/ABC/ABC195/A.cs
@@ -1,10 +1,15 @@
-﻿namespace Atcoder.ABC.ABC195
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC195
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var MH = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var ret = MH[1] % MH[0] == 0;
+            Console.WriteLine(ret ? "Yes" : "No");
         }
     }
 }
