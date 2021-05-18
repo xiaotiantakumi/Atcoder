05ec6e1c32df57f9e82541a4fe798c84088fea78
Takumi Oda <xiaotiantakumi@gmail.com>
2021-05-19 08:33:58 +0900

    ABC196のA回答

--- Atcoder/ABC/ABC196/A.cs
+++ Atcoder/ABC/ABC196/A.cs
@@ -1,10 +1,17 @@
-﻿namespace Atcoder.ABC.ABC196
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC196
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var ab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var cd = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var x = Math.Max(ab[0], ab[1]);
+            var y = Math.Min(cd[0], cd[1]);
+            Console.WriteLine(x - y);
         }
     }
 }
