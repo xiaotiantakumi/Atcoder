6ef72ddef4fa50d19cb7d109ed260a575dec9d0a
Takumi Oda <xiaotiantakumi@gmail.com>
2021-05-22 09:15:55 +0900

    ABC195のB回答

--- Atcoder/ABC/ABC195/B.cs
+++ Atcoder/ABC/ABC195/B.cs
@@ -1,9 +1,33 @@
-﻿namespace Atcoder.ABC.ABC195
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC195
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var ABW = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var A = ABW[0];
+            var B = ABW[1];
+            var W = ABW[2] * 1000;
+            int minCount = int.MaxValue;
+            int maxCount = int.MinValue;
+            for (int i = 1; i <= W; i++)
+            {
+                if (A * i <= W && W <= B * i)
+                {
+                    minCount = Math.Min(minCount, i);
+                    maxCount = Math.Max(maxCount, i);
+                }
+            }
+
+            if (minCount == int.MaxValue)
+            {
+                Console.WriteLine("UNSATISFIABLE");
+                return;
+            }
+            Console.WriteLine(minCount + " " + maxCount);
         }
     }
 }
