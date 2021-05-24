0db5bd59d73a9be88dc3174056a3f1bb34d41b94
Takumi Oda <xiaotiantakumi@gmail.com>
2021-05-24 23:36:32 +0900

    ABC196のB回答

--- Atcoder/ABC/ABC196/B.cs
+++ Atcoder/ABC/ABC196/B.cs
@@ -1,9 +1,20 @@
-﻿namespace Atcoder.ABC.ABC196
+﻿using System;
+
+namespace Atcoder.ABC.ABC196
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var s = Console.ReadLine();
+            var pos = s.IndexOf(".");
+            if (pos == -1)
+            {
+                Console.WriteLine(s);
+                return;
+            }
+
+            Console.WriteLine(s.Substring(0, pos));
         }
     }
 }
