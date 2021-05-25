aa9ac6468c5618ef3aa7bbcf6b5c27d809133c18
Takumi Oda <xiaotiantakumi@gmail.com>
2021-05-25 21:07:49 +0900

    ABC197のA回答

--- Atcoder/ABC/ABC197/A.cs
+++ Atcoder/ABC/ABC197/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC.ABC197
+﻿using System;
+
+namespace Atcoder.ABC.ABC197
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var str = Console.ReadLine();
+            var ret = str[1].ToString() + str[2].ToString() + str[0].ToString();
+            Console.WriteLine(ret);
         }
     }
 }
