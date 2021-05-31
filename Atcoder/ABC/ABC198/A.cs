3bab1f74c9c3e2bf39fcf31d8b510bfdf4ca20ce
Takumi Oda <xiaotiantakumi@gmail.com>
2021-05-31 23:05:53 +0900

    ABC198のA回答

--- Atcoder/ABC/ABC198/A.cs
+++ Atcoder/ABC/ABC198/A.cs
@@ -1,10 +1,25 @@
-﻿namespace Atcoder.ABC.ABC198
+﻿using System;
+
+namespace Atcoder.ABC.ABC198
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var n = int.Parse(Console.ReadLine());
+            int ret = 0;
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 1; j <= n - i; j++)
+                {
+                    if (i + j == n)
+                    {
+                        ret++;
+                    }
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
