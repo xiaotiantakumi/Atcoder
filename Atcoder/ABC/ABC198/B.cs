4e94c07829421bbda877fae9139b52b26e5401db
Takumi Oda <xiaotiantakumi@gmail.com>
2021-06-01 20:37:43 +0900

    ABC198のB回答

--- Atcoder/ABC/ABC198/B.cs
+++ Atcoder/ABC/ABC198/B.cs
@@ -1,9 +1,23 @@
-﻿namespace Atcoder.ABC.ABC198
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC198
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = Console.ReadLine();
+            var left = int.Parse(new string(N.Reverse().ToArray()));
+            var right = int.Parse(new string(left.ToString().Reverse().ToArray()));
+            if (left == right)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
