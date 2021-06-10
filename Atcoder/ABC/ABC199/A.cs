280d73e9a83da9e1385a9662ff59ed2dbacd14a4
Takumi Oda <xiaotiantakumi@gmail.com>
2021-06-10 23:09:10 +0900

    ABC199のA回答

--- Atcoder/ABC/ABC199/A.cs
+++ Atcoder/ABC/ABC199/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC.ABC199
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC199
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var ABC = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(Math.Pow(ABC[0], 2) + Math.Pow(ABC[1], 2) < Math.Pow(ABC[2], 2) ? "Yes" : "No");
         }
     }
 }
