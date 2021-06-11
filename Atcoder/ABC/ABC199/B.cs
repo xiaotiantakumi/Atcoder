f3102ee958a19b31420ba4c6c9f06464570ae702
Takumi Oda <xiaotiantakumi@gmail.com>
2021-06-11 21:59:25 +0900

    ABC199のB回答

--- Atcoder/ABC/ABC199/B.cs
+++ Atcoder/ABC/ABC199/B.cs
@@ -1,9 +1,17 @@
-﻿namespace Atcoder.ABC.ABC199
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC199
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var A = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var B = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var ret = B.Min() - A.Max() + 1;
+            Console.WriteLine(ret < 0 ? 0 : ret);
         }
     }
 }
