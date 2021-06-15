62b12bbce14bd2bc0526896d23b82b31353556d2
Takumi Oda <xiaotiantakumi@gmail.com>
2021-06-15 22:38:25 +0900

    ABC200のAB回答

--- Atcoder/ABC/ABC200/A.cs
+++ Atcoder/ABC/ABC200/A.cs
@@ -1,10 +1,14 @@
-﻿namespace Atcoder.ABC.ABC200
+﻿using System;
+
+namespace Atcoder.ABC.ABC200
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var N = int.Parse(Console.ReadLine());
+            var ret = (N / 100) + (N % 100 == 0 ? 0 : 1);
+            Console.WriteLine(ret);
         }
     }
 }
--- Atcoder/ABC/ABC200/B.cs
+++ Atcoder/ABC/ABC200/B.cs
@@ -1,9 +1,28 @@
-﻿namespace Atcoder.ABC.ABC200
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC200
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var NK = Console.ReadLine().Split().Select(decimal.Parse).ToArray();
+            var N = NK[0];
+            var K = NK[1];
+            for (int i = 0; i < K; i++)
+            {
+                if (N % 200 == 0)
+                {
+                    N /= 200;
+                }
+                else
+                {
+                    N = decimal.Parse(N.ToString() + "200");
+                }
+            }
+
+            Console.WriteLine(N);
         }
     }
 }
