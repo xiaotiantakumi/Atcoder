c4588f6fa13c8d5647d18d6cc34607a6e3f5dff2
Takumi Oda <xiaotiantakumi@gmail.com>
2021-06-17 22:47:24 +0900

    ABC200のC途中

--- Atcoder/ABC/ABC200/C.cs
+++ Atcoder/ABC/ABC200/C.cs
@@ -1,9 +1,31 @@
-﻿namespace Atcoder.ABC.ABC200
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC200
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            // 途中。
+            var N = int.Parse(Console.ReadLine());
+            var As = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var dic = new Dictionary<int, int>();
+            for (int i = 0; i < N; i++)
+            {
+                var tmp = As[i] % 200;
+                if (!dic.ContainsKey(tmp))
+                {
+                    dic[tmp] = 0;
+                }
+                else
+                {
+                    dic[tmp]++;
+                }
+            }
+
+            Console.WriteLine(dic.Values.Count());
         }
     }
 }
