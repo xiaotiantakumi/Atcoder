7618f7901b850f3b6f878e4aabbccb100286bd87
Takumi Oda <xiaotiantakumi@gmail.com>
2021-06-22 08:42:24 +0900

    ABC201のA回答

--- Atcoder/ABC/ABC201/A.cs
+++ Atcoder/ABC/ABC201/A.cs
@@ -1,10 +1,27 @@
-﻿namespace Atcoder.ABC.ABC201
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC201
 {
     public class A : AtcoderBase
     {
         public override  void Run()
         {
-            
+            var ABC = Console.ReadLine().Split().Select(int.Parse).OrderBy(x => x).ToArray();
+            var A = ABC[0];
+            var B = ABC[1];
+            var C = ABC[2];
+            var diff1 = C - B;
+            var diff2 = B - A;
+
+            if (diff2 == diff1)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
