06e6d7bc2ca3cc3682eebb29e796cd7ee3c7d198
Takumi Oda <xiaotiantakumi@gmail.com>
2021-06-23 23:08:59 +0900

    ABC201のB回答

--- Atcoder/ABC/ABC201/B.cs
+++ Atcoder/ABC/ABC201/B.cs
@@ -1,9 +1,23 @@
-﻿namespace Atcoder.ABC.ABC201
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atcoder.ABC.ABC201
 {
     public class B : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            for (int i = 0; i < N; i++)
+            {
+                var r = Console.ReadLine().Split();
+                dic.Add(r[0], int.Parse(r[1]));
+            }
+
+            var values = dic.OrderByDescending(x => x.Value).Select(x => x.Key).ToList();
+            Console.WriteLine(values[1]);
         }
     }
 }
