a51022640189dac50abf9b481196f11b9d325726
Takumi Oda <xiaotiantakumi@gmail.com>
2021-07-22 17:09:55 +0900

    EDPCのCD回答

--- Atcoder/Other/EDPC/C.cs
+++ Atcoder/Other/EDPC/C.cs
@@ -1,9 +1,42 @@
-﻿namespace Atcoder.Other.EDPC
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.Other.EDPC
 {
     public class C : AtcoderBase
     {
         public override void Run()
         {
+            var N = int.Parse(Console.ReadLine());
+            var dp = new int[N, 3];
+            for (int i = 0; i < N; i++)
+            {
+                var abc = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                dp[i, 0] = abc[0];
+                dp[i, 1] = abc[1];
+                dp[i, 2] = abc[2];
+                if (i != 0)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        var comp = dp[i, j];
+                        for (int k = 0; k < 3; k++)
+                        {
+                            if (j == k) continue;
+                            comp = Math.Max(dp[i, j] + dp[i - 1, k], comp);
+                        }
+                        dp[i, j] = comp;
+                    }
+                }
+            }
+
+            int ret = int.MinValue;
+            for (int i = 0; i < 3; i++)
+            {
+                ret = Math.Max(ret, dp[N - 1, i]);
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
--- Atcoder/Other/EDPC/D.cs
+++ Atcoder/Other/EDPC/D.cs
@@ -1,9 +1,36 @@
-﻿namespace Atcoder.Other.EDPC
+﻿using System;
+using System.Linq;
+
+namespace Atcoder.Other.EDPC
 {
     public class D : AtcoderBase
     {
         public override void Run()
         {
+            var NW = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var N = NW[0];
+            var W = NW[1];
+            int[,] dp = new int[110, 1100000];
+            int[] ws = new int[110];
+            int[] vs = new int[110];
+            for (int i = 1; i <= N; i++)
+            {
+                var wv = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                ws[i] = wv[0];
+                vs[i] = wv[1];
+            }
+
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = 0; j <= W; j++)
+                {
+                    dp[i, j] = dp[i - 1, j];
+                    if (j - ws[i] >= 0)
+                        dp[i, j] = Math.Max(dp[i, j], dp[i - 1, j - ws[i]] + vs[i]);
+                }
+            }
+
+            Console.WriteLine(dp[N, W]);
         }
     }
 }
