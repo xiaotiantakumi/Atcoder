032a91690428400f8d683d88d5f527f6d11449d9
Takumi Oda <xiaotiantakumi@gmail.com>
2021-08-13 10:51:57 +0900

    ABC201のC回答

--- Atcoder/ABC/ABC201/C.cs
+++ Atcoder/ABC/ABC201/C.cs
@@ -1,4 +1,6 @@
-﻿using CoderCommon;
+﻿using System;
+using System.Collections.Generic;
+using CoderCommon;
 
 namespace Atcoder.ABC.ABC201
 {
@@ -6,6 +8,49 @@ namespace Atcoder.ABC.ABC201
     {
         public override void Run()
         {
+            var S = Console.ReadLine();
+            List<string> includes = new List<string>();
+            List<string> notIncludes = new List<string>();
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] == 'o')
+                {
+                    includes.Add(i.ToString());
+                }
+                else if (S[i] == 'x')
+                {
+                    notIncludes.Add(i.ToString());
+                }
+            }
+
+            int ret = 0;
+            for (int i = 0; i < 10000; i++)
+            {
+                bool isContinue = false;
+                string targetStr = i.ToString().PadLeft(4, '0');
+                foreach (var current in targetStr)
+                {
+                    if (notIncludes.Contains(current.ToString()))
+                    {
+                        isContinue = true;
+                        break;
+                    }
+                }
+                if (isContinue) continue;
+
+                foreach (var current in includes)
+                {
+                    if (!targetStr.Contains(current))
+                    {
+                        isContinue = true;
+                        break;
+                    }
+                }
+                if (isContinue) continue;
+                ret++;
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
