d80478273083ccea985d231797c040247d07aa75
Takumi Oda <xiaotiantakumi@gmail.com>
2021-08-13 12:16:00 +0900

    ABC197のB回答

--- Atcoder/ABC/ABC197/B.cs
+++ Atcoder/ABC/ABC197/B.cs
@@ -1,4 +1,6 @@
-﻿using CoderCommon;
+﻿using System;
+using System.Linq;
+using CoderCommon;
 
 namespace Atcoder.ABC.ABC197
 {
@@ -6,6 +8,64 @@ namespace Atcoder.ABC.ABC197
     {
         public override void Run()
         {
+            var HWXY = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var H = HWXY[0];
+            var W = HWXY[1];
+            var X = HWXY[2] - 1;
+            var Y = HWXY[3] - 1;
+            char[,] map = new char[H, W];
+            for (int i = 0; i < H; i++)
+            {
+                var s = Console.ReadLine();
+                for (int j = 0; j < s.Length; j++)
+                {
+                    map[i, j] = s[j];
+                }
+            }
+
+            long ret = 1;
+            int start = -1;
+            while (true)
+            {
+                if (X + start < 0 || map[X + start, Y] == '#')
+                {
+                    break;
+                }
+                ret++;
+                start--;
+            }
+            start = 1;
+            while (true)
+            {
+                if (X + start == H || map[X + start, Y] == '#')
+                {
+                    break;
+                }
+                ret++;
+                start++;
+            }
+            start = -1;
+            while (true)
+            {
+                if (Y + start < 0 || map[X, Y + start] == '#')
+                {
+                    break;
+                }
+                ret++;
+                start--;
+            }
+            start = 1;
+            while (true)
+            {
+                if (Y + start == W || map[X, Y + start] == '#')
+                {
+                    break;
+                }
+                ret++;
+                start++;
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
