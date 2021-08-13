85368c0daf6752a18cc22c1e9d6485a40e7c1017
Takumi Oda <xiaotiantakumi@gmail.com>
2021-08-13 10:28:49 +0900

    ABC202のC回答

--- Atcoder/ABC/ABC202/C.cs
+++ Atcoder/ABC/ABC202/C.cs
@@ -1,4 +1,7 @@
-﻿using CoderCommon;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoderCommon;
 
 namespace Atcoder.ABC.ABC202
 {
@@ -6,6 +9,34 @@ namespace Atcoder.ABC.ABC202
     {
         public override void Run()
         {
+            SourceExpander.Expander.Expand(ignoreAnyError: false);
+            var N = int.Parse(Console.ReadLine());
+            var A = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var B = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var C = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Dictionary<int, int> bAjustDic = new Dictionary<int, int>();
+            foreach (var bIndex in C)
+            {
+                var index = bIndex - 1;
+                var key = B[index];
+                if (!bAjustDic.ContainsKey(key))
+                {
+                    bAjustDic[key] = 0;
+                }
+                bAjustDic[key]++;
+            }
+
+            decimal ret = 0;
+            foreach (var current in A)
+            {
+                if (bAjustDic.ContainsKey(current))
+                {
+                    ret += bAjustDic[current];
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
