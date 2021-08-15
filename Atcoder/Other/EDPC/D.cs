a8294abe0a7c89b331d2676a9c56502cce85e320
Takumi Oda <xiaotiantakumi@gmail.com>
2021-08-15 10:01:19 +0900

    EDPCのDE回答

--- Atcoder/Other/EDPC/D.cs
+++ Atcoder/Other/EDPC/D.cs
@@ -11,27 +11,26 @@ namespace Atcoder.Other.EDPC
             var NW = Console.ReadLine().Split().Select(int.Parse).ToArray();
             var N = NW[0];
             var W = NW[1];
-            int[,] dp = new int[110, 1100000];
-            int[] ws = new int[110];
-            int[] vs = new int[110];
-            for (int i = 1; i <= N; i++)
+            long[,] dp = new long[N + 1, W + 1];
+            for (int i = 0; i < N; i++)
             {
                 var wv = Console.ReadLine().Split().Select(int.Parse).ToArray();
-                ws[i] = wv[0];
-                vs[i] = wv[1];
-            }
-
-            for (int i = 1; i <= N; i++)
-            {
                 for (int j = 0; j <= W; j++)
                 {
-                    dp[i, j] = dp[i - 1, j];
-                    if (j - ws[i] >= 0)
-                        dp[i, j] = Math.Max(dp[i, j], dp[i - 1, j - ws[i]] + vs[i]);
+                    dp[i + 1, j] = dp[i, j];
+                    if (j - wv[0] >= 0){
+                        dp[i + 1, j] = Math.Max(dp[i, j], dp[i, j - wv[0]] + wv[1]);
+                    }
                 }
             }
 
-            Console.WriteLine(dp[N, W]);
+            long ret = 0;
+            for (int i = 0; i <= W; i++)
+            {
+                ret = Math.Max(ret, dp[N, i]);
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
--- Atcoder/Other/EDPC/E.cs
+++ Atcoder/Other/EDPC/E.cs
@@ -1,4 +1,7 @@
-﻿using CoderCommon;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoderCommon;
 
 namespace Atcoder.Other.EDPC
 {
@@ -6,6 +9,41 @@ namespace Atcoder.Other.EDPC
     {
         public override void Run()
         {
+            var NW = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var N = NW[0];
+            var W = NW[1];
+            long ret = 0;
+            List<int[]> wvs = new List<int[]>();
+            for (int i = 0; i < N; i++)
+            {
+                var wv = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                wvs.Add(wv);
+            }
+
+            var maxV = wvs.Sum(x => x[1]);
+            long[,] dp = new long[N + 1, maxV + 1];
+            for (int j = 1; j <= maxV; j++)
+            {
+                dp[0, j] = int.MaxValue;
+            }
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j <= maxV; j++)
+                {
+                    dp[i + 1, j] = dp[i, j];
+                    if (j - wvs[i][1] >= 0)
+                    {
+                        dp[i + 1, j] = Math.Min(dp[i, j], dp[i, j - wvs[i][1]] + wvs[i][0]);
+                    }
+                }
+            }
+
+            for (int i = 0; i <= maxV; i++)
+            {
+                if (dp[N, i] <= W) ret = Math.Max(ret, i);
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
