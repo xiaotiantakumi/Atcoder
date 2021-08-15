ce770188b524dabc4ec487dfdde94e8d023c2a39
Takumi Oda <xiaotiantakumi@gmail.com>
2021-08-15 17:15:30 +0900

    ABC212のA回答

--- Atcoder/ABC/ABC212/A.cs
+++ Atcoder/ABC/ABC212/A.cs
@@ -1,4 +1,6 @@
-﻿using CoderCommon;
+﻿using System;
+using System.Linq;
+using CoderCommon;
 
 namespace Atcoder.ABC.ABC212
 {
@@ -6,7 +8,20 @@ namespace Atcoder.ABC.ABC212
     {
         public override  void Run()
         {
-            
+            var AB = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            if (AB[0] == 0 && AB[1] > 0)
+            {
+                Console.WriteLine("Silver");
+            }
+            else if (AB[1] == 0 && AB[0] > 0)
+            {
+                Console.WriteLine("Gold");
+
+            }
+            else
+            {
+                Console.WriteLine("Alloy ");
+            }
         }
     }
 }
