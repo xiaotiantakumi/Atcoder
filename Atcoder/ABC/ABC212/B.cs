18e74aa40aebbaa401af4e6da9e4760882dc3edb
Takumi Oda <xiaotiantakumi@gmail.com>
2021-08-18 22:56:22 +0900

    ABC212のB回答

--- Atcoder/ABC/ABC212/B.cs
+++ Atcoder/ABC/ABC212/B.cs
@@ -1,4 +1,6 @@
-﻿using CoderCommon;
+﻿using System;
+using System.Linq;
+using CoderCommon;
 
 namespace Atcoder.ABC.ABC212
 {
@@ -6,6 +8,37 @@ namespace Atcoder.ABC.ABC212
     {
         public override void Run()
         {
+            var s = Console.ReadLine();
+            bool isWeak = false;
+            var c = s[0];
+            isWeak = s.All(x => x == c);
+
+            if (isWeak)
+            {
+                Console.WriteLine("Weak");
+                return;
+            }
+
+            bool flg = false;
+            for (int i = 1; i < s.Length; i++)
+            {
+                var pre = int.Parse(s[i - 1].ToString());
+                var current = int.Parse(s[i].ToString());
+                if ((pre == 9 && current == 0) || pre + 1 == current)
+                {
+                }
+                else
+                {
+                    flg = true;
+                }
+            }
+
+            if (!flg)
+            {
+                Console.WriteLine("Weak");
+                return;
+            }
+            Console.WriteLine("Strong");
         }
     }
 }
