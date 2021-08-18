ea08faf1d7a6a3d6214a1eca7b38e1fd7782afb0
Takumi Oda <xiaotiantakumi@gmail.com>
2021-08-18 22:54:05 +0900

    ABC203のAB回答

--- Atcoder/ABC/ABC203/A.cs
+++ Atcoder/ABC/ABC203/A.cs
@@ -1,4 +1,6 @@
-﻿using CoderCommon;
+﻿using System;
+using System.Linq;
+using CoderCommon;
 
 namespace Atcoder.ABC.ABC203
 {
@@ -6,7 +8,26 @@ namespace Atcoder.ABC.ABC203
     {
         public override  void Run()
         {
-            
+            var abc = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            if (abc[0] != abc[1] && abc[0] != abc[2] && abc[1] != abc[2])
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                if (abc[0] == abc[1])
+                {
+                    Console.WriteLine(abc[2]);
+                }
+                else if (abc[1] == abc[2])
+                {
+                    Console.WriteLine(abc[0]);
+                }
+                else if (abc[0] == abc[2])
+                {
+                    Console.WriteLine(abc[1]);
+                }
+            }
         }
     }
 }
--- Atcoder/ABC/ABC203/B.cs
+++ Atcoder/ABC/ABC203/B.cs
@@ -1,4 +1,7 @@
-﻿using CoderCommon;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoderCommon;
 
 namespace Atcoder.ABC.ABC203
 {
@@ -6,6 +9,18 @@ namespace Atcoder.ABC.ABC203
     {
         public override void Run()
         {
+            var NK = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            List<long> rooms = new List<long>();
+            long ret = 0;
+            for (int i = 0; i < NK[0]; i++)
+            {
+                for (int j = 0; j < NK[1]; j++)
+                {
+                    ret += (100 * (i + 1)) + j + 1;
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
