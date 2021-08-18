8e0d5dcf5265e3fe427b1b144a08f917164d082e
Takumi Oda <xiaotiantakumi@gmail.com>
2021-08-18 22:55:51 +0900

    PriorityQueueの場所移動

--- CoderCommon/Library/PriorityQueue.cs
+++ CoderCommon/Library/PriorityQueue.cs
@@ -2,9 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
-namespace Atcoder.Algorithm
+namespace CoderCommon.Library
 {
     public class PriorityQueue<T> : IEnumerable<T>
     {
